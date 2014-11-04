b70fbe207abd19bde0e945b1ca4215beb49c61bc
Andrew Grimberg <agrimberg@linuxfoundation.org>
2014-11-04 14:56:56 -0800

    Add the ovsdb-docker Vagrant config
    Given how sparsely configured this image actually is, it could probably
    be easily used by any other project that needs Java7 (needed for being a
    Jenkins slave at a minimum) and just docker after that.
    
    Change-Id: I4022bb69d1a701a7d40d742f86af38835d3dd5c9
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ vagrant/ovsdb-docker/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yum clean all
+# Add the ODL yum repo
+#yum install -q -y https://nexus.opendaylight.org/content/repositories/opendaylight-yum-epel-6-x86_64/rpm/opendaylight-release/0.1.0-1.el6.noarch/opendaylight-release-0.1.0-1.el6.noarch.rpm
+yum update -q -y
+
+yum install -q -y java-1.7.0-openjdk-devel docker-io supervisor git
+
+systemctl enable docker.service
+
+echo "***************************************************"
+echo "*   PLEASE RELOAD THIS VAGRANT BOX BEFORE USE     *"
+echo "***************************************************"
