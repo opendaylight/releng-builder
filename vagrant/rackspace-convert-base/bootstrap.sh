01314c1661c6b4acfccc53fc2ae42a24d7583306
Andrew Grimberg <agrimberg@linuxfoundation.org>
2014-11-07 14:57:55 -0800

    Handle base images that have selinux disabled
    The CentOS 6 base image has selinux set to disabled. Because of this, it
    requires a stepping through permissive with a relabel during a reboot.
    Unfortunately you can't have the scripts issue the reboot without
    vagrant getting annoyed.
    
    Change-Id: Ia46348162df9dc5deabf3a6d1907ca54cbd97b33
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- vagrant/rackspace-convert-base/bootstrap.sh
+++ vagrant/rackspace-convert-base/bootstrap.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# enable enforcing mode from the very start
-setenforce enforcing
+# vim: ts=4 sw=4 sts=4 et :
 
-# configure system for enforcing mode on next boot
-sed -i 's/SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
+# Handle the occurance where SELINUX is actually disabled
+if [ `grep SELINUX=permissive /etc/selinux/config` ]; then
+    # enable enforcing mode from the very start
+    setenforce enforcing
 
-yum clean all
-yum update -y
+    # configure system for enforcing mode on next boot
+    sed -i 's/SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
+else
+    sed -i 's/SELINUX=disabled/SELINUX=permissive/' /etc/selinux/config
+    touch /.autorelabel
+
+    echo "*******************************************"
+    echo "** SYSTEM REQUIRES A RESTART FOR SELINUX **"
+    echo "*******************************************"
+fi
+
+yum clean all -q
+yum update -y -q
--- vagrant/rackspace-convert-base/remove_requiretty.sh
+++ vagrant/rackspace-convert-base/remove_requiretty.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-/bin/sed -i 's/requiretty/!requiretty/' /etc/sudoers;
+
+# vim: sw=2 ts=2 sts=2 et :
+
+if [ ! -e /.autorelease ]; then
+  /bin/sed -i 's/requiretty/!requiretty/' /etc/sudoers;
+fi
