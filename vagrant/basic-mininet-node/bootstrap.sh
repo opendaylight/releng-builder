7dabcef47b42848afb159d2d43f1a3134157adb9
Andrew Grimberg <agrimberg@linuxfoundation.org>
2014-11-16 20:15:58 -0800

    Add the basic mininet integration lab box
    The 'mininet' system that is used by the integration lab is a system
    that combines mininet, netopeer / NETCONF and has PostgreSQL 9.3
    installed to support a VTN coordinator also be brought up.
    
    Change-Id: I95f7c38419985f6f0a6bb258a4173feaa8bfd42b
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ vagrant/basic-mininet-node/bootstrap.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# vim: sw=4 ts=4 sts=4 et tw=72 :
+
+yum clean all
+# Add the ODL yum repo (not needed for java nodes, but useful for
+# potential later layers)
+yum install -q -y https://nexus.opendaylight.org/content/repositories/opendaylight-yum-epel-6-x86_64/rpm/opendaylight-release/0.1.0-1.el6.noarch/opendaylight-release-0.1.0-1.el6.noarch.rpm
+
+# Make sure the system is fully up to date
+yum update -q -y
+
+# Add in git (needed for most everything) and XML-XPath as it is useful
+# for doing CLI based CML parsing of POM files
+yum install -q -y git perl-{XML-XPath,Digest-SHA}
+
+# install all available openjdk-devel sets
+yum install -q -y 'java-*-openjdk-devel'
+
+# we currently use Java7 (aka java-1.7.0-openjdk) as our standard make
+# sure that this is the java that alternatives is pointing to, dynamic
+# spin-up scripts can switch to any of the current JREs installed if
+# needed
+alternatives --set java /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/java
+alternatives --set java_sdk_openjdk /usr/lib/jvm/java-1.7.0-openjdk.x86_64
+
+# To handle the prompt style that is expected all over the environment
+# with how use use robotframework we need to make sure that it is
+# consistent for any of the users that are created during dynamic spin
+# ups
+echo 'PS1="[\u@\h \W]> "' >> /etc/skel/.bashrc
+
+# add in mininet and netopeer
+yum install -q -y netopeer-server-sl CPqD-ofsoftswitch13 mininet telnet
+
+# we need semanage installed for some of the next bit
+yum install -q -y policycoreutils-python
+
+# netconf / netopeer needs some special modifications to ssh
+semanage port -a -t ssh_port_t -p tcp '830'
+
+# The default /etc/ssh/sshd_config doesn't actually specify a port as such
+# we need to specify both 22 as well as 830 along with the netconf
+# subsystem
+echo << EOSSH >> /etc/ssh/sshd_config
+
+# Added for netconf / netopeer testing
+Port 22
+Port 830
+Subsystem netconf /usr/bin/netopeer-server-sl
+EOSSH
+
+# set the system up for VTN as well (note we are not adding all the bits
+# to allow VTN to operate in the Hydrogen design erra)
+mkdir /usr/local/vtn
+
+yum install -q -y http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-redhat93-9.3-1.noarch.rpm
+yum install -q -y postgresql93{,-{contrib,libs,server,odbc}}
+chkconfig postgresql-9.3 off
+
+# the current deploy_vtn.sh script used by integration will be pushed
+# into the system during dynamic spin-up as it has changed often enough
+# that sticking it into the image may not be good as changing it would
+# either require updating the image or doing it during the dynamic
+# spin-up anyway.
