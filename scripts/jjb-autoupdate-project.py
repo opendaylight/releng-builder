dbfe73599edd82eedd5042c409a927262242a945
Thanh Ha <thanh.ha@linuxfoundation.org>
2014-12-16 22:01:39 -0500

    Add License headers
    Change-Id: I483787bde5824ee4274601668cb306adac7ec89f
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- scripts/jjb-autoupdate-project.py
+++ scripts/jjb-autoupdate-project.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python
+##############################################################################
+# Copyright (c) 2014 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Thanh Ha (The Linux Foundation) - Initial implementation
+##############################################################################
 
 import os
 
