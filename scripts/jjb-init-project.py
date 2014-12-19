29e26c2f2b65f83c09ad225e9044e89dce85f3c5
Thanh Ha <thanh.ha@linuxfoundation.org>
2014-12-18 22:53:16 -0500

    Add integration job to common template
    This patch adds a new job type project-integration to the common job
    template. It serves to be the single job to handle all integration jobs
    existing on the master silo. This integration job is by default disabled
    and unused.
    
    Projects can pass a list of dependent projects in their project.cfg file for example:
    
    DEPENDENCIES: aaa,controller,yangtools
    
    This will flip the switch to enable the project-integration job and
    cause a build to be triggered when any of the dependent jobs are built.
    
    For email it will send an email with the project tags for ALL projects
    listed in the dependencies list thusly notifying all dependent projects
    of the build issue.
    
    Change-Id: I8e06fb378dadb6d8b083b08cba366eb863efac76
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- scripts/jjb-init-project.py
+++ scripts/jjb-init-project.py
@@ -19,6 +19,14 @@ import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("project", help="project")
+parser.add_argument("-d", "--dependencies",
+                    help=("Project dependencies\n\n"
+                          "A comma-seperated (no spaces) list of projects "
+                          "your project depends on. "
+                          "This is used to create an integration job that "
+                          "will trigger when a dependent project-merge job "
+                          "is built successfully.\n\n"
+                          "Example: aaa,controller,yangtools"))
 parser.add_argument("-g", "--mvn-goals", help="Maven Goals")
 parser.add_argument("-o", "--mvn-opts", help="Maven Options")
 args = parser.parse_args()
@@ -28,6 +36,10 @@ project_dir = os.path.join("jjb", project)
 project_file = os.path.join(project_dir, "%s.yaml" % project)
 mvn_goals = args.mvn_goals  # Defaults to "clean install" if not passsed
 mvn_opts = args.mvn_opts    # Defaults to blank if not passed
+dependencies = args.dependencies
+dependent_jobs = ""
+disabled = "true"   # Always disabled unless project has dependencies
+email_prefix = "[%s]" % project
 
 template_file = os.path.join("jjb", "job.yaml.template")
 
@@ -40,13 +52,25 @@ if not mvn_goals:
 if not mvn_opts:
     mvn_opts = "-Xmx1024m -XX:MaxPermSize=256m"
 
+if dependencies:
+    disabled = "false"
+    email_prefix = (email_prefix + " " +
+                    " ".join(['[%s]' % d for d in dependencies.split(",")]))
+    dependent_jobs = ",".join(
+        ['%s-merge' % d for d in dependencies.split(",")])
+
 # Create project directory if it doesn't exist
 if not os.path.exists(project_dir):
     os.makedirs(project_dir)
 
-print("project: %s\ngoals: %s\noptions: %s" % (project,
-                                               mvn_goals,
-                                               mvn_opts))
+print("project: %s\n"
+      "goals: %s\n"
+      "options: %s\n"
+      "dependencies: %s" %
+      (project,
+       mvn_goals,
+       mvn_opts,
+       dependencies))
 
 # Create initial project YAML file
 with open(template_file, "r") as infile:
@@ -54,8 +78,9 @@ with open(template_file, "r") as infile:
         for line in infile:
             if not re.match("\s*#", line):
                 line = re.sub("PROJECT", project, line)
-            if not re.match("\s*#", line):
+                line = re.sub("DISABLED", disabled, line)
                 line = re.sub("MAVEN_GOALS", mvn_goals, line)
-            if not re.match("\s*#", line):
                 line = re.sub("MAVEN_OPTS", mvn_opts, line)
+                line = re.sub("DEPENDENCIES", dependent_jobs, line)
+                line = re.sub("EMAIL_PREFIX", email_prefix, line)
             outfile.write(line)
