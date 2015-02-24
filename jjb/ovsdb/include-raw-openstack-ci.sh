fe1bb55a9d8a48288417d36fef7566029c0282d0
Kyle Mestery <mestery@mestery.com>
2015-02-24 11:42:46 +0000

    Move to using devstack-gate for this job
    Instead of using a custom script, lets use the OpenStack devstack-gate
    infrastructure to run the ODL tests.
    
    Change-Id: Ib0a990f35764abd4b9f03385a414b0756aab62f9
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- jjb/ovsdb/include-raw-openstack-ci.sh
+++ jjb/ovsdb/include-raw-openstack-ci.sh
@@ -1,206 +1,123 @@
 #!/bin/bash
+#
+# NOTE: This file takes two jobs from the OpenStack infra and
+#       puts them here. See here:
+#
+# https://github.com/openstack-infra/project-config/blob/master/jenkins/jobs/networking-odl.yaml
 
-export PATH=$PATH:/bin:/sbin:/usr/sbin
-export DEVSTACKDIR=$WORKSPACE/$BUILD_TAG
-mkdir -p $DEVSTACKDIR
-cd $DEVSTACKDIR
+export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin
 
-cat <<EOL > firewall.sh
-sudo iptables -I INPUT -p tcp --dport 5672 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 9292 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 9696 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 35357 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 6080 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 8773 -j ACCEPT
-sudo iptables -I INPUT -p udp --dport 8472 -j ACCEPT
-sudo iptables -I INPUT -p udp --dport 4789 -j ACCEPT
+# *SIGH*. This is required to get lsb_release
+sudo yum -y install redhat-lsb-core indent python-testrepository
 
-# For the client
-sudo iptables -I INPUT -p tcp --dport 5000 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 8774 -j ACCEPT
+echo "Making /opt/stack/new jenkins:jenkins"
+sudo /usr/sbin/groupadd jenkins
+sudo mkdir -p /opt/stack/new
+sudo chown -R jenkins:jenkins /opt/stack/new
+sudo bash -c 'echo "jenkins ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
+sudo bash -c 'echo "stack ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
 
-# For ODL
-sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
-EOL
-chmod +x firewall.sh
+# Save existing WORKSPACE
+SAVED_WORKSPACE=$WORKSPACE
+export WORKSPACE=~/workspace
+mkdir -p $WORKSPACE
+cd $WORKSPACE
 
-env
-cat $DEVSTACKDIR/firewall.sh
-$DEVSTACKDIR/firewall.sh
-
-# FIXME: update version of pip being used here
-curl -O https://pypi.python.org/packages/source/p/pip/pip-6.0.8.tar.gz
-tar xvfz pip-6.0.8.tar.gz
-cd pip-6.0.8
-sudo -E python setup.py install
-sudo pip install testtools
-
-sudo mkdir -p /opt/stack
-sudo chown $(whoami) /opt/stack
-sudo chmod 755 /opt/stack
-cd /opt/stack
-
-cd $DEVSTACKDIR
-
-sudo yum -y install qemu libvirt-daemon
-git clone https://github.com/openstack-dev/devstack.git
-cd devstack
-
-####
-# Specify changeset being worked on if it's networking-odl
-####
-if [ "$GERRIT_PROJECT" == "stackforge/networking-odl" ]; then
-    cat <<EOLLC > local.conf
-[[local|localrc]]
-enable_plugin networking-odl https://$GERRIT_HOST/$GERRIT_PROJECT $GERRIT_REFSPEC
-EOLLC
+# This is the job which checks out devstack-gate
+if [[ ! -e devstack-gate ]]; then
+    echo "Cloning devstack-gate"
+    git clone https://git.openstack.org/openstack-infra/devstack-gate
 else
-    cat <<EOLLC > local.conf
-[[local|localrc]]
-enable_plugin networking-odl https://github.com/stackforge/networking-odl
-EOLLC
+    echo "Fixing devstack-gate git remotes"
+    cd devstack-gate
+    git remote set-url origin https://git.openstack.org/openstack-infra/devstack-gate
+    git remote update
+    git reset --hard
+    if ! git clean -x -f ; then
+        sleep 1
+        git clean -x -f
+    fi
+    git checkout master
+    git reset --hard remotes/origin/master
+    if ! git clean -x -f ; then
+        sleep 1
+        git clean -x -f
+    fi
+    cd ..
 fi
 
-cat <<EOLLC >> local.conf
-LOGFILE=stack.sh.log
-SCREEN_LOGDIR=/opt/stack/data/log
-VERBOSE=True
-LOG_COLOR=False
-RECLONE=yes
-GIT_TIMEOUT=0
-GIT_BASE=https://git.openstack.org
-
-# The below is needed on Fedora/CentOS6
-#disable_service rabbit
-#enable_service qpid
-
-disable_service swift
-enable_service n-cpu
-enable_service n-cond
-disable_service n-net
-enable_service q-svc
-enable_service q-dhcp
-enable_service q-l3
-enable_service q-meta
-enable_service quantum
-enable_service tempest
-
-API_RATE_LIMIT=False
-
-Q_PLUGIN=ml2
-Q_ML2_PLUGIN_MECHANISM_DRIVERS=logger,opendaylight
-ENABLE_TENANT_TUNNELS=True
-
-ODL_MODE=allinone
-ODL_NETVIRT_DEBUG_LOGS=True
-ODL_MGR_IP=$(ip addr | grep inet | grep eth0 | awk -F" " '{print $2}'| sed -e 's/\/.*$//')
-ODL_ARGS="-Xmx1024m -XX:MaxPermSize=512m"
-ODL_BOOT_WAIT=90
-
-VNCSERVER_LISTEN=0.0.0.0
-
-HOST_NAME=$(hostname)
-SERVICE_HOST_NAME=$(hostname)
-HOST_IP=$(ip addr | grep inet | grep eth0 | awk -F" " '{print $2}'| sed -e 's/\/.*$//')
-SERVICE_HOST=$(hostname)
-
-MYSQL_HOST=$(hostname)
-RABBIT_HOST=$(hostname)
-GLANCE_HOSTPORT=$(hostname):9292
-KEYSTONE_AUTH_HOST=$(hostname)
-KEYSTONE_SERVICE_HOST=$(hostname)
-
-MYSQL_PASSWORD=mysql
-RABBIT_PASSWORD=rabbit
-#QPID_PASSWORD=rabbit
-SERVICE_TOKEN=service
-SERVICE_PASSWORD=admin
-ADMIN_PASSWORD=admin
-EOLLC
-
-# Add Neutron specific config if required
+# Set the pieces we want to test
 if [ "$GERRIT_PROJECT" == "openstack/neutron" ]; then
-    cat <<EOLLC >> local.conf
-NEUTRON_REPO=https://$GERRIT_HOST/$GERRIT_PROJECT
-NEUTRON_BRANCH=$GERRIT_REFSPEC
-EOLLC
+    ZUUL_PROJECT=$GERRIT_PROJECT
+    ZUUL_BRANCH=$GERRIT_REFSPEC
+elif [ "$GERRIT_PROJECT" == "openstack-dev/devstack" ]; then
+    ZUUL_PROJECT=$GERRIT_PROJECT
+    ZUUL_BRANCH=$GERRIT_REFSPEC
 fi
 
-echo 'cat local.conf'
-cat local.conf
-
-####
-# Clone the changeset being worked on if it's devstack
-####
-if [ "$GERRIT_PROJECT" == "openstack-dev/devstack" ]; then
-    git fetch https://$GERRIT_HOST/$GERRIT_PROJECT $GERRIT_REFSPEC && git checkout FETCH_HEAD
+echo "Setting environment variables"
+# And this runs devstack-gate
+export PYTHONUNBUFFERED=true
+export DEVSTACK_GATE_TIMEOUT=120
+export DEVSTACK_GATE_NEUTRON=1
+export DEVSTACK_GATE_TEMPEST=1
+export BRANCH_OVERRIDE=master
+if [ "$BRANCH_OVERRIDE" != "default" ] ; then
+    export OVERRIDE_ZUUL_BRANCH=$BRANCH_OVERRIDE
 fi
-
-# Run devstack
-./stack.sh
-
-if [ "$?" != "0" ]; then
-    echo "stack.sh failed"
-    # Copy logs off
-    mkdir -p $WORKSPACE/logs/devstack
-    mkdir -p $WORKSPACE/logs/opendaylight
-    cp -r /opt/stack/data/log/* $WORKSPACE/logs/devstack
-    cp -r $DEVSTACKDIR/devstack/stack.sh.log* $WORKSPACE/logs
-    cp -r $DEVSTACKDIR/devstack/local.conf $WORKSPACE/logs
-    cp -r /opt/stack/opendaylight/*/logs $WORKSPACE/logs/opendaylight
-    cp -r /opt/stack/opendaylight/*/data/log $WORKSPACE/logs/opendaylight
-    cp -r /opt/stack/opendaylight/*/etc $WORKSPACE/logs/opendaylight
-    tar cvzf $WORKSPACE/opendaylight-full-logs.tgz $WORKSPACE/logs
-    exit 1
+# Because we are testing a non standard project, add
+# our project repository. This makes zuul do the right
+# reference magic for testing changes.
+export PROJECTS="stackforge/networking-odl $PROJECTS"
+# Note the actual url here is somewhat irrelevant because it
+# caches in nodepool, however make it a valid url for
+# documentation purposes.
+if [ "$GERRIT_PROJECT" == "stackforge/networking-odl" ]; then
+    export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-odl https://$GERRIT_HOST/$GERRIT_PROJECT $GERRIT_REFSPEC"
 else
-    echo "stack.sh completed successfully"
-fi
-
-# running tempest
-if [[ -n ${BUILD_ID} ]]; then
-    cd /opt/stack/tempest
-
-    # put all the info in the following file for processing later
-    log_for_review=odl_tempest_test_list.txt
-
-    echo "Running tempest tests:" > /tmp/${log_for_review}
-    echo "" >> /tmp/${log_for_review}
-    testr init > /dev/null 2>&1 || true
-    cmd_api="tempest.api.network.test_networks \
-             tempest.api.network.test_networks_negative \
-             tempest.api.network.test_ports tempest.api.network.test_routers"
-    cmd_net_basic="tempest.scenario.test_network_basic_ops"
-    cmd="testr run $cmd_api $cmd_net_basic"
-    echo "opendaylight-test:$ "${cmd}  >> /tmp/${log_for_review}
-    ${cmd} >> /tmp/${log_for_review}
-    echo "" >> /tmp/${log_for_review}
-    echo "" >> /tmp/${log_for_review}
-
-    x=$(grep "id=" /tmp/${log_for_review})
-    y="${x//[()=]/ }"
-    z=$(echo ${y} | awk '{print $3}' | sed 's/\,//g')
-
-    #echo "x ($x) y ($y) z ($z)"
-
-    echo "List of tempest tests ran (id="${z}"):" >> /tmp/${log_for_review}
-    echo "" >> /tmp/${log_for_review}
-
-    grep -ri successful   .testrepository/${z}  |  awk '{ gsub(/\[/, "\ ");  print $1 " " $2}' >> /tmp/${log_for_review}
+    export DEVSTACK_LOCAL_CONFIG="enable_plugin networking-odl https://git.openstack.org/stackforge/networking-odl"
 fi
 
 
-# Copy logs off
-mkdir -p $WORKSPACE/logs/devstack
-mkdir -p $WORKSPACE/logs/tempest
+# Keep localrc to be able to set some vars in pre_test_hook
+export KEEP_LOCALRC=1
+
+# Unset this because it's set by the underlying Jenkins node ...
+unset GIT_BASE
+
+# Only run certain tempest tests
+export DEVSTACK_GATE_TEMPEST_REGEX="tempest.api.network.test_networks \
+                                    tempest.api.network.test_networks_negative"
+
+# Specifically set the services we want
+#OVERRIDE_ENABLED_SERVICES=q-svc,q-dhcp,q-l3,q-meta,quantum,key,g-api,g-reg,n-api,n-crt,n-obj,n-cpu,n-cond,n-sch,n-xvnc,n-cauth,h-eng,h-api,h-api-cfn,h-api-cw,rabbit,tempest,mysql
+
+# Enable ODL debug logs and set memory parameters
+export ODL_NETVIRT_DEBUG_LOGS=True
+export ODL_JAVA_MIN_MEM=512m
+export ODL_JAVA_MAX_MEM=784m
+export ODL_JAVA_MAX_PERM_MEM=784m
+
+# Trim down the boot wait time
+export ODL_BOOT_WAIT=30
+
+echo "Copying devstack-vm-gate-wrap.sh"
+cp devstack-gate/devstack-vm-gate-wrap.sh ./safe-devstack-vm-gate-wrap.sh
+echo "Running safe-devstack-vm-gate-wrap.sh"
+./safe-devstack-vm-gate-wrap.sh
+# Save the return value so we can exit with this
+DGRET=$?
+
+# Restore WORKSPACE
+OS_WORKSPACE=$WORKSPACE
+export WORKSPACE=$SAVED_WORKSPACE
+
+# Copy all the logs
+cp -r $OS_WORKSPACE/logs $WORKSPACE
+cp -a /opt/stack/new/logs/q-odl-karaf* $WORKSPACE/logs
 mkdir -p $WORKSPACE/logs/opendaylight
-cp -r /opt/stack/tempest/tempest.log* $WORKSPACE/logs/tempest
-cp -r /opt/stack/data/log/* $WORKSPACE/logs/devstack
-cp -r /opt/stack/opendaylight/*/logs $WORKSPACE/logs/opendaylight
-cp -r /opt/stack/opendaylight/*/data/log $WORKSPACE/logs/opendaylight
-cp -r /opt/stack/opendaylight/*/etc $WORKSPACE/logs/opendaylight
-cp -r /tmp/${log_for_review} $WORKSPACE/logs
-cp -r /tmp/${log_for_review} $WORKSPACE
-cp -r $DEVSTACKDIR/devstack/stack.sh.log* $WORKSPACE/logs
-cp -r $DEVSTACKDIR/devstack/local.conf $WORKSPACE/logs
-tar cvzf $WORKSPACE/opendaylight-full-logs.tgz $WORKSPACE/logs
+cp -a /opt/stack/new/opendaylight/distribution*/etc $WORKSPACE/logs/opendaylight
+# Unzip the logs to make them easier to view
+gunzip $WORKSPACE/logs/*.gz
+
+exit $DGRET
