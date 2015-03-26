4ce033ca5724e99cd221680228c99f9ae8ab292a
Richard  Hill <ricjhill@gmail.com>
2015-03-26 16:20:19 +0100

    Removed	 white space from file
    Edited 'include-raw-integration-configure-clustering.sh'
    to improve formatting by removing white space.
    
    Change-Id: I57e5b03af68377dec4a79a6a3064104b539716df
    Signed-off-by: Richard  Hill <ricjhill@gmail.com>

--- jjb/integration/include-raw-integration-configure-clustering.sh
+++ jjb/integration/include-raw-integration-configure-clustering.sh
@@ -1,10 +1,9 @@
-#  this script configures replication on a single ODL controller.
-#  files touched are akka.conf modules-shards.conf
-
 echo "##################################################"
 echo "## include-raw-integration-configure-clustering ##"
 echo "##################################################"
 
+#  this script configures replication on a single ODL controller.
+#  files touched are akka.conf modules-shards.conf
 
 
 
@@ -15,48 +14,40 @@ echo "##################################"
 declare CONTROLLERIPS=($(cat slave_addresses.txt | grep CONTROLLER | awk -F = '{print $2}'))
 declare -p CONTROLLERIPS
 
-## Describe  the script run on guest vm (node) to configure clustering
-SCRIPT='configuration-script.sh'
-cat > ${WORKSPACE}/configuration-script.sh <<EOF
 
-   source configuration-functions.sh
+
+
+## Describe  the script run on guest vm (node) to configure clustering
+
+
+cat > ${WORKSPACE}/configuration-script.sh <<EOF
+   source /tmp/configuration-functions.sh
    # Functions used to edit akka.comf and module-shards
-   # editakkaconf \$1
-   # configuremoduleshardsconf \$1 
-   testme \$1 
+   editakkaconf \$1
+   configuremoduleshardsconf \$1 
+
 EOF
+
 
 
 # Describe CONFIGURATION FUNCTIONS  available for the  script above
 CONFIGURATIONFUNCTIONS='configuration-functions.sh'
-cat > ${WORKSPACE}/configuration-functions.sh <<EOF
-
-function testme
-{
-echo "lets see whats here"
-echo "\$1" 
-echo "\$2"
-ls
-ifconfig
-}
-
-EOF
-
-
 
 set -x
 for  i in "${!CONTROLLERIPS[@]}"
 do
    echo "IP address of node is: $i and index is   ${CONTROLLERIPS[$i]}"
-   scp -v ${WORKSPACE}/$SCRIPT  $CONTROLLERIP:/tmp
-   scp -v ${WORKSPACE}/$CONFIGURATIONFUNCTIONS $CONTROLLERIP:/tmp
+   scp -v ${WORKSPACE}/slave_addresses.txt  ${CONTROLLERIPS[$i]}:/tmp
+
+   scp -v ${WORKSPACE}/configuration-functions.sh ${CONTROLLERIPS[$i]}:/tmp
+   scp -v ${WORKSPACE}/configuration-script.sh    ${CONTROLLERIPS[$i]}:/tmp
+
    echo "configure controller $CONTROLLERIP on $i" 
-   ssh -v $CONTROLLERIP 'bash /tmp/$CONFIGURATIONFUNCTIONS'
-   ssh -v $CONTROLLERIP 'bash /tmp/$SCRIPT'
-  
+
+
+   ssh -v ${CONTROLLERIPS[$i]} 'bash /tmp/configuration-script.sh'  
 done
 set +x
-
 
 
 echo "######################################################"
