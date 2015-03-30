62b4608e35a5b81faae916e362615441bb719bd5
Richard Hill <ricjhill@gmail.com>
2015-03-30 23:43:52 +0200

    Escaped $ in HERE document
    Escaped $ in HERE document and added debug
    of output.
    
    Change-Id: I3e22d7249abb5596a8ff21947b7a80a1acf5efb7
    Signed-off-by: Richard  Hill <ricjhill@gmail.com>

--- jjb/integration/include-raw-integration-configuration_functions.sh
+++ jjb/integration/include-raw-integration-configuration_functions.sh
@@ -4,37 +4,39 @@ echo "#########################################################"
 
 # writes the  functions needed for configuring clustering to 
 # configuration-functions.sh  in the  WORKSPACE
+env
+
+set -x
 
 cat > ${WORKSPACE}/configuration-functions.sh  <<EOF
 
-# receives argument to GREP slaves address_txt e.g. 'CONTROLLER'. Expects slave_addresses.txt is available
+# Receives argument to GREP slaves address_txt e.g. 'CONTROLLER'. 
+# Expects slave_addresses.txt is available
+
 function getslaveaddresses 
 {
-  #set -x
-	if [ "\${1}"=='CONTROLLER' ] 
-        then
-	CONTROLLERIPS=(\$(cat slave_addresses.txt | grep \$1 | awk -F = '{print \$2}'))
-  #set +x
-        declare -p CONTROLLERIPS
-        fi
+   declare -a CONTROLLERIPS=($CONTROLLER0, $CONTROLLER1, $CONTROLLER2)
+   declare -p CONTROLLERIPS
+   export CONTROLLERIPS
 }
 
 
 function editakkaconf  
 {
-
+set -x
 # Expects 1 argument \$LOOPINCR. 
 # \$CONTROLLERIPS[] is set by getslaveaddresses(). 
 # \$BUNDLEFOLDER is set in "include-raw-integration-deploy-controller.sh" 
 # A single copy of akka.conf is assumed to be available in \$BUNDLEFOLDER
 
-if [ -z \${BUNDLEFOLDER} ] || [ -f \${BUNDLEFOLDER} ]; then
-    echo "Location of ODL BUNDLEFOLDER:\$BUNDLEFOLDER is not defined"
-    exit 1
+if [ -z ${BUNDLEFOLDER} ] || [ -f ${BUNDLEFOLDER} ]; then
+    echo "WARNING: Location of ODL BUNDLEFOLDER:\$BUNDLEFOLDER is not defined"
+
 fi
-if [ -z \${CONTROLLERIPS} ] || [ -f \${CONTROLLERIPS} ]; then
-    echo "Cluster IPs not known due to UNBOUND varible: CONTROLLERIPS[@] does not exist"
-    exit 1
+if [ -z \${CONTROLLERIPS} ]; then
+    echo "WARNING: Cluster IPs not known due to UNBOUND varible: CONTROLLERIPS[@] does not exist"
+    echo "WARNING: Calling getslaveaddresses to populate  CONTROLLERIPS[@]"
+    getslaveaddresses
 fi
 
 
@@ -44,24 +46,23 @@ fi
 # BEGIN function variables #
 ############################
 
-  # make local copies of global varibles because globals may produce interestying bugs.
+  # make local copies of global varibles because globals may produce interesting bugs.
 
   local LOOPINCR=\$1
-  local AKKACONF=\$(find \${BUNDLEFOLDER} -name akka.conf)
+  local AKKACONF=\$(find /tmp/${BUNDLEFOLDER} -name akka.conf)
   # set the IP of the current controller. Uses  \$CONTROLLERIPS[] and \$LOOPINCR
+
   local CURRENTCONTROLLERIPADDR=( "\${CONTROLLERIPS[\$1]}" )
-  # declare -p CURRENTCONTROLLERIPADDR 
-  local CONTROLLER0=( "\${CONTROLLERIPS[0]}" )  
-  local CONTROLLER1=( "\${CONTROLLERIPS[1]}" )  
-  local CONTROLLER2=( "\${CONTROLLERIPS[2]}" )  
+  declare -p CURRENTCONTROLLERIPADDR 
+  
   # used to verify IP address of current VM
   local HOSTIPADDR=\$(/sbin/ifconfig eth0 | grep "inet " | awk '{print \$2}' | awk '{print \$1}')
 
   local CLUSTERDATAORIG="\"akka.tcp:\/\/opendaylight-cluster-data@127.0.0.1:2550"\"
-  local CLUSTERDATANEW="\"akka.tcp:\/\/opendaylight-cluster-data@\$CONTROLLER0:2550\",\"akka.tcp:\/\/opendaylight-cluster-data@\$CONTROLLER1:2550\",\"akka.tcp:\/\/opendaylight-cluster-data@\$CONTROLLER2:2550"\"
+  local CLUSTERDATANEW="\"akka.tcp:\/\/opendaylight-cluster-data@$CONTROLLER0:2550\",\"akka.tcp:\/\/opendaylight-cluster-data@$CONTROLLER1:2550\",\"akka.tcp:\/\/opendaylight-cluster-data@$CONTROLLER2:2550"\"
 
   local CLUSTERRPCORIG="\"akka.tcp:\/\/odl-cluster-rpc@127.0.0.1:2551"\"
-  local CLUSTERRPCNEW="\"akka.tcp:\/\/odl-cluster-rpc@\$CONTROLLER0:2551\",\"akka.tcp:\/\/odl-cluster-rpc@\$CONTROLLER1:2551\",\"akka.tcp:\/\/odl-cluster-rpc@\$CONTROLLER2:2551\""
+  local CLUSTERRPCNEW="\"akka.tcp:\/\/odl-cluster-rpc@$CONTROLLER0:2551\",\"akka.tcp:\/\/odl-cluster-rpc@$CONTROLLER1:2551\",\"akka.tcp:\/\/odl-cluster-rpc@$CONTROLLER2:2551\""
 
   local UIDORIG="member-1"
   local UIDNEW="member-\$1"
@@ -77,16 +78,16 @@ fi
 
 #set -x
   if [ -z \${HOSTIPADDR} ]; then 
-    echo "HOSTIPADDR is empty."
+    echo "WARNING:  HOSTIPADDR is empty."
   fi
 
 #set +x
 
-  if [ "\${CURRENTCONTROLLERIPADDR}" = "\${HOSTIPADDR}" ] 
+  if [ "${CURRENTCONTROLLERIPADDR}" == "${HOSTIPADDR}" ] 
   then 
-    echo "CURRENTCONTROLLERIPADRR:\${CURRENTCONTROLLERIPADDR} == HOSTIPADDR:\${HOSTIPADDR}"
+    echo "CURRENTCONTROLLERIPADRR:${CURRENTCONTROLLERIPADDR} == HOSTIPADDR:${HOSTIPADDR}"
   else
-    echo "WARNING: CURRENTCONTROLLERIPADRR:\${CURRENTCONTROLLERIPADDR} != HOSTIPADDR:\${HOSTIPADDR}"
+    echo "WARNING: CURRENTCONTROLLERIPADRR:${CURRENTCONTROLLERIPADDR} != HOSTIPADDR:${HOSTIPADDR}"
   fi
 
 ####################################
@@ -94,7 +95,8 @@ fi
 ####################################
   
   cp \${AKKACONF} \${AKKACONF}.bak
-  sed -ri "s:hostname = \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\":hostname = \$CURRENTCONTROLLERIPADDR:" \${AKKACONF}.bak
+  sed -ri "s:hostname = \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\":hostname =
+\$CURRENTCONTROLLERIPADDR:" \${AKKACONF}.bak
 
 ####################################
 # configure  seednode in akka.conf #
@@ -110,40 +112,34 @@ fi
   sed -i "s/\$UIDORIG/\$UIDNEW/g" \${AKKACONF}.bak
   cp \${AKKACONF}.bak \${AKKACONF}
   cat \${AKKACONF}
-  
+set +x  
 }
 function configuremoduleshardsconf
 {
-  if [ -z \${BUNDLEFOLDER} ] || [ -f \${BUNDLEFOLDER} ]; then
-    echo "Location of ODL BUNDLEFOLDER:\$BUNDLEFOLDER is not defined"
-    exit 1
+  set -x
+  if [ -z ${BUNDLEFOLDER} ] || [ -f ${BUNDLEFOLDER} ]; then
+    echo "MODULESHARDSCONF WARNING:  Location of ODL BUNDLEFOLDER:\$BUNDLEFOLDER is not defined"
   fi
-  if [ -z \${CONTROLLERIPS} ] || [ -f \${CONTROLLERIPS} ]; then
-    echo "Cluster IPs not known due to UNBOUND varible: CONTROLLERIPS[@] does not exist"
-    exit 1
-  fi
-
 
   local REPLICACONFORIG='"member-1"'
   local REPLICACONFNEW='"member-0",\n\t\t\t"member-1",\n\t\t\t"member-2"'
   
 
-  MODULESHARDSCONF=\$(find \${BUNDLEFOLDER} -name module-shards.conf)
+  MODULESHARDSCONF=\$(find /tmp/${BUNDLEFOLDER} -name module-shards.conf)
 
   if [ -z \${MODULESHARDSCONF} ] 
   then
     printf "source file module-shards.conf  was not found\n"
-    exit 1
   else
     printf "MODULESHARDSCONF: \${MODULESHARDSCONF}"
-    cp \${MODULESHARDSCONF}  \${MODULESHARDSCONF}.bak 
+    cp \${MODULESHARDSCONF} \${MODULESHARDSCONF}.bak 
     printf \$REPLICACONFNEW
     sed -i "s/\$REPLICACONFORIG/\$REPLICACONFNEW/g" \${MODULESHARDSCONF}.bak 
 
     cp \${MODULESHARDSCONF}.bak  \${MODULESHARDSCONF} 
     cat \${MODULESHARDSCONF}
  fi  
- 
+ set +x
 }
 
 function deployandruncontrollerscript
@@ -173,7 +169,10 @@ function runcontrollerscript
 }
 
 EOF
+set +x
 
+
+#less configuration-functions.sh
 echo "############################################################"
 echo "## END include-raw-integration-configuration_functions.sh ##"
 echo "############################################################"
