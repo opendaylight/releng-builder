6619a5680d42f4c6cc2bf02dcd7407bd58e4294e
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-03-30 10:34:21 -0700

    Fix the vagrant definition for devstack
    The vagrant definition for devstack is supposed to be installing the
    Development Tools group but it was trying to do it as @development-tools
    when the group is @development
    
    Change-Id: Iecfe45ba6f05d22f3a5d0711dd56ba01e7f4287f
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- vagrant/ovsdb-devstack/bootstrap.sh
+++ vagrant/ovsdb-devstack/bootstrap.sh
@@ -9,15 +9,17 @@ sed -i 's/SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
 yum clean all
 yum update -y
 yum install -q -y deltarpm python{,-{crypto,devel,lxml,setuptools}} \
-	@development-tools {lib{xml2,xslt,ffi},openssl}-devel \
-	java git sudo
+    @development {lib{xml2,xslt,ffi},openssl}-devel \
+    java git sudo
 
 # figure out what the latest kernel installed is and switch to it
 # NOTE: This is done like this becase the Rackspace F20 images are using
 # extlinux / syslinux and don't switch to the newest kernel on update
 NEWKERNEL=`rpm -qa | grep kernel-3 | sort -r | head -1 | cut -c 8-`
-BOOTLABEL=`grep ${NEWKERNEL} /boot/extlinux.conf | grep LABEL | cut -c 7-`
-sed -i "s/ONTIMEOUT linux/ONTIMEOUT ${BOOTLABEL}/" /boot/extlinux.conf
+if [ -f /boot/extlinux.conf ]; then
+    BOOTLABEL=`grep ${NEWKERNEL} /boot/extlinux.conf | grep LABEL | cut -c 7-`
+    sed -i "s/ONTIMEOUT linux/ONTIMEOUT ${BOOTLABEL}/" /boot/extlinux.conf
+fi
 
 if [ ! -f /etc/udev/rules.d/80-net-setup-link.rules ]; then
     ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
@@ -26,3 +28,5 @@ fi
 echo "***************************************************"
 echo "*   PLEASE RELOAD THIS VAGRANT BOX BEFORE USE     *"
 echo "***************************************************"
+
+# vim: sw=4 ts=4 sts=4 et :
