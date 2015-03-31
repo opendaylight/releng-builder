03b2373bb2ccdf985dd4903955cfe2c869ff0a29
Richard Hill <ricjhill@gmail.com>
2015-03-31 15:06:56 +0200

    Populated ${CONTROLLERIPS[@]} in run tests shell
    Used slave_addresses.txt to source values of
    controller IPs and populate the CONTROLLERIPS
    array in the shell instance used to run robot
    tests.. Also added set -x debugging.
    
    Change-Id: I3225c4a4348b15b6214b96f323841dbd2923d6f5
    Signed-off-by: Richard  Hill <ricjhill@gmail.com>

--- jjb/integration/include-raw-integration-run-test.sh
+++ jjb/integration/include-raw-integration-run-test.sh
@@ -2,22 +2,28 @@ echo "###########################################"
 echo "##  include-raw-integration-run-test.sh  ##"
 echo "###########################################"
 # Expects $BUNDLEFOLDER to be set earlier in Jenkins job.
+set -x
 
   if [ -z ${BUNDLEFOLDER} ] || [ -f ${BUNDLEFOLDER} ]; then
     echo "Location of ODL BUNDLEFOLDER:$BUNDLEFOLDER is not defined"
     exit 1
   fi
 
+# populate $(CONTROLLERIPS)
+
+  declare CONTROLLERIPS=($(cat slave_addresses.txt | grep CONTROLLER | awk -F = '{print $2}'))
+  declare -p CONTROLLERIPS
 
 # Creates a script to run controller inside a dynamic jenkins slave
 
 cat > ${WORKSPACE}/run-test-controller-script.sh <<EOF
-
-
+set -x
 cd /tmp
+cd ${BUNDLEFOLDER}
+echo "BUNDLE: $BUNDLEFOLDER"
 echo "Checking status of controller..."
-
-    ./start &
+    bash bin/status & 
+    bash bin/start &
 echo "Waiting for controller to come up..."
 COUNT="0"
 while true; do
@@ -49,16 +55,16 @@ sleep 60
 
 echo "Checking OSGi bundles..."
 ./client 'bundle:list'
-
+set +x
 EOF
 
-set -x
+
 for  i in "${!CONTROLLERIPS[@]}"
 do
    echo "Running tests on ${CONTROLLERIPS[$i]} which is node $i "
    
-   scp ${WORKSPACE}/run-test-controller-script.sh ${CONTROLLER}$i:/tmp
-   ssh ${CONTROLLER}$i "bash /tmp/run-test-controller-script.sh $i"
+   scp ${WORKSPACE}/run-test-controller-script.sh ${CONTROLLERIPS[$i]}:/tmp
+   ssh ${CONTROLLERIPS[$i]} "bash /tmp/run-test-controller-script.sh $i"
    
 set +x   
 
@@ -75,8 +81,9 @@ SUITES=$( egrep -v '(^[[:space:]]*#|^[[:space:]]*$)' testplan.txt | tr '\012' ' 
 echo "Starting Robot test suites ${SUITES} ..."
 pybot -N ${TESTPLAN} -c critical -e exclude --loglevel TRACE -v BUNDLEFOLDER:${BUNDLEFOLDER} -v WORKSPACE:/tmp -v CONTROLLER:${CONTROLLER0} -v MININET:${MININET0} -v MININET_USER:${USER} -v USER_HOME:${HOME} ${TESTOPTIONS} ${SUITES}
 #
-#echo "Fetching Karaf log"
-#scp $CONTROLLER0:/tmp/$BUNDLEFOLDER/data/log/karaf.log .
+echo "Fetching Karaf log"
+scp $CONTROLLER0:/tmp/$BUNDLEFOLDER/data/log/karaf.log .
+cat karaf.log 
 #
 ## vim: ts=4 sw=4 sts=4 et ft=sh :
 echo "##############################################"
