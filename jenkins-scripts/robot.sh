0e595e083dba01e4b423e83fe1b2a43f73d5339d
Jamo Luhrsen <james.luhrsen@hp.com>
2015-04-03 11:08:01 -0700

    Installing Latest Requests and SSH Robot Libraries
    Change-Id: I2c8784bb26f95b03115175fb25b064904096dd81
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>

--- jenkins-scripts/robot.sh
+++ jenkins-scripts/robot.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 yum clean all
-yum install -y unzip
-yum install -y python-netaddr
+yum install -y -q unzip python-netaddr @development
+yum remove -y robotframework-{sshlibrary,requests}
+
+## install Latest Robot SSHLibrary from source
+cd /tmp
+wget http://pypi.python.org/packages/source/r/robotframework-sshlibrary/robotframework-sshlibrary-2.1.1.tar.gz > /dev/null 2>&1
+tar -xvf robotframework-sshlibrary-2.1.1.tar.gz > /dev/null 2>&1
+cd robotframework-sshlibrary-2.1.1
+python setup.py install > /dev/null 2>&1
+
+## install Latest Robot RequestsLibrary from source
+cd /tmp
+wget https://github.com/bulkan/robotframework-requests/archive/v0.3.8.tar.gz > /dev/null 2>&1
+tar -xvf v0.3.8.tar.gz > /dev/null 2>&1
+cd robotframework-requests-0.3.8/
+python setup.py install > /dev/null 2>&1
 
 # disable firewall rules
 service iptables stop
