481126b0d13df08dc5b618d0b216af0c271e889f
Flavio Fernandes <ffernand@redhat.com>
2015-04-09 22:32:21 -0400

    wip
    Change-Id: I828b42434a9183d34c0c294ab8ceea59e3baeaff
    Signed-off-by: Flavio Fernandes <ffernand@redhat.com>

---
+++ jjb/ovsdb-devel/include-raw-jjb-update.sh
@@ -0,0 +1,12 @@
+source /opt/virtualenv/jenkins-job-builder/bin/activate
+jenkins-jobs update --delete-old jjb/
+
+# Submit patches for any jobs that can be auto updated
+function submitJJB {
+    git commit -asm "Update automated project templates"
+    git push origin HEAD:refs/for/master
+}
+
+gitdir=$(git rev-parse --git-dir); scp -p -P 29418 jenkins-releng@git.opendaylight.org:hooks/commit-msg ${gitdir}/hooks/
+python scripts/jjb-autoupdate-project.py
+git diff --exit-code || submitJJB
