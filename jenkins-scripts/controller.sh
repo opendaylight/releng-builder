01361f662f11a8bf03ea088b26a3318a4b39e043
Luis Gomez <ecelgp@gmail.com>
2015-04-17 10:27:16 -0700

    Adding sshpass to centos after new karaf upgrade
    New Karaf upgrade requires password for bin/client command
    
    Change-Id: Ie2edf2ef0748563db886c82c33b5e1fab0479b3a
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- jenkins-scripts/controller.sh
+++ jenkins-scripts/controller.sh
@@ -3,4 +3,8 @@
 # disable the firewall
 service iptables stop
 
+# install sshpass
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/sshpass-1.05-1.el6.x86_64.rpm
+yum install -y sshpass-1.05-1.el6.x86_64.rpm
+
 # vim: sw=2 ts=2 sts=2 et :
