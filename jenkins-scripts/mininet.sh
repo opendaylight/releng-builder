abeb0329c6059e29de895e889d7d53a7e8047f4a
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-04-22 13:39:36 -0700

    Update the spin-up script for mininet systems
    Netopeer and CPqD aren't properly installed on the base image. Fix the
    installation
    
    Change-Id: I3b33d41f9bc112ec04ce2ff1c9646c8c975511ea
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- jenkins-scripts/mininet.sh
+++ jenkins-scripts/mininet.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# for whatever reason netopeer & CPqD aren't installed (they weren't in
+# one of the yum repos we were hooked up to when the base image was
+# built, they are now. Make sure they're install
+yum install -q -y netopeer-server-sl CPqD-ofsoftswitch13
+
+# the vagrant configuration for netopeer doesn't configure SSH correctly
+# as it uses and here document via echo and not cat fix that
+cat << EOSSH >> /etc/ssh/sshd_config
+
+# Added for netconf / netopeer testing
+Port 22
+Port 830
+Subsystem netconf /usr/bin/netopeer-server-sl
+EOSSH
+
+# sshd has to get a restart because of the above
+service sshd restart
+
+# found out while doing testing to fix netopeer that the selinux perms
+# aren't set correctly (thanks Rackspace for having an EL6 image that
+# didn't have selinux on at first!) fix it so that the password can be
+# set
+/sbin/restorecon -R /etc
+
 # According to Luis in RT7956 the controller SSH capabilities require
 # that for NETCONF it uses a password (how broken!) So we're going to
 # force a password onto the jenkins user
