c55d8da36ec9062dbab25b0c5299262bf7ce2f9d
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-04-27 16:25:05 -0400

    Fix autorelease maven deploy build step
    Need to cleanup $m2repo/org/opendaylight in case we pull in build
    artifacts from a previous build on the slave.
    
    Only deploy projects that are built and ignore all other projects.
    
    Change-Id: I7aadbb3edf644e4ddfe7090f63ab5dad265b23fc
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/autorelease/include-raw-autorelease-maven-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2015 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+mkdir -p hide/from/pom/files
+cd hide/from/pom/files
+mkdir -p m2repo/org/opendaylight/
+
+(IFS='
+'
+for m in `xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 -t -m '//x:modules' -v '//x:module' ../../../../pom.xml`; do
+    mv "/tmp/r/org/opendaylight/$m" m2repo/org/opendaylight/
+done)
+
+mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.2:deploy-staged-repository -DrepositoryDirectory="`pwd`/m2repo" -DnexusUrl=http://nexus.opendaylight.org/ -DstagingProfileId="21a27b7f3bbb8d" -DserverId="opendaylight.weekly" -s $AUTORELEASE_SETTINGS -gs $ODL_GLOBAL_SETTINGS
