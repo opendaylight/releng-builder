93db7ef8ff0ebf7875ee468278f93b742f51c2c9
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-05-13 06:01:12 -0700

    Port Change #19894 into Jenkins spin-up
    Change 19894 was applied against the vagrant build scripts. To get this
    into use faster let's port it over to the Jenkins spin-up scripts for
    the moment.
    
    Change-Id: I7ce998554608e8781686911f16649a449bd6578c
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- jenkins-scripts/builder.sh
+++ jenkins-scripts/builder.sh
@@ -3,4 +3,16 @@
 yum clean all
 yum install -y python-{tox,virtualenv} xmlstarlet
 
+# add in mono components for VTN
+yum install -y yum-utils
+#The following is needed for the new code in vtn project.
+#these packages will enable C# compilation.
+rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+#Added the mono tools repository
+yum-config-manager -q -y --add-repo http://download.mono-project.com/repo/centos6
+#Install the nuget binary
+yum install -q -y http://download.mono-project.com/repo/centos/RPMS/noarch/nuget-2.8.3+md58+dhx1-0.noarch.rpm
+#install the mono toolchain
+yum -q -y install mono-complete
+
 # vim: sw=2 ts=2 sts=2 et :
