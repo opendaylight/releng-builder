fe95b0a5fead002e9dc3078d7e13c1dfb07d0c69
Vratko Polak <vrpolak@cisco.com>
2015-05-19 13:59:58 +0200

    JJB init script: Remove superfluous lines from autogenerated files
    Change-Id: I85f59b1c72a9d471c460f4d0e63177cb959f2fef
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- scripts/jjb-init-project.py
+++ scripts/jjb-init-project.py
@@ -194,5 +194,8 @@ with open(project_file, "w") as outfile:
                     line = re.sub("EMAIL_PREFIX", email_prefix, line)
                     line = re.sub("SONAR_BRANCH", sonar_branch, line)
                     line = re.sub("ARCHIVE_ARTIFACTS", archive_artifacts, line)
+                    # The previous command may have created superfluous lines.
+                    # If a line has no non-whitespace, it has to be '\n' only.
+                    line = re.sub(r'^\s+\n', "", line)
                 outfile.write(line)
         outfile.write("\n")
