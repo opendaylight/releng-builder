b984eb14649f4372f4b77d89a64ff50d2ec0340b
Jamo Luhrsen <james.luhrsen@hp.com>
2015-05-20 15:23:09 -0700

    installing netaddr and ipaddress python libs
    --  does "pip install" work?  no other examples of it,
    so not sure.
    
    Change-Id: I9f50ee66e4ea85b0a52fb97bef75d37db9e864e9
    Signed-off-by: Jamo Luhrsen <james.luhrsen@hp.com>

--- jenkins-scripts/mininet.sh
+++ jenkins-scripts/mininet.sh
@@ -11,6 +11,11 @@ EOF
 # built, they are now. Make sure they're install
 yum install -q -y netopeer-server-sl CPqD-ofsoftswitch13
 
+# netaddr and ipaddress libraries can be useful on this system as 
+# some tests are starting to push pyhon scripts/tools to this VM
+# during CI tests
+yum install -q -y python-{ipaddress,netaddr}
+
 # the vagrant configuration for netopeer doesn't configure SSH correctly
 # as it uses and here document via echo and not cat fix that
 cat << EOSSH >> /etc/ssh/sshd_config
