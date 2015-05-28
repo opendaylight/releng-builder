ed0956613d8c2122cff4f24e8b15494e6bd14dbd
gvrangan <venkatrangang@hcl.com>
2015-05-28 19:55:42 +0900

    Fixed URL for nuget and mono installation
    Change-Id: Ie2d74ef0c9b86ee6c72c0b53252e55a91e2c1a1d
    Signed-off-by: gvrangan <venkatrangang@hcl.com>

--- vagrant/basic-builder/bootstrap.sh
+++ vagrant/basic-builder/bootstrap.sh
@@ -30,8 +30,8 @@ yum install -q -y {jansson,libevent,libnl,libuuid}-devel \
 #these packages will enable C# compilation.
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 #Added the mono tools repository
-yum-config-manager -q -y --add-repo http://download.mono-project.com/repo/centos6
+yum-config-manager -q -y --add-repo http://origin-download.mono-project.com/repo/centos6/
 #Install the nuget binary
-yum install -q -y http://download.mono-project.com/repo/centos/RPMS/noarch/nuget-2.8.3+md58+dhx1-0.noarch.rpm
+yum install -q -y http://origin-download.mono-project.com/repo/centos/n/nuget/nuget-2.8.3+md58+dhx1-0.noarch.rpm
 #install the mono toolchain
 yum -q -y install mono-complete
