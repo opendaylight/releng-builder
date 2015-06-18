0d50154484cc68b311676b0bc3d323f088560eb3
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-06-18 12:22:03 -0400

    Add JJB file for OVSDB netvirt
    Change-Id: I881ce2652d1905c38e88dc31b78e4007ca65d071
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

---
+++ jjb/ovsdb/include_raw_l3_forwarding.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'Enable l3 forwarding'
+echo 'ovsdb.l3.fwd.enabled=yes' >> $WORKSPACE/target/assembly/etc/custom.properties
+echo 'Add l3 gateway'
+echo 'ovsdb.l3gateway.mac=00:00:5E:00:02:01' >> $WORKSPACE/target/assembly/etc/custom.properties
