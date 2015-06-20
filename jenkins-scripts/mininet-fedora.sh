be59b8342bd7c89547dd5a1c90df3fe28ab75527
Luis Gomez <ecelgp@gmail.com>
2015-06-20 15:12:05 -0700

    Disable Firewall and SELinux in f21 mininet VM
    We need this for OVSDB test cases
    
    Change-Id: I41d327938f28ccd0bae74a217de166905e39c776
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- jenkins-scripts/mininet-fedora.sh
+++ jenkins-scripts/mininet-fedora.sh
@@ -9,4 +9,9 @@ EOF
 # make sure the firewall is stopped
 service iptables stop
 
+# disable vm security
+/usr/sbin/setenforce 0
+systemctl disable firewalld
+systemctl stop firewalld
+
 # vim: sw=2 ts=2 sts=2 et :
