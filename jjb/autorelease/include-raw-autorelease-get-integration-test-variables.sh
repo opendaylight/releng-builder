64e331d88342e16a9512efd820117588e5fca37a
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-09-04 11:09:55 -0400

    Run integration-test job post-build
    WIP
    
    - Still need to actually kick off the integration-test job
    
    Change-Id: I68690d66829d345dfc0ef0a23fea22ebe9f0eae5
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/autorelease/include-raw-autorelease-get-integration-test-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2015 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+NEXUSURL=http://nexus.opendaylight.org/content/repositories/
+VERSION=`grep -m1 '<version>' ${WORKSPACE}/integration/distribution/distributions/karaf/pom.xml | awk -F'[<|>]' '/version/ { printf $3 }'`
+REPOID=`grep "Closing staging repository with ID" $WORKSPACE/deploy-staged-repository.log | cut -d '"' -f2`
+
+echo BUNDLEURL=${NEXUSURL}/${REPOID}/org/opendaylight/integration/distribution-karaf/${VERSION}/distribution-karaf-${VERSION}.zip > $WORKSPACE/variables.txt
