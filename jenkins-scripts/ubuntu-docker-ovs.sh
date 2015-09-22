acdcc5ce8da02e6bf68f4fec1a45709bf9faaa6f
Tomas Cechvala <tcechval@cisco.com>
2015-09-22 17:21:09 +0200

    GBP Vagrant setup for Ubuntu VM
    This is based on vagrant/ubuntu
    Bootstrap involves Docker and OVS installation
    
    Change-Id: I91d1d42d95e257912c767c004bd79c4b68c4e6e6
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

---
+++ jenkins-scripts/ubuntu-docker-ovs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install virtualenv
+apt-get install python-virtualenv
+
+# add user jenkins to docker group
+/usr/sbin/usermod -a -G docker jenkins
+
+# pull docker images
+docker pull alagalah/odlpoc_ovs230
+
+# vim: sw=2 ts=2 sts=2 et :
+
