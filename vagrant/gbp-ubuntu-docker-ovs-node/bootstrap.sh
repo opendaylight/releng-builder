f001735bc0ac20373329908f1bcd66ab63ef86a5
Tomas Cechvala <tcechval@cisco.com>
2015-09-22 17:21:09 +0200

    GBP Vagrant setup for Ubuntu VM
    This is based on vagrant/ubuntu
    Bootstrap involves Docker and OVS installation
    
    Change-Id: I91d1d42d95e257912c767c004bd79c4b68c4e6e6
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

---
+++ vagrant/gbp-ubuntu-docker-ovs-node/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# vim: sw=4 ts=4 sts=4 et tw=72 :
+
+echo "---> Updating operating system"
+apt-get update -qq
+apt-get upgrade -y --force-yes -qq
+
+echo "---> Installing Group Based Policy requirements"
+apt-get install -y software-properties-common -qq
+apt-get install -y python-software-properties -qq
+apt-get install -y python-pip -qq
+apt-get install -y git-core git -qq
+apt-get install -y curl -qq
+# docker
+curl -sSL https://get.docker.com/ | sh
+docker pull alagalah/odlpoc_ovs230
+# OVS
+curl https://raw.githubusercontent.com/pritesh/ovs/nsh-v8/third-party/start-ovs-deb.sh | bash
