6b44b6a364e5a6518c7f1856614cfb43a3ceb9d2
Jamo Luhrsen <jluhrsen@redhat.com>
2015-09-29 09:01:33 -0700

    adding sshuttle to sudo command list for jenkins user
    Change-Id: Ib9a92e04087febc7bab503ecdc401845e688c579
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jenkins-scripts/robot.sh
+++ jenkins-scripts/robot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# make sure we don't require tty for sudo operations
+cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
+Defaults:jenkins !requiretty
+jenkins     ALL = NOPASSWD: /usr/bin/sshuttle
+EOF
+
 yum clean all
 yum install -y -q unzip python-netaddr @development
 yum remove -y robotframework-{sshlibrary,requests}
