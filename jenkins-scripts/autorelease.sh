6afeb834207a9a616a74153d07560efc87ff32e9
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-10-06 18:49:02 -0400

    Fix workspace path to /w/workspace
    Change-Id: I307b0653526d8006ac14118ab69237ebdd0c19a6
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/autorelease.sh
+++ jenkins-scripts/autorelease.sh
@@ -2,6 +2,6 @@
 
 # Format and mount rackspace data disk
 mkfs -t ext3 /dev/xvde1
-mkdir /opt/jenkins/workspace
-mount /dev/xvde1 /opt/jenkins/workspace
-chown jenkins:jenkins /opt/jenkins/workspace
+mkdir /w/workspace
+mount /dev/xvde1 /w/workspace
+chown jenkins:jenkins /w/workspace
