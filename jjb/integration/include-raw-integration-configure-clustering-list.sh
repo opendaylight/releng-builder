eac62e1bf998271a043b47968eaad4d756a46e17
Shaleen Saxena <ssaxena@brocade.com>
2015-10-23 16:52:53 -0400

    Configure clustering on a list of controllers.
    Adding a new script called include-raw-integration-configure-clustering-list.sh,
    which is a list version of include-raw-integration-configure-clustering.sh.
    The older script worked for exactly 3 controllers. The new script takes a list of
    controllers using an environment variable called CONTROLLER_LIST. This would
    have a comma-separated list of controllers. This allows any arbitrary number
    of controllers to be configured as part of a cluster.
    
    Change-Id: Iaebf8fdedc0998974733c896de97d51bbaf3e452
    Signed-off-by: Shaleen Saxena <ssaxena@brocade.com>

---
+++ jjb/integration/include-raw-integration-configure-clustering-list.sh
@@ -0,0 +1,112 @@
+# (C) 2015 Brocade Communications Systems, Inc.
+# 130 Holger Way, San Jose, CA 95134.
+# All rights reserved.
+#
+# Brocade, the B-wing symbol, Brocade Assurance, ADX, AnyIO, DCX, Fabric OS,
+# FastIron, HyperEdge, ICX, MLX, MyBrocade, NetIron, OpenScript, VCS, VDX, and
+# Vyatta are registered trademarks, and The Effortless Network and the On-Demand
+# Data Center are trademarks of Brocade Communications Systems, Inc., in the
+# United States and in other countries. Other brands and product names mentioned
+# may be trademarks of others.
+#
+# Use of the software files and documentation is subject to license terms.
+
+
+echo "#################################################"
+echo "##         Configure Cluster and Start         ##"
+echo "#################################################"
+
+AKKACONF=/tmp/${BUNDLEFOLDER}/configuration/initial/akka.conf
+MODULESCONF=/tmp/${BUNDLEFOLDER}/configuration/initial/modules.conf
+MODULESHARDSCONF=/tmp/${BUNDLEFOLDER}/configuration/initial/module-shards.conf
+JOLOKIACONF=/tmp/${BUNDLEFOLDER}/deploy/jolokia.xml
+
+# Create the list of controllers from the CONTROLLER_LIST variable
+CONTROLLERIPS=( ${CONTROLLER_LIST//,/ } )
+
+# Utility function for joining strings.
+function join {
+    delim=',\n\t\t'
+    final=$1; shift
+
+    for str in $* ; do
+	final=${final}${delim}${str}
+    done
+
+    echo ${final}
+}
+
+# Create the strings for data and rpc seed nodes
+# First create various arrays with one element per controller.
+# Then merge each array using the join utility defined above.
+count=1
+for ip in ${CONTROLLERIPS[@]} ; do
+   data_seed_nodes[$count]=\\\"akka.tcp:\\/\\/opendaylight-cluster-data@${ip}:2550\\\"
+   rpc_seed_nodes[$count]=\\\"akka.tcp:\\/\\/odl-cluster-rpc@${ip}:2551\\\"
+   member_names[$count]=\\\"member-${count}\\\"
+   count=$[count + 1]
+done
+data_seed_list=$(join ${data_seed_nodes[@]})
+rpc_seed_list=$(join ${rpc_seed_nodes[@]})
+member_name_list=$(join ${member_names[@]})
+
+# echo ${CONTROLLERLIST}
+# echo ${CONTROLLERIPS[@]}
+# echo ${data_seed_list}
+# echo ${rpc_seed_list}
+# echo ${member_name_list}
+
+# Create the configuration script to be run on controllers.
+cat > ${WORKSPACE}/configuration-script.sh <<EOF
+
+CONTROLLERID="member-\$1"
+CONTROLLERIP=\$2
+
+echo "Configuring hostname in akka.conf"
+sed -i -e "s:{{HOST}}:\${CONTROLLERIP}:" ${AKKACONF}
+
+echo "Configuring data seed nodes in akka.conf"
+sed -i -e "s/{{{DS_SEED_NODES}}}/[${data_seed_list}]/g" ${AKKACONF}
+
+echo "Configuring rpc seed nodes in akka.conf"
+sed -i -e "s/{{{RPC_SEED_NODES}}}/[${rpc_seed_list}]/g" ${AKKACONF}
+
+echo "Define unique name in akka.conf"
+sed -i -e "s/{{MEMBER_NAME}}/\${CONTROLLERID}/g" ${AKKACONF}
+
+echo "Define replication type in module-shards.conf"
+sed -i -e "s/{{{REPLICAS_1}}}/[${member_name_list}]/g" ${MODULESHARDSCONF}
+sed -i -e "s/{{{REPLICAS_2}}}/[${member_name_list}]/g" ${MODULESHARDSCONF}
+sed -i -e "s/{{{REPLICAS_3}}}/[${member_name_list}]/g" ${MODULESHARDSCONF}
+sed -i -e "s/{{{REPLICAS_4}}}/[${member_name_list}]/g" ${MODULESHARDSCONF}
+
+echo "Dump akka.conf"
+cat ${AKKACONF}
+
+echo "Dump modules.conf"
+cat ${MODULESCONF}
+
+echo "Dump module-shards.conf"
+cat ${MODULESHARDSCONF}
+
+echo "Starting controller..."
+/tmp/${BUNDLEFOLDER}/bin/start
+
+EOF
+
+# Copy over the configuration script and configuration files to each controller
+# Execute the configuration script on each controller.
+for i in "${!CONTROLLERIPS[@]}"
+do
+    echo "Configuring member-$((i+1)) with IP address ${CONTROLLERIPS[$i]}"
+    ssh ${CONTROLLERIPS[$i]} "mkdir /tmp/${BUNDLEFOLDER}/configuration/initial"
+    scp  ${WORKSPACE}/test/tools/clustering/cluster-deployer/templates/openflow/akka.conf.template ${CONTROLLERIPS[$i]}:${AKKACONF}
+    scp  ${WORKSPACE}/test/tools/clustering/cluster-deployer/templates/openflow/modules.conf.template ${CONTROLLERIPS[$i]}:${MODULESCONF}
+    scp  ${WORKSPACE}/test/tools/clustering/cluster-deployer/templates/openflow/module-shards.conf.template ${CONTROLLERIPS[$i]}:${MODULESHARDSCONF}
+    scp  ${WORKSPACE}/test/tools/clustering/cluster-deployer/templates/openflow/jolokia.xml.template ${CONTROLLERIPS[$i]}:${JOLOKIACONF}
+    scp  ${WORKSPACE}/configuration-script.sh    ${CONTROLLERIPS[$i]}:/tmp/
+    ssh ${CONTROLLERIPS[$i]} "bash /tmp/configuration-script.sh $((i+1)) ${CONTROLLERIPS[$i]}"
+done
+
+# vim: ts=4 sw=4 sts=4 et ft=sh :
+
