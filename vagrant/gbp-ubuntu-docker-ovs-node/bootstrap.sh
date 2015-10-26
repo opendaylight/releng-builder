0eb5a54ea4af92bfda565ff26121232bf3dbd537
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-10-26 08:35:45 -0700

    Vagrant and start-up script updates for GBP image
    This change is a working setup for the image that is proposed by GBP for
    testing purposes.
    
    NOTE: Due to snapshots refusing to connect to the network after OVS is
    installed but not having an issue once the system is up and OVS is
    installed, the final OVS installation / statup is left as a step to
    happen in the jenkins-script. That change is part of this submission.
    
    Change-Id: I0cd2861d6e8cfe50538a9064eb30ca44b9d70153
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- vagrant/gbp-ubuntu-docker-ovs-node/bootstrap.sh
+++ vagrant/gbp-ubuntu-docker-ovs-node/bootstrap.sh
@@ -6,27 +6,47 @@ echo "---> Updating operating system"
 apt-get update -qq
 apt-get upgrade -y --force-yes -qq
 
-echo "---> Installing Group Based Policy requirements"
-apt-get install -y software-properties-common -qq
-apt-get install -y python-software-properties -qq
-apt-get install -y python-pip -qq
-apt-get install -y python-virtualenv -qq
-apt-get install -y git-core git -qq
-apt-get install -y curl -qq
-apt-get install -y bridge-utils -qq
+# we need puppetlabs-apt in our puppet manifest
+#puppet module install puppetlabs-apt --version 2.2.0
+puppet module install garethr-docker --version 4.1.1
 
-# docker
-curl -sSL https://get.docker.com/ | sh
+# do the package install via puppet so that we know it actually installs
+# properly and it also makes it quieter but with better error reporting
+echo "---> Installing Group Based Policy requirements"
+puppet apply /vagrant/gbp_packages.pp
 
 # configure docker networking so that it does not conflict with LF internal networks
 # configure docker daemon to listen on port 5555 enabling remote managment
+# This has to happen before docker gets installed or things go sideways
+# badly
 cat <<EOL > /etc/default/docker
 # /etc/default/docker
 DOCKER_OPTS='-H unix:///var/run/docker.sock -H tcp://0.0.0.0:5555 --bip=10.250.0.254/24'
 EOL
-ip link set dev docker0 down
-brctl delbr docker0
-restart docker
+
+# docker
+echo "---> Installing docker"
+puppet apply /vagrant/docker_setup.pp
+
+echo "---> stopping docker"
+puppet apply -e "service { 'docker': ensure => stopped }"
+
+echo "---> cleaning docker configs that break after snapshotting"
+rm -f /var/lib/docker/repositories-aufs /etc/docker/key.json
 
 # OVS
-curl https://raw.githubusercontent.com/pritesh/ovs/nsh-v8/third-party/start-ovs-deb.sh | bash
+echo "---> Installing ovs"
+puppet module install puppetlabs-vcsrepo
+puppet apply /vagrant/ovs_setup.pp
+
+pushd /root/ovs
+DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary | \
+ grep 'dpkg-deb: building package'
+popd
+
+# Note this does not actually install OVS. Everytime we've tried to do
+# that the snapshot system hangs on spin-up for some reason. As such the
+# final installation will have to be left as a spin-up task
+# configure a root level cronjob to deal with the final install on boot
+# The following is what should be used in the spin-up task
+# dpkg --install /root/openvswitch-datapath-dkms* && dpkg --install /root/openvswitch-{common,switch}*
