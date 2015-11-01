55dc4a200a7adcff45dae5b22ff8b2ac8ff30b8c
Tomas Cechvala <tcechval@cisco.com>
2015-11-01 22:22:56 +0100

    NOPASSWD for Jenkins on Ubuntu VM
    Enabling sudo within a script.
    NOPASSWD for particular commands.
    
    Change-Id: I788752954aead8d63bb64346071ee1d77725817a
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

--- jenkins-scripts/ubuntu-docker-ovs.sh
+++ jenkins-scripts/ubuntu-docker-ovs.sh
@@ -11,5 +11,16 @@ dpkg --install /root/openvswitch-{common,switch}*
 # pull docker images
 docker pull alagalah/odlpoc_ovs230
 
+
+# make sure we don't require tty for sudo operations
+cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
+Defaults:jenkins !requiretty
+jenkins     ALL=NOPASSWD: /usr/bin/ovs-vsctl
+jenkins     ALL=NOPASSWD: /usr/bin/ovs-ofctl
+jenkins     ALL=NOPASSWD: /usr/bin/ovs-dpctl
+jenkins     ALL=NOPASSWD: /sbin/ip
+jenkins     ALL=NOPASSWD: /var/run/netns 
+EOF
+
 # vim: sw=2 ts=2 sts=2 et :
 
