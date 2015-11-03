5c1555a716212c17089a65b13f10300db7b8ca78
Tomas Cechvala <tcechval@cisco.com>
2015-11-03 13:58:50 +0100

    Nopasswd for more commands at gbp-trusty
    Based on testing in Jenkins Sandbox
    Added paths for mkdir, rm, ln
    
    Change-Id: I661f731c2b8976184c4fe52752a449b3f6436371
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

--- jenkins-scripts/ubuntu-docker-ovs.sh
+++ jenkins-scripts/ubuntu-docker-ovs.sh
@@ -20,6 +20,9 @@ jenkins     ALL=NOPASSWD: /usr/bin/ovs-ofctl
 jenkins     ALL=NOPASSWD: /usr/bin/ovs-dpctl
 jenkins     ALL=NOPASSWD: /sbin/ip
 jenkins     ALL=NOPASSWD: /var/run/netns
+jenkins     ALL=NOPASSWD: /bin/mkdir
+jenkins     ALL=NOPASSWD: /bin/ln
+jenkins     ALL=NOPASSWD: /bin/rm
 EOF
 
 # vim: sw=2 ts=2 sts=2 et :
