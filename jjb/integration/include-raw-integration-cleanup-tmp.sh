c1e871f867d541e252e15bcd5e3cf1150da930e5
Vratko Polak <vrpolak@cisco.com>
2015-11-05 12:11:03 +0100

    Archive karaf logs compressed by xz
    Log setting changed toallow karaf.log to have size 5GB
    as compression will make the resulting artifacts small enough.
    
    Lines where karaf.log was "cat" to console were replaced
    with "head" printing only first million bytes.
    
    Change-Id: Iaf7be3716018b6982bf703b4814263f0d55e2692
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- jjb/integration/include-raw-integration-cleanup-tmp.sh
+++ jjb/integration/include-raw-integration-cleanup-tmp.sh
@@ -2,3 +2,8 @@
 # ${ROBOT_VENV} comes from the include-raw-integration-install-robotframework.sh
 # script.
 rm -rf ${ROBOT_VENV}
+
+# Cleanup workspace.
+# Leftover files from previous runs can be wrongly copied as results.
+# TODO: Should this be a separate script/macro?
+rm -rf .
