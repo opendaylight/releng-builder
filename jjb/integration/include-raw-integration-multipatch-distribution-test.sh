694f76f42255ea4372cda36319ef876237a92ac4
Vratko Polak <vrpolak@cisco.com>
2015-11-27 10:09:25 +0100

    Multipatch build script: Add support for cherry-picks
    The old format with colon now performs cherry pick.
    New format with equal sign means checkout.
    Neither checkout nor cherry-pick is required.
    Multiple cherry-picks can applied to the same project.
    At most one checkout is applied,
    has to be specified before cherry picks.
    
    This still only works with master branches
    (or whatever is implied by the checkout code).
    
    Additional changes: Provide {yangttols-settings}
    as otherwise testInvalidVersion unit test fails.
    Java memory parameters increased.
    
    Change-Id: I07030232a381ee1049370d6c7783fd35726fbb76
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- jjb/integration/include-raw-integration-multipatch-distribution-test.sh
+++ jjb/integration/include-raw-integration-multipatch-distribution-test.sh
@@ -15,46 +15,59 @@ echo "<modelVersion>4.0.0</modelVersion>" >> $POM_FILE
 echo "<packaging>pom</packaging>" >> $POM_FILE
 echo "<modules>" >> $POM_FILE
 
+# TODO: Is "patches" still the correct word?
 IFS=',' read -ra PATCHES <<< "${PATCHES_TO_BUILD}"
 
-# for each patch, clone the project, and fetch/checkout the patch set.
-# each patch is found in the ${PATCHES_TO_BUILD} variable as a comma
-# separated list of project:changeset values
-# Example:  PATCHES_TO_BUILD=snmp:97/27997/1,tcpmd5:91/26391/3
+# For each patch, clone the project.
+# Optionally checkout a specific patch set.
+# Also optionally, cherry-pick series of patch sets.
+# Each patch is found in the ${PATCHES_TO_BUILD} variable
+# as a comma separated list of project[=checkout][:cherry-pick]* values
 #
-# TODO: this version will not handle multiple patches from the same project and will be
-#       done at a later time.  cherry-picking will be needed, with more complex logic
+# Example:  PATCHES_TO_BUILD='odlparent=45/30045/2,yangtools:53/26853/25,mdsal,controller=61/29761/5:45/29645/6,bgpcep:39/30239/1:59/30059/2'
 #
-distribution_status="not_patched"
-for i in "${PATCHES[@]}"
+distribution_status="not_included"
+for patch_code in "${PATCHES[@]}"
 do
-    echo "working on ${i}"
-    PROJECT=`echo ${i} | cut -d\: -f 1`
+    echo "working on ${patch_code}"
+    PROJECT=`echo ${patch_code} | cut -d\: -f 1 | cut -d\= -f 1`
     if [ "${PROJECT}" == "integration/distribution" ]; then
-        distribution_status="patched"
+        distribution_status="included"
     fi
     PROJECT_SHORTNAME="${PROJECT##*/}"  # http://stackoverflow.com/a/3162500
-    PATCH=`echo ${i} | cut -d\: -f 2`
-    echo "<module>${PROJECT_SHORTNAME}</module>" >> $POM_FILE
-    echo "cloning ${PROJECT} and checking out ${PATCH}"
+    echo "cloning project ${PROJECT}"
     git clone https://git.opendaylight.org/gerrit/p/${PROJECT}
+    echo "<module>${PROJECT_SHORTNAME}</module>" >> ${POM_FILE}
     cd ${PROJECT_SHORTNAME}
-    git fetch https://git.opendaylight.org/gerrit/${PROJECT} refs/changes/${PATCH}
-    git checkout FETCH_HEAD
-    cd $BUILD_DIR
-
+    # TODO: Add non-master branch support here.
+    CHECKOUT=`echo ${patch_code} | cut -d\= -s -f 2 | cut -d\: -f 1`
+    if [ "x${CHECKOUT}" != "x" ]; then
+        echo "checking out ${CHECKOUT}"
+        git fetch "https://git.opendaylight.org/gerrit/${PROJECT}" "refs/changes/$CHECKOUT"
+        git checkout FETCH_HEAD
+    fi
+    PICK_SEGMENT=`echo "${patch_code}" | cut -d\: -s -f 2-`
+    IFS=':' read -ra PICKS <<< "${PICK_SEGMENT}"
+    for pick in "${PICKS[@]}"
+    do
+        echo "cherry-picking ${pick}"
+        git fetch "https://git.opendaylight.org/gerrit/${PROJECT}" "refs/changes/${pick}"
+        git cherry-pick --no-commit FETCH_HEAD
+    done
+    cd ${BUILD_DIR}
 done
 
-if [ "${distribution_status}" == "not_patched" ]; then
+if [ "${distribution_status}" == "not_included" ]; then
+    echo "adding integration/distribution"
     # clone distribution and add it as a module in root pom
     git clone https://git.opendaylight.org/gerrit/p/integration/distribution
-    # TODO: Should we add support for non-master distribution branches?
-    echo "<module>distribution</module>" >> $POM_FILE
+    # FIXME: Unify with Change 30105 to add support for non-master distribution branches.
+    echo "<module>distribution</module>" >> ${POM_FILE}
 fi
 
 # finish pom file
-echo "</modules>" >> $POM_FILE
-echo "</project>" >> $POM_FILE
+echo "</modules>" >> ${POM_FILE}
+echo "</project>" >> ${POM_FILE}
 
 # Extract the BUNDLEVERSION from the distribution pom.xml
 BUNDLEVERSION=`xpath $BUILD_DIR/distribution/pom.xml '/project/version/text()' 2> /dev/null`
