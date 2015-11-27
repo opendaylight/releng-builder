848b74ea9bf241e44d60c235231659e0aabecb02
Vratko Polak <vrpolak@cisco.com>
2015-11-27 15:31:17 +0100

    Make test-integration to rebase before build
    This is to resemble state at future merge more closely.
    
    Change-Id: I562ca5487413ba389640ecc644dff08b0e1891c4
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ jjb/integration/include-raw-integration-rebase-gerrit-patch.sh
@@ -0,0 +1,11 @@
+# Make sure changes merged after patch creation are applied.
+
+set -exu
+echo "Rebase the patch on top of ${GERRIT_PROJECT}"
+cd "${GERRIT_PROJECT}"
+export EMAIL="sandbox@jenkins.opendaylight.org"
+# TODO: Is there a more appropriate e-mail?
+export GIT_COMMITTER_NAME="Rebase Macro"
+git checkout "${BRANCH}"
+git rebase "${GERRIT_REFSPEC}"
+cd "${WORKSPACE}"
