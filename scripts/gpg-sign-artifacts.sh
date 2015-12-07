db6e3eba90ff565dec38e30bfb71027cb3c3dc0d
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-06 21:26:01 -0500

    Add script for gpg signing artifacts
    Change-Id: Ic422dda93bbec234dad1f5413c4c931f856b66af
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ scripts/gpg-sign-artifacts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2015 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+for f in `find . -type f ! -name "*.sha1" ! -name "*.md5"`
+do
+    echo "Signing $f"
+    gpg --batch -abq $f
+done
