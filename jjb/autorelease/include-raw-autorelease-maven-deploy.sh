8a23c4ea4c6d30e35d061ca1cb7e42cb26db8350
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-16 15:45:33 -0500

    Change staging profile for autorelease
    Change-Id: I3852acd7d5570ef37d4fe60f76d4f1aec50a40bd
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/autorelease/include-raw-autorelease-maven-deploy.sh
+++ jjb/autorelease/include-raw-autorelease-maven-deploy.sh
@@ -34,7 +34,7 @@ rsync -avz --exclude 'maven-metadata-local.xml' \
 mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.2:deploy-staged-repository \
     -DrepositoryDirectory="`pwd`/m2repo" \
     -DnexusUrl=http://nexus.opendaylight.org/ \
-    -DstagingProfileId="21a27b7f3bbb8d" \
-    -DserverId="opendaylight.weekly" \
+    -DstagingProfileId="425e43800fea70" \
+    -DserverId="opendaylight.staging" \
     -s $AUTORELEASE_SETTINGS \
     -gs $ODL_GLOBAL_SETTINGS | tee $WORKSPACE/deploy-staged-repository.log
