3743ea7a8107481603708d06af50eabf2aab4e4a
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-01-25 10:22:34 -0500

    Fix path to integration distribution-karaf
    Change-Id: Ic94a7bee41fd82bde398e005e503cd534ee1c84c
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/autorelease/include-raw-autorelease-get-integration-test-variables.sh
+++ jjb/autorelease/include-raw-autorelease-get-integration-test-variables.sh
@@ -10,7 +10,7 @@
 ##############################################################################
 
 NEXUSURL=http://nexus.opendaylight.org/content/repositories/
-VERSION=`grep -m1 '<version>' ${WORKSPACE}/integration/distribution/distributions/karaf/pom.xml | awk -F'[<|>]' '/version/ { printf $3 }'`
+VERSION=`grep -m1 '<version>' ${WORKSPACE}/integration/distribution/distribution-karaf/pom.xml | awk -F'[<|>]' '/version/ { printf $3 }'`
 REPOID=`grep "Closing staging repository with ID" $WORKSPACE/deploy-staged-repository.log | cut -d '"' -f2`
 
 echo BUNDLEURL=${NEXUSURL}/${REPOID}/org/opendaylight/integration/distribution-karaf/${VERSION}/distribution-karaf-${VERSION}.zip > $WORKSPACE/variables.prop
