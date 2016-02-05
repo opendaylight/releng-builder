ab5f0d5ea8279e30c3528f236886f65e53407392
Luis Gomez <ecelgp@gmail.com>
2016-02-04 18:03:18 -0800

    Add jdk selection to distrbution deploy job
    Change-Id: I5be8e56ec65d9aab35dda21ad5859a3f094d8850
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- jjb/integration/include-raw-integration-deploy-controller-verify.sh
+++ jjb/integration/include-raw-integration-deploy-controller-verify.sh
@@ -1,6 +1,19 @@
 CONTROLLERMEM="3072m"
 ACTUALFEATURES="odl-integration-all"
 
+if [ ${JDKVERSION} == 'openjdk8' ]; then
+    echo "Setting the JDK Version to 8"
+    sudo /usr/sbin/alternatives --set java /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.60-2.b27.el7_1.x86_64/jre/bin/java
+    export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.60-2.b27.el7_1.x86_64
+    java -version
+fi
+if [ ${JDKVERSION} == 'openjdk7' ]; then
+    echo "Setting the JDK Version to 7"
+    sudo /usr/sbin/alternatives --set java /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.85-2.6.1.2.el7_1.x86_64/jre/bin/java
+    export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.85-2.6.1.2.el7_1.x86_64
+    java -version
+fi
+
 echo "Kill any controller running"
 ps axf | grep karaf | grep -v grep | awk '{print "kill -9 " $1}' | sh
 
