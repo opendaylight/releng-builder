2b9f906a7878c6dd18fb5461a143c8666ee88e97
Jozef Behran <jbehran@cisco.com>
2016-02-19 11:31:12 +0100

    Add section showing properties of the environment.
    Currently the list of installed packages and their versions
    is shown.
    
    Change-Id: If536c6f0d61d43751c174f523737002fe78932c0
    Signed-off-by: Jozef Behran <jbehran@cisco.com>

--- jjb/integration/include-raw-integration-get-bundle-vars.sh
+++ jjb/integration/include-raw-integration-get-bundle-vars.sh
@@ -1,3 +1,12 @@
+# TODO: Extract this environment properties display section into its own
+#       script and edit YAML templates of the jobs to include this new
+#       script as the first script on each job.
+echo "#################################################"
+echo "##       Show Environment Properties           ##"
+echo "#################################################"
+echo "@@@ Installed PIP packages and their versions @@@"
+pip freeze
+
 echo "#################################################"
 echo "##       Inject Global Variables               ##"
 echo "#################################################"
