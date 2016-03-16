dee7ea050c6058e9900f156baa4d0f8c0945626d
Tomas Cechvala <tcechval@cisco.com>
2016-03-16 09:23:47 +0100

    Making Openstack jobs reusable
    Other projects are also interested in having
    single node Devstack Tempest jobs.
    
    Change-Id: Ib1e802da959232c0332d15a57d920f817895fd35
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

---
+++ jjb/integration/include-raw-integration-deploy-robot-testing.sh
@@ -0,0 +1,15 @@
+echo "Changing the testplan path..."
+cat ${WORKSPACE}/test/csit/testplans/${TESTPLAN} | sed "s:integration:${WORKSPACE}:" > testplan.txt
+cat testplan.txt
+
+SUITES=$( egrep -v '(^[[:space:]]*#|^[[:space:]]*$)' testplan.txt | tr '\012' ' ' )
+
+echo "Starting Robot test suites ${SUITES} ..."
+
+pybot -e exclude -v WORKSPACE:/tmp -v USER_HOME:$HOME -L TRACE -v DEVSTACK_SYSTEM_USER:$USER \
+                        -v DEVSTACK_SYSTEM_IP:$ODL_SYSTEM_IP -v DEFAULT_LINUX_PROMPT:\]\> \
+                        -v OPENSTACK_BRANCH:$OPENSTACK_BRANCH -v ODL_VERSION:$ODL_VERSION \
+                        -v TEMPEST_REGEX:$TEMPEST_REGEX ${SUITES} || true
+
+# vim: ts=4 sw=4 sts=4 et ft=sh :
+
