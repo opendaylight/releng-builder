3145d0da7d6079898a9755a5cd2f0a52771f9b51
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-03-30 12:57:07 -0400

    Produce 2 staging repositories for release builds
    This allows us to get around running an Nginx server so that the
    distribution job can run automatically. Instead we push 2 staging repos
    the 2nd one will allow us to sign artifacts.
    
    Change-Id: Icfe3768eaa93fb6e3cd30b29f22d3da461c8e955
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/autorelease/include-raw-autorelease-maven-deploy.sh
+++ jjb/autorelease/include-raw-autorelease-maven-deploy.sh
@@ -9,15 +9,6 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
-# Job will pass in a variable ${DATESTAMP} if this variable is false than we
-# we are likely releasing a release candidate. We should skip closing the
-# Nexus staging repository so that we can sign the artifacts.
-SKIP_STAGING_CLOSE=false
-if [ "${DATESTAMP}" == "false" ]
-then
-    SKIP_STAGING_CLOSE=true
-fi
-
 # Assuming that mvn deploy created the hide/from/pom/files/stage directory.
 cd hide/from/pom/files
 mkdir -p m2repo/org/opendaylight/
@@ -39,10 +30,25 @@ rsync -avz --exclude 'maven-metadata*' \
            "stage/org/opendaylight/integration" m2repo/org/opendaylight/
 
 mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.2:deploy-staged-repository \
-    -DskipStagingRepositoryClose=${SKIP_STAGING_CLOSE} \
     -DrepositoryDirectory="`pwd`/m2repo" \
     -DnexusUrl=http://nexus.opendaylight.org/ \
     -DstagingProfileId="425e43800fea70" \
     -DserverId="opendaylight.staging" \
     -s $SETTINGS_FILE \
     -gs $GLOBAL_SETTINGS_FILE | tee $WORKSPACE/deploy-staged-repository.log
+
+# Job will pass in a variable ${DATESTAMP} if this variable is false than we
+# we are likely releasing a release candidate. Produce a 2nd staging repository
+# for GPG signing.
+
+if [ "${DATESTAMP}" == "false" ]
+then
+    mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.2:deploy-staged-repository \
+        -DskipStagingRepositoryClose=true
+        -DrepositoryDirectory="`pwd`/m2repo" \
+        -DnexusUrl=http://nexus.opendaylight.org/ \
+        -DstagingProfileId="425e43800fea70" \
+        -DserverId="opendaylight.staging" \
+        -s $SETTINGS_FILE \
+        -gs $GLOBAL_SETTINGS_FILE | tee $WORKSPACE/deploy-staged-repository-gpg.log
+fi
