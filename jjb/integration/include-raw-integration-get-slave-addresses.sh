a49dea0cc307843eaa4ea445096df3c3d13b7bc3
gvrangan <venkatrangang@hcl.com>
2016-04-07 01:12:44 +0000

    Builder job for deploying Openstack for testing
    Change-Id: I48a8fc573042d8e55898b6817ec6880eccdb4597
    Signed-off-by: gvrangan <venkatrangang@hcl.com>
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/include-raw-integration-get-slave-addresses.sh
+++ jjb/integration/include-raw-integration-get-slave-addresses.sh
@@ -2,21 +2,36 @@
 
 ODL_SYSTEM=()
 TOOLS_SYSTEM=()
+OPENSTACK_SYSTEM=()
+
+echo "JCLOUDS IPS are ${JCLOUDS_IPS}"
 
 IFS=',' read -ra ADDR <<< "${JCLOUDS_IPS}"
 
 for i in "${ADDR[@]}"
 do
     REMHOST=`ssh ${i} hostname`
-    if [ `echo ${REMHOST} | grep 'java\|devstack'` ]; then
-        ODL_SYSTEM=( "${ODL_SYSTEM[@]}" "${i}" )
-    else
-        TOOLS_SYSTEM=( "${TOOLS_SYSTEM[@]}" "${i}" )
-    fi
+    case ${REMHOST} in
+    *java*)
+       ODL_SYSTEM=( "${ODL_SYSTEM[@]}" "${i}" )
+       ;;
+    *devstack*)
+       OPENSTACK_SYSTEM=( "${OPENSTACK_SYSTEM[@]}" "${i}" )
+       ;;
+    *)
+       TOOLS_SYSTEM=( "${TOOLS_SYSTEM[@]}" "${i}" )
+       ;;
+    esac
 done
 
 echo "NUM_ODL_SYSTEM=${#ODL_SYSTEM[@]}" >> slave_addresses.txt
 echo "NUM_TOOLS_SYSTEM=${#TOOLS_SYSTEM[@]}" >> slave_addresses.txt
+#if HA Proxy is requested the last devstack node will be configured as haproxy
+if [ "${ENABLE_HAPROXY_FOR_NEUTRON}" == "yes" ]; then
+   echo "NUM_OPENSTACK_SYSTEM=$(( ${#OPENSTACK_SYSTEM[@]} - 1 ))" >> slave_addresses.txt
+else
+   echo "NUM_OPENSTACK_SYSTEM=${#OPENSTACK_SYSTEM[@]}" >> slave_addresses.txt
+fi
 
 # Add alias for ODL_SYSTEM_1_IP as ODL_SYSTEM_IP
 echo "ODL_SYSTEM_IP=${ODL_SYSTEM[0]}" >> slave_addresses.txt
@@ -32,4 +47,10 @@ do
     echo "TOOLS_SYSTEM_$((i+1))_IP=${TOOLS_SYSTEM[${i}]}" >> slave_addresses.txt
 done
 
+echo "OPENSTACK_CONTROL_NODE_IP=${OPENSTACK_SYSTEM[0]}" >> slave_addresses.txt
+for i in `seq 1 $(( ${#OPENSTACK_SYSTEM[@]} - 1 ))`
+do
+    echo "OPENSTACK_COMPUTE_NODE_$((i))_IP=${OPENSTACK_SYSTEM[${i}]}" >> slave_addresses.txt
+done
 # vim: sw=4 ts=4 sts=4 et ft=sh :
+
