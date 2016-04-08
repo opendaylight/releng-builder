594524e738aa9a5f8cad622158838289b8867aec
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-04-08 17:44:27 -0400

    Temporarily disable installs from archive.apache.org
    According to archive.apache.org it is under maintenance until April 11th
    2016 so all installs from that path are failing. Temporarily disable so
    that CSIT can successfully spin up a controller.
    
    Change-Id: I1e407a62abcc33549125acaf3c5b55f0a44f5eba
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/controller.sh
+++ jenkins-scripts/controller.sh
@@ -7,26 +7,33 @@
 yum install -y sshpass
 
 # vim: sw=2 ts=2 sts=2 et :
+
+
+###
+### Disable archive.apache.org downloads since that site is under maintenance until
+### April 11th 2016.
+###
+
 # Installation of Hbase
-mkdir /tmp/Hbase
-cd /tmp/Hbase
+#mkdir /tmp/Hbase
+#cd /tmp/Hbase
 
-wget --no-verbose https://archive.apache.org/dist/hbase/hbase-0.94.15/hbase-0.94.15.tar.gz
+#wget --no-verbose https://archive.apache.org/dist/hbase/hbase-0.94.15/hbase-0.94.15.tar.gz
 
-echo "Installing the Hbase Server..."
-tar -xvf hbase*.tar.gz
+#echo "Installing the Hbase Server..."
+#tar -xvf hbase*.tar.gz
 
 
 #Installation of Cassandra
 
 
-mkdir /tmp/cassandra
-cd /tmp/cassandra
+#mkdir /tmp/cassandra
+#cd /tmp/cassandra
 
-wget --no-verbose http://archive.apache.org/dist/cassandra/2.1.12/apache-cassandra-2.1.12-bin.tar.gz
+#wget --no-verbose http://archive.apache.org/dist/cassandra/2.1.12/apache-cassandra-2.1.12-bin.tar.gz
 
-echo "Installing the Cassandra Server..."
-tar -xvf apache*.tar.gz
+#echo "Installing the Cassandra Server..."
+#tar -xvf apache*.tar.gz
 
 
 cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
