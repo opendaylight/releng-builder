00f1363428c2809f4507fd9329e63461b9e07a0b
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-04-19 19:20:36 -0400

    Add release job template
    This new release job type will allow projects not part of autorelease to
    perform a release by staging their release in Nexus similar to
    autorelease and release the artifacts to opendaylight.release repo once
    approved.
    
    Change-Id: I7a26c160a9a49236e6ae2744336bcfeaecade9a3
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/include-raw-stage-release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2016 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Assuming that mvn deploy created the hide/from/pom/files/stage directory.
+cd hide/from/pom/files
+mkdir -p m2repo/org/opendaylight/
+
+rsync -avz --exclude 'maven-metadata*' \
+           --exclude '_remote.repositories' \
+           --exclude 'resolver-status.properties' \
+           "stage/org/opendaylight/$m" m2repo/org/opendaylight/
+
+mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.2:deploy-staged-repository \
+    -DrepositoryDirectory="`pwd`/m2repo" \
+    -DnexusUrl=https://nexus.opendaylight.org/ \
+    -DstagingProfileId="$STAGING_PROFILE_ID" \
+    -DserverId="opendaylight.staging" \
+    -s $SETTINGS_FILE \
+    -gs $GLOBAL_SETTINGS_FILE | tee $WORKSPACE/deploy-staged-repository.log
