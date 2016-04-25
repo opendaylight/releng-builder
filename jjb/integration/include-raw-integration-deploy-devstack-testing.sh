4aba0e59536d813fcda27fd8113aa2cf71761306
Tomas Cechvala <tcechval@cisco.com>
2016-04-25 10:11:23 +0200

    Fixing robot reference to devstack VM
    ODL_SYSTEM_IP -> OPENSTACK_CONTROL_NODE_IP
    
    Change-Id: I02d4ae5eefa3b9b50b7465b693efc243de23eaa0
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

--- jjb/integration/include-raw-integration-deploy-devstack-testing.sh
+++ jjb/integration/include-raw-integration-deploy-devstack-testing.sh
@@ -9,14 +9,14 @@ echo "Starting Robot test suites ${SUITES} ..."
 pybot -e exclude \
 -v WORKSPACE:/tmp -v USER_HOME:$HOME -L TRACE \
 -v DEVSTACK_SYSTEM_USER:$USER \
--v DEVSTACK_SYSTEM_IP:$ODL_SYSTEM_IP \
+-v DEVSTACK_SYSTEM_IP:$OPENSTACK_CONTROL_NODE_IP \
 -v DEFAULT_LINUX_PROMPT:\]\> \
 -v OPENSTACK_BRANCH:$OPENSTACK_BRANCH \
 -v ODL_VERSION:$ODL_VERSION \
 -v TEMPEST_REGEX:$TEMPEST_REGEX ${SUITES} || true
 
-scp $ODL_SYSTEM_IP:/opt/stack/logs/devstacklog.txt $WORKSPACE/
-scp -r $ODL_SYSTEM_IP:/opt/stack/logs/*karaf* $WORKSPACE/
+scp $OPENSTACK_CONTROL_NODE_IP:/opt/stack/logs/devstacklog.txt $WORKSPACE/
+scp -r $OPENSTACK_CONTROL_NODE_IP:/opt/stack/logs/*karaf* $WORKSPACE/
 
 # vim: ts=4 sw=4 sts=4 et ft=sh :
 
