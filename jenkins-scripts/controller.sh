8518c7b20af42fcbfe3b7c0bdea31831cef6236f
Chaudhry Usama <chaudhryusama@gmail.com>
2016-04-27 23:39:13 -0700

    Added Mirror image for TSDR Tools in the controller script
    Change-Id: I0150a1c2e3e57e1ab7cde7b3889909898fb7a390
    Signed-off-by: Chaudhry Usama <chaudhryusama@gmail.com>

--- jenkins-scripts/controller.sh
+++ jenkins-scripts/controller.sh
@@ -13,9 +13,16 @@ cd /tmp/Hbase
 
 wget --no-verbose https://archive.apache.org/dist/hbase/hbase-0.94.15/hbase-0.94.15.tar.gz
 
-echo "Installing the Hbase Server..."
-tar -xvf hbase*.tar.gz
-
+if [ -f hbase-0.94.15.tar.gz ]
+then
+    echo "Installing the Hbase Server..."
+    tar -xvf hbase-0.94.15.tar.gz
+else
+    wget --no-verbose http://apache.osuosl.org/hbase/hbase-0.94.27/hbase-0.94.27.tar.gz
+    echo "Installing the Hbase Server..."
+    tar -xvf hbase-0.94.27.tar.gz
+    mv hbase-0.94.27 hbase-0.94.15
+fi
 
 #Installation of Cassandra
 
@@ -25,9 +32,16 @@ cd /tmp/cassandra
 
 wget --no-verbose http://archive.apache.org/dist/cassandra/2.1.12/apache-cassandra-2.1.12-bin.tar.gz
 
-echo "Installing the Cassandra Server..."
-tar -xvf apache*.tar.gz
-
+if [ -f apache-cassandra-2.1.12-bin.tar.gz ]
+then
+    echo "Installing the Cassandra Server..."
+    tar -xvf apache-cassandra-2.1.12-bin.tar.gz
+else
+    wget --no-verbose http://apache.osuosl.org/cassandra/2.1.14/apache-cassandra-2.1.14-bin.tar.gz
+    echo "Installing the Cassandra Server..."
+    tar -xvf apache-cassandra-2.1.14-bin.tar.gz
+    mv apache-cassandra-2.1.14 apache-cassandra-2.1.12
+fi
 
 cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
 Defaults:jenkins !requiretty
