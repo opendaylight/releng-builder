90621e6b3a8ef8334d6891a51a9599fdd9f10024
Andrew Grimberg <agrimberg@linuxfoundation.org>
2016-05-02 10:32:23 -0700

    Changes needed for vagrant in the ODL Priv Cloud
    Rework the baseline definition for use in the ODL Priv Cloud. This new
    definition is more flexible but is also a baseline validation that we
    can use to modify the other vagrant types so we can have a single image
    snapshot instead of a multi-layer one as we have historically had
    
    Change-Id: Ib38d79d95dfa7ac075def3bc54186555a19f1128
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- vagrant/baseline/README.md
+++ vagrant/baseline/README.md
@@ -1,35 +1,14 @@
-basline can be used to prepare systems in the Rackspace (or potentially
-other environments) for following vagrant layers.
+baseline can be used for preparing basic test images. It's suitable for
+use only as a verification that our baseline library script is working
+as expected or for a very vanilla image.
 
-While the base image that is looked for is
-'Fedora 20 (Heisenbug) (PVHVM)' which is no longer even offered, the
-variable is being left in place so to prompt selection of a proper base
-image to spin up against.
-
-This is controlled by the RSIMAGE environment variable
+This is controlled by the IMAGE environment variable
 
 ex:
 
-$ RSIMAGE='CentOS 7 (PVHVM)' vagrant up --provider=rackspace
+$ export RESEAL=true
+$ IMAGE='CentOS 7' vagrant up --provider=openstack
 
-This vagrant will just set the instance up at the most basic to be
-Vagrant capable and also SELinux enforcing. It will then "reseal" itself
-and state the the system is ready for imaging. Any further RackSpace
-specific Vagrant definitions will expect a base system of the form
-"$DISTRO - Vagrant ready" for the base image name
-
-ex:
-
-Fedora 20 (Heisenbug) - Vagrant ready
-
-or
-
-CentOS 7 - Vagrant ready
-
-NOTE: The reseal operation _destroys_ the SSH keys that were used to
-bring the Vagrant system up effectively making the system unable to
-perform SSH based logins again. This is intentional.
-
-If you are bringing up an Ubuntu system you _must_ also set
-RSPTY='default' or the bring up will hang indefinitely during the OS
-upgrade phase.
+If $RESEAL is not set then the system will not be cleaned up in
+preparation for snapshotting. This is mostly useful for troubleshooting
+a vagrant definition before you do your final creation and snapshot.
--- vagrant/lib/baseline.sh
+++ vagrant/lib/baseline.sh
@@ -4,26 +4,39 @@
 
 rh_systems() {
     # Handle the occurance where SELINUX is actually disabled
-    if [ `grep SELINUX=permissive /etc/selinux/config` ]; then
-        # make sure that the filesystem is properly labelled.
-        # it could be not fully labeled correctly if it was just switched
-        # from disabled, the autorelabel misses some things
-        # skip relabelling on /dev as it will generally throw errors
-        restorecon -R -e /dev /
+    SELINUX=$(grep -E '^SELINUX=(disabled|permissive|enforcing)$' /etc/selinux/config)
+    MODE=$(echo "$SELINUX" | cut -f 2 -d '=')
+    case "$MODE" in
+        permissive)
+            echo "************************************"
+            echo "** SYSTEM ENTERING ENFORCING MODE **"
+            echo "************************************"
+            # make sure that the filesystem is properly labelled.
+            # it could be not fully labeled correctly if it was just switched
+            # from disabled, the autorelabel misses some things
+            # skip relabelling on /dev as it will generally throw errors
+            restorecon -R -e /dev /
 
-        # enable enforcing mode from the very start
-        setenforce enforcing
+            # enable enforcing mode from the very start
+            setenforce enforcing
 
-        # configure system for enforcing mode on next boot
-        sed -i 's/SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
-    else
-        sed -i 's/SELINUX=disabled/SELINUX=permissive/' /etc/selinux/config
-        touch /.autorelabel
+            # configure system for enforcing mode on next boot
+            sed -i 's/SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
+        ;;
+        disabled)
+            sed -i 's/SELINUX=disabled/SELINUX=permissive/' /etc/selinux/config
+            touch /.autorelabel
 
-        echo "*******************************************"
-        echo "** SYSTEM REQUIRES A RESTART FOR SELINUX **"
-        echo "*******************************************"
-    fi
+            echo "*******************************************"
+            echo "** SYSTEM REQUIRES A RESTART FOR SELINUX **"
+            echo "*******************************************"
+        ;;
+        enforcing)
+            echo "*********************************"
+            echo "** SYSTEM IS IN ENFORCING MODE **"
+            echo "*********************************"
+        ;;
+    esac
 
     # Allow jenkins access to alternatives command to switch java version
     cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
@@ -37,7 +50,7 @@ EOF
 
     # add in components we need or want on systems
     echo "---> Installing base packages"
-    yum install -y -q @base
+    yum install -y -q @base https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
     # separate group installs from package installs since a non-existing
     # group with dnf based systems (F21+) will fail the install if such
     # a group does not exist
@@ -50,8 +63,8 @@ EOF
     echo "---> Configuring OpenJDK"
     yum install -y -q 'java-*-openjdk-devel'
 
-    FACTER_OS=`/usr/bin/facter operatingsystem`
-    FACTER_OSVER=`/usr/bin/facter operatingsystemrelease`
+    FACTER_OS=$(/usr/bin/facter operatingsystem)
+    FACTER_OSVER=$(/usr/bin/facter operatingsystemrelease)
     case "$FACTER_OS" in
         Fedora)
             if [ "$FACTER_OSVER" -ge "21" ]
@@ -79,23 +92,41 @@ Defaults:jenkins !requiretty
 jenkins ALL = NOPASSWD: /usr/bin/update-alternatives
 EOF
 
+    export DEBIAN_FRONTEND=noninteractive
+    cat <<EOF >> /etc/apt/apt.conf
+APT {
+  Get {
+    Assume-Yes "true";
+    allow-change-held-packages "true";
+    allow-downgrades "true";
+    allow-remove-essential "true";
+  };
+};
+
+Dpkg::Options {
+  "--force-confdef";
+  "--force-confold";
+};
+
+EOF
+
     echo "---> Updating operating system"
-    apt-get update -qq
-    apt-get upgrade -y --force-yes -qq
+    apt-get update
+    apt-get upgrade
 
     # add in stuff we know we need
     echo "---> Installing base packages"
-    apt-get install -y --force-yes -qq unzip xz-utils puppet git libxml-xpath-perl
+    apt-get install unzip xz-utils puppet git libxml-xpath-perl
 
     # install Java 7
     echo "---> Configuring OpenJDK"
-    apt-get install -y --force-yes -qq openjdk-7-jdk
+    apt-get install openjdk-7-jdk
 
     # make jdk8 available
     add-apt-repository -y ppa:openjdk-r/ppa
-    apt-get update -qq
+    apt-get update
     # We need to force openjdk-8-jdk to install
-    apt-get install -y -qq openjdk-8-jdk
+    apt-get install openjdk-8-jdk
 
     # make sure that we still default to openjdk 7
     update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
@@ -111,10 +142,10 @@ all_systems() {
 
     # Do any Distro specific installations here
     echo "Checking distribution"
-    FACTER_OS=`/usr/bin/facter operatingsystem`
+    FACTER_OS=$(/usr/bin/facter operatingsystem)
     case "$FACTER_OS" in
         RedHat|CentOS)
-            if [ `/usr/bin/facter operatingsystemrelease | /bin/cut -d '.' -f1` = "7" ]; then
+            if [ "$(/usr/bin/facter operatingsystemrelease | /bin/cut -d '.' -f1)" = "7" ]; then
                 echo
                 echo "---> CentOS 7"
                 echo "No extra steps currently for CentOS 7"
@@ -133,20 +164,15 @@ all_systems() {
 }
 
 echo "---> Attempting to detect OS"
-# OS selector
-if [ -f /usr/bin/yum ]
-then
-    OS='RH'
-else
-    OS='UBUNTU'
-fi
+# upstream cloud images use the distro name as the initial user
+ORIGIN=$(logname)
 
-case "$OS" in
-    RH)
+case "${ORIGIN}" in
+    fedora|centos)
         echo "---> RH type system detected"
         rh_systems
     ;;
-    UBUNTU)
+    ubuntu)
         echo "---> Ubuntu system detected"
         ubuntu_systems
     ;;
