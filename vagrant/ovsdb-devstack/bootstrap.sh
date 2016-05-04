c7c346203154d3d3c5822e2f8e4c6bbdf063b3b6
Andrew Grimberg <agrimberg@linuxfoundation.org>
2016-05-04 10:29:43 -0700

    Update devstack vagrant definition for private cloud
    This should update the ovsdb-devstack vagrant definition for our private
    cloud. References to Fedora should be scrubbed, but if they aren't it's
    will likely only be in comments ;)
    
    Change-Id: I207adc4d5718bc16df98b2386444ba9701e98d00
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- vagrant/ovsdb-devstack/bootstrap.sh
+++ vagrant/ovsdb-devstack/bootstrap.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-# enable enforcing mode from the very start
-setenforce enforcing
-
-# configure system for enforcing mode on next boot
-sed -i 's/SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
-
-yum clean all
-yum update -y
+echo '---> Installing non-baseline requirements'
 yum install -q -y deltarpm python{,-{crypto,devel,lxml,setuptools}} \
-    @development {lib{xml2,xslt,ffi},openssl}-devel \
-    java git sudo
+    @development {lib{xml2,xslt,ffi},openssl}-devel
 
-# figure out what the latest kernel installed is and switch to it
-# NOTE: This is done like this becase the Rackspace F20 images are using
-# extlinux / syslinux and don't switch to the newest kernel on update
-NEWKERNEL=`rpm -qa | grep kernel-3 | sort -r | head -1 | cut -c 8-`
-if [ -f /boot/extlinux.conf ]; then
-    BOOTLABEL=`grep ${NEWKERNEL} /boot/extlinux.conf | grep LABEL | cut -c 7-`
-    sed -i "s/ONTIMEOUT linux/ONTIMEOUT ${BOOTLABEL}/" /boot/extlinux.conf
-fi
-
+echo '---> Updating net link setup'
 if [ ! -f /etc/udev/rules.d/80-net-setup-link.rules ]; then
     ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 fi
