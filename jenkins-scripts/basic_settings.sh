822e9c121ed2156ffdc3afb1f733bd2b0e610b69
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-05-13 16:26:03 -0400

    Add script to create jenkins user for openstack
    Change-Id: Ic820fc0e5862ca2fccd702898a42d843ad059218
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/basic_settings.sh
+++ jenkins-scripts/basic_settings.sh
@@ -29,4 +29,15 @@ cat <<EOKNOWN >  /etc/ssh/ssh_known_hosts
 [140.211.169.26]:29418,[git.opendaylight.org]:29418 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAyRXyHEw/P1iZr/fFFzbodT5orVV/ftnNRW59Zh9rnSY5Rmbc9aygsZHdtiWBERVVv8atrJSdZool75AglPDDYtPICUGWLR91YBSDcZwReh5S9es1dlQ6fyWTnv9QggSZ98KTQEuE3t/b5SfH0T6tXWmrNydv4J2/mejKRRLU2+oumbeVN1yB+8Uau/3w9/K5F5LgsDDzLkW35djLhPV8r0OfmxV/cAnLl7AaZlaqcJMA+2rGKqM3m3Yu+pQw4pxOfCSpejlAwL6c8tA9naOvBkuJk+hYpg5tDEq2QFGRX5y1F9xQpwpdzZROc5hdGYntM79VMMXTj+95dwVv/8yTsw==
 EOKNOWN
 
+#######################
+# Create Jenkins User #
+#######################
+
+DISTRO=$(logname)
+useradd -m jenkins
+mkdir /home/jenkins/.ssh
+mkdir /w
+cp -r /home/${DISTRO}/.ssh/authorized_keys /home/jenkins/.ssh/authorized_keys
+chown -R jenkins:jenkins /home/jenkins/.ssh /w
+
 # vim: sw=2 ts=2 sts=2 et :
