c86354f948bfda6745ec38d8731db08589f10913
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-05-18 13:22:36 -0400

    Generate ssh keypairs for use by Robot jobs
    Creates ssh keypairs as id_rsa and id_rsa.pub that can be used by Robot
    systems as authentication keys when connecting to mininet and controller
    nodes.
    
    Change-Id: Iddf6b8038445c7ce5afa9a90537e134bed2b6d8f
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/create_jenkins_user.sh
+++ jenkins-scripts/create_jenkins_user.sh
@@ -10,4 +10,6 @@ useradd -m -s /bin/bash jenkins
 mkdir /home/jenkins/.ssh
 mkdir /w
 cp -r /home/${OS}/.ssh/authorized_keys /home/jenkins/.ssh/authorized_keys
+# Generate ssh key for use by Robot jobs
+echo -e 'y\n' | ssh-keygen -N "" -f /home/jenkins/.ssh/id_rsa -t rsa
 chown -R jenkins:jenkins /home/jenkins/.ssh /w
