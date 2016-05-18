024e523314548938a34c45623024789210ae6984
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-05-18 15:01:15 -0400

    Allow jenkins user to run sudo in devstack
    Change-Id: I6b5a58052dadbfb570746a095230178b71c537fc
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/devstack.sh
+++ jenkins-scripts/devstack.sh
@@ -25,6 +25,7 @@ EOHOSTS
 # make sure we don't require tty for sudo operations
 cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
 Defaults:jenkins !requiretty
+jenkins     ALL = NOPASSWD: ALL
 EOF
 
 # vim: sw=2 ts=2 sts=2 et :
