f287137d03c00ab3504c5b1439dbcfb489396a83
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-05-19 16:32:06 -0400

    Configure sudoers at the beginning of the script
    Change-Id: Iad1ddd27e6256d2d0ad950256f033eed7e4a5ca3
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/ubuntu-docker-ovs.sh
+++ jenkins-scripts/ubuntu-docker-ovs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# make sure we don't require tty for sudo operations
+cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
+Defaults:jenkins !requiretty
+jenkins     ALL = NOPASSWD: ALL
+EOF
+
 # Do the final install of OVS that the has to be done at boot time for
 # some reason due to how the snapshots keep behaving.
 dpkg --install /root/openvswitch-datapath-dkms* && \
@@ -11,12 +17,4 @@ dpkg --install /root/openvswitch-{common,switch}*
 # pull docker images
 docker pull alagalah/odlpoc_ovs230
 
-
-# make sure we don't require tty for sudo operations
-cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
-Defaults:jenkins !requiretty
-jenkins     ALL = NOPASSWD: ALL
-EOF
-
 # vim: sw=2 ts=2 sts=2 et :
-
