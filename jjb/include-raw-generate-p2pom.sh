a1c1b87a184eabc4589ca21ce2f4b1f5d19730f7
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-05-19 19:05:36 -0400

    Improve p2deploy scripts to support snapshot deploy
    - Will deploy a release if an opendaylight.snapshot URL is not found
    - Will deploy latest SNAPSHOT build if P2ZIP_URL is blank
    - Will deploy snapshot build if P2ZIP_URL contains opendaylight.snapshot
    
    TODO:
    - Currently hardcoded version to 1.1.1-SNAPSHOT it would be better if we
      can detect the latest x.y.z-SNAPSHOT automatically
    
    Change-Id: I1fe343e0664ae8badc1f1f2bce88f66dea759c19
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/include-raw-generate-p2pom.sh
+++ jjb/include-raw-generate-p2pom.sh
@@ -9,10 +9,23 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
-FILE_NAME=`echo $P2ZIP_URL | awk -F'/' '{ print $NF }'`
-VERSION=`echo $P2ZIP_URL | awk -F'/' '{ print $(NF-1) }'`
+if [[ $P2ZIP_URL == "" ]]; then
+    P2ZIP_URL=opendaylight.snapshot/$(find . -name "*.zip" -type f -exec ls "{}" + | head -1)
+    FILE_NAME=`echo $P2ZIP_URL | awk -F'/' '{ print $NF }'`
+    RELEASE_PATH="snapshot"
+else
+    FILE_NAME=`echo $P2ZIP_URL | awk -F'/' '{ print $NF }'`
+    VERSION=`echo $P2ZIP_URL | awk -F'/' '{ print $(NF-1) }'`
+    RELEASE_PATH="release/$VERSION"
+    wget --quiet $P2ZIP_URL -O $FILE_NAME
+fi
 
-wget $P2ZIP_URL -O $FILE_NAME
+# If we detect a snapshot build then release to a snapshot repo
+# YangIDE has indicated that the only want the latest snapshot released to
+# the snapshot directory.
+if echo $P2ZIP_URL | grep opendaylight.snapshot; then
+    RELEASE_PATH="snapshot"
+fi
 
 cat > ${WORKSPACE}/pom.xml <<EOF
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
@@ -47,7 +60,7 @@ cat > ${WORKSPACE}/pom.xml <<EOF
               <serverId>opendaylight-p2</serverId>
               <repositoryUrl>https://nexus.opendaylight.org/service/local/repositories/p2repos/content-compressed</repositoryUrl>
               <file>$FILE_NAME</file>
-              <repositoryPath>org.opendaylight.$PROJECT/$VERSION</repositoryPath>
+              <repositoryPath>org.opendaylight.$PROJECT/$RELEASE_PATH</repositoryPath>
             </configuration>
           </execution>
         </executions>
