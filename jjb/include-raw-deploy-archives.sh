08d82a1a8e2326a19f2df9dfcaf72e4aa1247223
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-06-13 00:05:35 -0400

    Add macro to deploy archives to Nexus
    Change-Id: Ibc3d4a74416995b699e420864626dabd8a3c0658
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/include-raw-deploy-archives.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+ARCHIVES_DIR="$JOB_NAME/`date +'%Y-%m-%d.%H%M%S'`-$BUILD_NUMBER-$BUILD_CAUSE"
+
+mkdir .archives
+cd .archives/
+
+cat > deploy-archives.xml <<EOF
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>logs</groupId>
+  <artifactId>logs</artifactId>
+  <version>1.0.0</version>
+  <packaging>pom</packaging>
+
+  <build>
+    <plugins>
+      <plugin>
+        <groupId>org.apache.maven.plugins</groupId>
+        <artifactId>maven-deploy-plugin</artifactId>
+        <version>2.8.2</version>
+        <configuration>
+          <skip>true</skip>
+        </configuration>
+      </plugin>
+      <plugin>
+        <groupId>org.sonatype.plugins</groupId>
+        <artifactId>maven-upload-plugin</artifactId>
+        <version>0.0.1</version>
+        <executions>
+          <execution>
+            <id>publish-site</id>
+            <phase>deploy</phase>
+            <goals>
+              <goal>upload-file</goal>
+            </goals>
+            <configuration>
+              <serverId>opendaylight-log-archives</serverId>
+              <repositoryUrl>https://nexus.opendaylight.org/service/local/repositories/logs/content-compressed</repositoryUrl>
+              <file>archives.zip</file>
+              <repositoryPath>$SILO</repositoryPath>
+            </configuration>
+          </execution>
+        </executions>
+      </plugin>
+    </plugins>
+  </build>
+</project>
+EOF
+
+mkdir -p $ARCHIVES_DIR
+mv $WORKSPACE/archives/ $ARCHIVES_DIR
+touch $ARCHIVES_DIR/_build-details.txt
+echo "build-url: $BUILD_URL" >> $ARCHIVES_DIR/_build-details.txt
+wget -O $ARCHIVES_DIR/_console-output.log ${{BUILD_URL}}consoleText
+
+zip -r archives.zip $JOB_NAME/
+
+# Notify Gerrit where logs are located
+if [ -v "$GERRIT_PATCHSET_REVISION" ]; then
+    LOG_MESSAGE="Logs located at https://nexus.opendaylight.org/content/sites/logs/$SILO/$ARCHIVES_DIR/"
+    ssh -p 29418 git.opendaylight.org gerrit review -m "$LOG_MESSAGE" $GERRIT_PATCHSET_REVISION
+fi
