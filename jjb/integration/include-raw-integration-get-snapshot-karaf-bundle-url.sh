59c9bb42f55a341e0702da15f57a30f5e8cadf07
Vratko Polak <vrpolak@cisco.com>
2016-06-17 12:50:04 +0200

    Add a job to build snapshot-karaf distribution
    Only for Boron stream.
    The job is started daily, and triggers integration-distribution-test-boron.
    
    Change-Id: I9bb7ea8da1724c8fad7f217338bf9e9f9c226ff2
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ jjb/integration/include-raw-integration-get-snapshot-karaf-bundle-url.sh
@@ -0,0 +1,14 @@
+
+# Extract the BUNDLEVERSION from the pom.xml
+BUNDLEVERSION=`xpath snapshot-karaf/snapshot-karaf-distribution/pom.xml '/project/version/text()' 2> /dev/null`
+echo "Bundle version is ${BUNDLEVERSION}"
+
+BUNDLEURL="${BUILD_URL}org.opendaylight.integration\$snapshot-karaf/snapshot-karaf-distribution/artifact/org.opendaylight.integration/snapshot-karaf-distribution/${BUNDLEVERSION}/snapshot-karaf-distribution-${BUNDLEVERSION}.zip"
+echo "Bundle url is ${BUNDLEURL}"
+
+# Set BUNDLEVERSION & BUNDLEURL
+echo BUNDLEVERSION=${BUNDLEVERSION} > bundle.txt
+echo BUNDLEURL=${BUNDLEURL} >> bundle.txt
+
+# NOTE: BUNDLEVERSION & BUNDLEURL will be re-imported back into the environment with the
+# Inject environment variables plugin (next step)
