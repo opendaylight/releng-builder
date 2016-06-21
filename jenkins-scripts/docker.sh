e7356f6851dff7fc876c8fe9812ec2fe16a44dfd
Miguel Angel Munoz Gonzalez <miguel.angel.munoz.gonzalez@ericsson.com>
2016-06-21 09:11:11 +0200

    Add new job template, macro and jenkins job for SFC with docker
    It adds a new macro for using docker vms.
    
    This also requires a new job template to include previous macro.
    
    Additionally it includes an SFC job that sets up a deploy with
    3 node cluster and an docker vm that is intended to host several
    containers, simulatind SFFs, SFs or classifiers
    
    Change-Id: I0408abe670fbcc9f9fa1b940ae0b9c24c09c293a
    Signed-off-by: Miguel Angel Munoz Gonzalez <miguel.angel.munoz.gonzalez@ericsson.com>

--- jenkins-scripts/docker.sh
+++ jenkins-scripts/docker.sh
@@ -3,7 +3,7 @@
 # make sure we don't require tty for sudo operations
 cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
 Defaults:jenkins !requiretty
-jenkins     ALL = NOPASSWD: /sbin/ifconfig
+jenkins     ALL = NOPASSWD: ALL
 EOF
 
 # make sure jenkins is part of the docker only if jenkins has already been
