737ccf8fb770bfde3bc7651cbad212f49788ee4f
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-06-22 17:28:28 -0400

    Remove systemctl calls
    The systemctl restart docker hangs on the RPC and doesn't appear to be
    necessary. Stopping firewalld is already handled by other scripts so
    should already be disabled as well.
    
    Change-Id: I778a5a33fa0653ab318cd61d1f200366b8437d24
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/docker.sh
+++ jenkins-scripts/docker.sh
@@ -14,11 +14,4 @@ then
   /usr/sbin/usermod -a -G docker jenkins
 fi
 
-# stop firewall
-systemctl stop firewalld
-
-# restart docker daemon - needed after firewalld stop
-systemctl restart docker
-
 # vim: sw=2 ts=2 sts=2 et :
-
