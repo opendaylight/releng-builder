04e3f05c316e85cb276d621785b64aed2da8b2e3
gvrangan <venkatrangang@hcl.com>
2016-06-29 18:15:10 +0000

    Disable  n-cpu service in Control Node
    Change-Id: I401f02fc81e848badb94f9ab619da332d60f688b
    Signed-off-by: gvrangan <venkatrangang@hcl.com>

--- jjb/integration/include-raw-integration-deploy-openstack-run-test.sh
+++ jjb/integration/include-raw-integration-deploy-openstack-run-test.sh
@@ -400,7 +400,7 @@ scp ${WORKSPACE}/disable_firewall.sh ${OPENSTACK_CONTROL_NODE_IP}:/tmp
 ssh ${OPENSTACK_CONTROL_NODE_IP} "sudo bash /tmp/disable_firewall.sh"
 echo "sleep for a minute and print hypervisor-list"
 sleep 60
-ssh ${OPENSTACK_CONTROL_NODE_IP} "cd /opt/stack/devstack; source openrc admin admin; nova hypervisor-list;nova-manage service list;sudo systemctl status libvirtd"
+ssh ${OPENSTACK_CONTROL_NODE_IP} "cd /opt/stack/devstack; source openrc admin admin; nova hypervisor-list;nova-manage service list"
 
 #Need to disable firewalld and iptables in compute nodes as well
 for i in `seq 1 $((NUM_OPENSTACK_SYSTEM - 1))`
