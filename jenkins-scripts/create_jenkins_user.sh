4959c694c04f9203b2607cec0725660e8e3ce08b
Daniel Farrell <dfarrell@redhat.com>
2016-06-30 19:35:19 -0400

    Add Int/Pack RPM deps to builder.sh startup script
    Installs software required for building RPMs, adds Jenkins user to
    mock group so they have permissions to build RPMs.
    
    Change-Id: I2ef1ff50ec78b9b2e2b81778cc6776e6810a5db9
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- jenkins-scripts/create_jenkins_user.sh
+++ jenkins-scripts/create_jenkins_user.sh
@@ -22,3 +22,7 @@ cp -r /home/${OS}/.ssh/authorized_keys /home/jenkins/.ssh/authorized_keys
 # Generate ssh key for use by Robot jobs
 echo -e 'y\n' | ssh-keygen -N "" -f /home/jenkins/.ssh/id_rsa -t rsa
 chown -R jenkins:jenkins /home/jenkins/.ssh /w
+
+
+# Add jenkins user to mock group so they can build Int/Pack's RPMs
+usermod -a -G mock jenkins
