d18b341aac5267dc241147049d7da15ce2066490
Daniel Farrell <dfarrell@redhat.com>
2016-07-19 14:29:09 -0400

    Add Jenkins job to build RPMs
    Add job that accepts params that define an RPM build and uses
    Int/Pack's build.py script to build an ODL RPM.
    
    Change-Id: I51ee7db2f04a39a4bbc771a223cafced8446b5ca
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ jjb/packaging/include-raw-build-rpm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+# Install required packages
+virtualenv rpm_build
+source rpm_build/bin/activate
+pip install --upgrade pip
+pip install -r $WORKSPACE/packaging/rpm/requirements.txt
+
+# Build release specified by build params
+$WORKSPACE/packaging/rpm/build.py --major "$VERSION_MAJOR" \
+                                  --minor "$VERSION_MINOR" \
+                                  --patch "$VERSION_PATCH" \
+                                  --rpm "$RPM_RELEASE" \
+                                  --sysd_commit "$SYSD_COMMIT" \
+                                  --codename "$CODENAME" \
+                                  --download_url "$DOWNLOAD_URL" \
+                                  --changelog_date "$CHANGELOG_DATE" \
+                                  --changelog_name "$CHANGELOG_NAME" \
+                                  --changelog_email "$CHANGELOG_EMAIL"
