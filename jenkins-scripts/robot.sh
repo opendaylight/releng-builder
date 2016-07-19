20e9d5004cd2ca797ec31675a687267857d9deae
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-07-19 19:00:03 -0400

    Move robot dependencies into packer provision script
    Install robot dependencies during image provisioning to make the spinup
    scripts portion run faster.
    
    Change-Id: I6dd06ff5a044f3fc82d7fa55a1aeafbea0b9478f
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/robot.sh
+++ jenkins-scripts/robot.sh
@@ -7,23 +7,15 @@ jenkins     ALL = NOPASSWD: /usr/bin/sshuttle, /usr/bin/kill, /usr/sbin/iptables
 EOF
 
 yum clean all
-yum install -y -q unzip python-netaddr sshuttle @development
-yum remove -y robotframework-{sshlibrary,requests}
+yum install -y -q python-netaddr
 
-# These development packages are needed for successful installation of robotframework-sshlibrary (done elsewhere)
-yum install -y -q libffi-devel openssl-devel
-
-## Install netcat & docker-py
-yum install -y -q nc python-docker-py
+## Install docker-py
+yum install -y -q python-docker-py
 
 # Install dependencies for matplotlib library used in longevity framework
-yum install -y yum-utils
 yum-builddep -y python-matplotlib
 
 # make sure the firewall is stopped
 service iptables stop
-
-# install crudini command line tool for editing config files
-yum install -y crudini
 
 # vim: sw=2 ts=2 sts=2 et :
