e3e62cc598c15ef23883a2de63f05393955065cc
Victor Pickard <vpickard@redhat.com>
2016-07-25 09:48:19 -0400

    Split OVSDB IT into 4 targets
    The 4 targets are:
    1. build OVSDB
    2. southbound-it
    3. library-it
    4. hwvtepsouthbound-it
    
    For each OVS version (2.4.0, 2.5.0):
    run 2-4 above.
    
    For now, removed OVS 2.3.3, as there is a packaging
    issue in the Docker 2.3.3 image that is causing
    an error starting the HWVTEP emulator.
    
    Change-Id: Ifff0353f7176a7dec1b7d4f10e2d5ee4a72d7b59
    Signed-off-by: Victor Pickard <vpickard@redhat.com>

--- jjb/ovsdb/include-setup-hwvtep-docker.sh
+++ jjb/ovsdb/include-setup-hwvtep-docker.sh
@@ -4,7 +4,6 @@ set -e
 
 echo "---> Configuring OVS for HW VTEP Emulator"
 /usr/bin/docker exec $CID supervisorctl stop ovsdb-server
-sleep 5
 /usr/bin/docker exec $CID supervisorctl start ovsdb-server-vtep
 /usr/bin/docker exec $CID ovs-vsctl add-br br-vtep
 /usr/bin/docker exec $CID ovs-vsctl add-port br-vtep eth0
@@ -12,7 +11,7 @@ sleep 5
 /usr/bin/docker exec $CID vtep-ctl add-port br-vtep eth0
 /usr/bin/docker exec $CID vtep-ctl set Physical_Switch br-vtep tunnel_ips=192.168.254.20
 /usr/bin/docker exec $CID vtep-ctl set-manager ptcp:6640
-
+sleep 5
 echo "---> Starting OVS HW VTEP Emulator"
 /usr/bin/docker exec $CID supervisorctl start ovs-vtep
-sleep 10
+sleep 5
