a44f0640a7980397188e95a6411449387ba2e4ca
gvrangan <venkatrangang@hcl.com>
2016-07-27 17:44:09 +0000

    Remove the mono installation commands from builder.sh
    The required operations happen when the packer-builder script is run.
    The commands in this script installed the latest mono that was downgraded by the
    packer-builder
    
    Change-Id: Iaf34f2cccce417728555db5ce9357c5b56f17d1d
    Signed-off-by: gvrangan <venkatrangang@hcl.com>

--- jenkins-scripts/builder.sh
+++ jenkins-scripts/builder.sh
@@ -4,22 +4,6 @@
 yum clean all
 
 #
-# VTN
-#
-
-# Add mono components for VTN
-yum install -y yum-utils
-# The following is needed for the new code in VTN project
-# These packages will enable C# compilation
-rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-# Add the mono tools repository
-yum-config-manager -q -y --add-repo http://origin-download.mono-project.com/repo/centos
-# Install the nuget binary
-yum install -q -y http://origin-download.mono-project.com/repo/centos/n/nuget/nuget-2.8.3+md58+dhx1-0.noarch.rpm
-# Install the mono toolchain
-yum -q -y install mono-complete
-
-#
 # Integration/Packaging
 #
 
@@ -28,8 +12,6 @@ yum install -y fedora-packager
 
 # Install software for building docs
 yum install -y libxslt-devel
-
-
 
 # disable the firewall
 /bin/bash ./disable_firewall.sh
@@ -45,7 +27,6 @@ cd /tmp/Hbase
 wget --no-verbose http://apache.osuosl.org/hbase/hbase-0.94.27/hbase-0.94.27.tar.gz
 
 echo "Installing the Hbase Server..."
-
 tar -xvf hbase-0.94.27.tar.gz
 
 #Installation of Cassandra
