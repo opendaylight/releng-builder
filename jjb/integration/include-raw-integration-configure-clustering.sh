1e7bfc59e405b67995ecea46d76d79e080f8553b
Peter Gubka <pgubka@cisco.com>
2016-08-26 12:54:28 +0200

    removing rib per node configuration
    Bgpcep was updated in boron to use clustering singleton service. This
    allows to have one rib (not rib per node).
    This update will probably break bgpcep-csit-3node-periodic-bgpclustering-only-beryllium.
    To fix this I will prepare later proper configplans.
    
    Change-Id: Ia4eecaf714bcaa7729d460d2e26dec7e2b39a85c
    Signed-off-by: Peter Gubka <pgubka@cisco.com>

--- jjb/integration/include-raw-integration-configure-clustering.sh
+++ jjb/integration/include-raw-integration-configure-clustering.sh
@@ -113,7 +113,6 @@ echo "Configuring cluster"
 /tmp/${BUNDLEFOLDER}/bin/configure_cluster.sh \$1 ${nodes_list}
 
 echo "Update bgp configuration in 41-bgp-example.xml"
-sed -i -e "s/<rib-id>example-bgp-rib/<rib-id>example-bgp-rib-\$1/g" ${BGPCONF}
 sed -i -e "s/<topology-id>example-ipv4-topology/<topology-id>example-ipv4-topology-\$1/g" ${BGPCONF}
 sed -i -e "s/<topology-id>example-ipv6-topology/<topology-id>example-ipv6-topology-\$1/g" ${BGPCONF}
 sed -i -e "s/<topology-id>example-linkstate-topology/<topology-id>example-linkstate-topology-\$1/g" ${BGPCONF}
