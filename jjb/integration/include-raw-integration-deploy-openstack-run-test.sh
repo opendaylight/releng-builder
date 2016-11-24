e5050bc930fd7f99b288c80ef0c681530a4ac119
Jamo Luhrsen <jluhrsen@redhat.com>
2016-11-23 17:12:39 -0800

    archive openstack node logs individually, and not as a tarball
    Change-Id: Ie573cfde7fceb82b07f0b07ab5e5d4c5938511b3
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/include-raw-integration-deploy-openstack-run-test.sh
+++ jjb/integration/include-raw-integration-deploy-openstack-run-test.sh
@@ -333,25 +333,33 @@ do
     rm ${WORKSPACE}/odl${i}_karaf.log.tar
 done
 
-scp ${OPENSTACK_CONTROL_NODE_IP}:/opt/stack/devstack/nohup.out "openstack_control_stack.log"
+mkdir control
+scp ${OPENSTACK_CONTROL_NODE_IP}:/opt/stack/devstack/nohup.out "control/openstack_control_stack.log"
 ${SSH} ${OPENSTACK_CONTROL_NODE_IP} "tar -cf /tmp/control_node_openstack_logs.tgz  /opt/stack/logs/*"
 scp "${OPENSTACK_CONTROL_NODE_IP}:/tmp/control_node_openstack_logs.tgz" control_node_openstack_logs.tgz
-scp ${OPENSTACK_CONTROL_NODE_IP}:/var/log/openvswitch/ovs-vswitchd.log "ovs-vswitchd_control.log"
+tar -xvf control_node_openstack_logs.tgz -C control
+mv control ${WORKSPACE}/archives/
+rm control_node_openstack_logs.tgz
+scp ${OPENSTACK_CONTROL_NODE_IP}:/var/log/openvswitch/ovs-vswitchd.log "control/ovs-vswitchd_control.log"
 rm control_ovs-vswitchd.log
 scp extra_debug.sh ${OPENSTACK_CONTROL_NODE_IP}:/tmp
 ${SSH} ${OPENSTACK_CONTROL_NODE_IP} "bash /tmp/extra_debug.sh > /tmp/extra_debug.log"
-scp "${OPENSTACK_CONTROL_NODE_IP}:/tmp/extra_debug.log" extra_debug_control.log
+scp "${OPENSTACK_CONTROL_NODE_IP}:/tmp/extra_debug.log" control/extra_debug_control.log
 for i in `seq 1 $((NUM_OPENSTACK_SYSTEM - 1))`
 do
     OSIP=OPENSTACK_COMPUTE_NODE_${i}_IP
-    scp "${!OSIP}:/opt/stack/devstack/nohup.out" "openstack_compute_stack_${i}.log"
+    mkdir compute_${i}
+    scp "${!OSIP}:/opt/stack/devstack/nohup.out" "compute_${i}/openstack_compute_stack_${i}.log"
     ${SSH} "${!OSIP}" "tar -cf /tmp/compute_node_${i}_openstack_logs.tgz  /opt/stack/logs/*"
     scp "${!OSIP}:/tmp/compute_node_${i}_openstack_logs.tgz"  "compute_node_${i}_openstack_logs.tgz"
-    scp "${!OSIP}:/var/log/openvswitch/ovs-vswitchd.log" "ovs-vswitchd_compute_${i}.log"
+    tar -xvf compute_node_${i}_openstack_logs.tgz -C compute_${i}
+    mv compute_${i} ${WORKSPACE}/archives/
+    rm compute_node_${i}_openstack_logs.tgz
+    scp "${!OSIP}:/var/log/openvswitch/ovs-vswitchd.log" "compute_${i}/ovs-vswitchd_compute_${i}.log"
     rm compute_${1}_ovs-vswitchd.log
     scp "extra_debug.sh" "${!OSIP}:/tmp"
     ${SSH} "${!OSIP}" "bash /tmp/extra_debug.sh > /tmp/extra_debug.log"
-    scp "${!OSIP}:/tmp/extra_debug.log" "extra_debug_compute_${i}.log"
+    scp "${!OSIP}:/tmp/extra_debug.log" "compute_${i}/extra_debug_compute_${i}.log"
 done
 
 ls local.conf* | xargs -I % mv % %.log
