e4d651ceea28503d029c86510b17adda401efa08
Josh <jhershbe@redhat.com>
2016-11-30 18:41:26 +0200

    Images with pre-loaded devstack pips and yums
    This patch adds templates and a provisioning
    script to build images that are pre-installed
    with (a lot of) the various pip and yum pkgs
    required for running devstack.
    
    This aims to (hopefully) reduce the run time of CSIT.
    
    Change-Id: Ic837dd2ef7a96141130bb0d71f578cd06de5f1f4
    Signed-off-by: Josh <jhershbe@redhat.com>

---
+++ packer/provision/devstack-pre-pip.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo '---> Installing non-baseline requirements'
+yum install -q -y deltarpm python{,-{crypto,devel,lxml,setuptools}} \
+    @development {lib{xml2,xslt,ffi},openssl}-devel
+
+echo '---> Updating net link setup'
+if [ ! -f /etc/udev/rules.d/80-net-setup-link.rules ]; then
+    ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
+fi
+
+echo '---> Pre-installing yum and pip packages'
+projs="requirements keystone glance cinder neutron nova horizon"
+branch=${os_branch}
+
+yum install -y git wget
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+
+mkdir tmp
+cd tmp
+
+git clone https://github.com/openstack-dev/devstack.git
+(cd devstack && git checkout ${branch})
+sed -e 's/#.*//' devstack/files/rpms/general | xargs yum install -y
+
+base_url=https://github.com/openstack/
+for proj in $projs
+do
+    git clone ${base_url}${proj}
+    (cd ${proj} && git checkout ${branch})
+    pip install -c requirements/upper-constraints.txt -e ${proj}
+    pip install -c requirements/upper-constraints.txt -r ${proj}/test-requirements.txt
+done
+
+cd $OLDPWD
+rm -fr tmp
+
+# vim: sw=4 ts=4 sts=4 et :
