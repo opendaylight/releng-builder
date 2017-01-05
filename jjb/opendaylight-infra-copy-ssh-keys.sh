8c5215d08a9fc199bfc478b38be709d01409b3fa
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-01-04 23:45:20 -0500

    OpenStack Heat Orchestration Templates
    Use HOT [0] to instantiate CSIT instances in the public cloud. The idea
    here would be to store heat orchestration templates inside
    releng/builder and then use them to spin up the full CSIT labs in the
    public cloud via heat calls against the public cloud api.
    
    CSIT jobs will need to be rewritten to have a builder step that
    instantiates the CSIT lab similar to what JClouds provides today
    followed by a publisher step to clear up the lab at the end of the
    build.
    
    Will spin up nodes using the naming format ($JOB_NAME is actually a sum
    calculation (echo "$JOB_NAME" | sum | awk '{ print $1 }'):
    
        $JOB_NAME-$BUILD_NUMBER-{type}
    
    Stack name will simply be $JOB_NAME-$BUILD_NUMBER.
    
    This patch preserves the build conditions for JCLOUDS such that the
    common scripts work whether the job is using JClouds or Heat. We will
    want to remove the conditional statements once all ODL jobs have moved
    over to using Heat.
    
    [0] http://docs.openstack.org/developer/heat/template_guide/index.html
    
    Change-Id: I5cd7fc407119681c48885140ffc8ee594f8cab0c
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/opendaylight-infra-copy-ssh-keys.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+function copy-ssh-keys-to-slave() {
+    RETRIES=60
+    for j in $(seq 1 $RETRIES); do
+        if `ssh-copy-id -i /home/jenkins/.ssh/id_rsa.pub "jenkins@${i}" > /dev/null 2>&1`; then
+            ssh jenkins@${i} 'echo "$(facter ipaddress_eth0) $(/bin/hostname)" | sudo tee -a /etc/hosts'
+            echo "Successfully copied public keys to slave ${i}"
+            break
+        elif [ $j -eq $RETRIES ]; then
+            echo "SSH not responding on ${i} after $RETIRES tries. Giving up."
+            exit 1
+        else
+            echo "SSH not responding on ${i}. Retrying in 10 seconds..."
+            sleep 10
+        fi
+    done
+}
+
+# TODO: Remove condition when we no longer use JClouds plugin
+if [ -z "$JCLOUDS_IPS" ]; then
+    # If JCLOUDS_IPS is not set then we will spawn instances with
+    # OpenStack Heat.
+    sudo yum install -y jq
+    source $WORKSPACE/.venv-openstack/bin/activate
+    CONTROLLER_IPS=`openstack --os-cloud rackspace stack show -f json -c outputs $STACK_NAME | jq -r '.outputs[] | select(.output_key=="vm_0_ips") | .output_value[]'`
+    MININET_IPS=`openstack --os-cloud rackspace stack show -f json -c outputs $STACK_NAME | jq -r '.outputs[] | select(.output_key=="vm_1_ips") | .output_value[]'`
+    ADDR=($CONTROLLER_IPS $MININET_IPS)
+else
+    echo "OpenStack IPS are ${JCLOUDS_IPS}"
+    IFS=',' read -ra ADDR <<< "${JCLOUDS_IPS}"
+fi
+
+pids=""
+for i in "${ADDR[@]}"; do
+    ( copy-ssh-keys-to-slave ) &
+    # Store PID of process
+    pids+=" $!"
+done
+
+# Detect when a process failed to copy ssh keys and fail build
+for p in $pids; do
+    if wait $p; then
+        echo "Process $p successfully copied ssh keys."
+    else
+        echo "Process $p failed to copy ssh keys."
+        exit 1
+    fi
+done
+echo "Copying ssh keys complete."
