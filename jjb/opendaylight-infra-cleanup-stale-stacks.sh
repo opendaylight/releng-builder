f8dcb0766495de580fd59dcc210a654cf9504970
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-01-18 12:35:01 -0500

    Add job to cleanup stale stacks
    Sometimes a job might fail to delete the heat stack at the end of the
    job for an unknown reason. Add a cleanup job to run periodically and
    remove these stacks if found.
    
    This job is currently designed to run every 6 hrs.
    
    Change-Id: Ib486c108acc15cd58bc12cc473e934d25cb38457
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/opendaylight-infra-cleanup-stale-stacks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+virtualenv $WORKSPACE/.venv
+source $WORKSPACE/.venv/bin/activate
+pip install --upgrade pip
+pip install --upgrade python-openstackclient python-heatclient
+pip freeze
+
+DELETE_LIST=(`openstack --os-cloud rackspace stack list -f json | \
+              jq -r '.[] | \
+              select((."Stack Status" == "CREATE_FAILED") or \
+                     (."Stack Status" == "DELETE_FAILED")) | \
+              ."Stack Name"'`)
+for i in "${DELETE_LIST[@]}"; do
+   openstack --os-cloud rackspace stack delete --yes $i
+done
