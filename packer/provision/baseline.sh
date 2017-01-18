40b09d65a754725a6f4b01c6e590b8ce3b0c8141
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-01-17 22:45:59 -0500

    Add git-review to centos systems
    Change-Id: Ibd9a23c7064a3f5388553cb65cc483ca3c09ff4c
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- packer/provision/baseline.sh
+++ packer/provision/baseline.sh
@@ -55,7 +55,7 @@ EOF
     # separate group installs from package installs since a non-existing
     # group with dnf based systems (F21+) will fail the install if such
     # a group does not exist
-    yum install -y unzip xz puppet git perl-XML-XPath
+    yum install -y unzip xz puppet git git-review perl-XML-XPath
 
     # All of our systems require Java (because of Jenkins)
     # Install all versions of the OpenJDK devel but force 1.7.0 to be the
