2a6fdc332102ef194fbfde10aae0887c14a63313
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-01-18 15:22:39 -0500

    Allow stack 15 minutes to spawn before giving up
    Change-Id: I09be14d48fd4c86773866786455638dbbeca5ceb
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/opendaylight-infra-stack.sh
+++ jjb/opendaylight-infra-stack.sh
@@ -9,7 +9,7 @@ cd /builder/openstack-hot
 
 JOB_SUM=`echo $JOB_NAME | sum | awk '{{ print $1 }}'`
 VM_NAME="$JOB_SUM-$BUILD_NUMBER"
-openstack --os-cloud rackspace stack create --wait -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
+openstack --os-cloud rackspace stack create --wait --timeout 15 -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
 OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
 if [ "$OS_STATUS" != "CREATE_COMPLETE" ]; then
     echo "Failed to initialize infrastructure. Quitting..."
