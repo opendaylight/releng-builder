252bdd2e43415869176c834dfe2acaad7be650ad
Anil Belur <abelur@linuxfoundation.org>
2017-01-18 11:53:41 +1000

    Remove git-review install from autorelease spinup script
    Change-Id: I4d256cc26c84fbed7264011579fd3205da9d702c
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jenkins-scripts/autorelease.sh
+++ jenkins-scripts/autorelease.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Temporary measure until patch Ie03aa8a6364076690e5963b422c17e4feb70a6e2
-# is merged and images updated.
-yum install -y git-review
-
 # Format and mount rackspace data disk
 mkfs -t ext3 /dev/xvde1
 mkdir /w/workspace
