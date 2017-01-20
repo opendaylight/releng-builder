df0d9ef00e2349e7925d0e6d412389589f800468
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-01-19 22:28:50 -0500

    Fix docs-rtd curly brace escape
    Didn't realize in the previous patch
    I0ed5176a3216b6a115bd2bae527df4bb5b1d2c45 that we needed to escape curly
    braces but keep {rtdproject} as it's a JJB variable we're passing in.
    
    Change-Id: I7357262e12219c642f29d507087e5ea1a45496a6
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/docs/docs-post-rtd.sh
+++ jjb/docs/docs-post-rtd.sh
@@ -2,6 +2,6 @@
 if [ $GERRIT_BRANCH == "master" ]; then
     RTD_BUILD_VERSION=latest
 else
-    RTD_BUILD_VERSION=${GERRIT_BRANCH/\//-}
+    RTD_BUILD_VERSION=${{GERRIT_BRANCH/\//-}}
 fi
 curl -X POST --data "version_slug=$RTD_BUILD_VERSION" https://readthedocs.org/build/{rtdproject}
