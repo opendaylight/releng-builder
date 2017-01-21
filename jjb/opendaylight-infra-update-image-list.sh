10f924267361b2daaf3fdc2fcdd5bb4507497832
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-01-21 14:22:17 -0500

    Add job to generate an image list for docs
    With Heat now available we need a way to automate the list of available
    images projects can use in their jobs in the docs. This creates a job
    that will pull from openstack a list of available public images and
    pushes a patch automatically to Gerrit to update the relevant
    documentation page. The script will automatically detect if an unmerged
    image update patch already exists and update existing patch if available
    otherwise create a new one.
    
    Change-Id: If5ea2c53b21b06ab1ed5d6c9c17f30356a930dc4
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/opendaylight-infra-update-image-list.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+virtualenv $WORKSPACE/.venv
+source $WORKSPACE/.venv/bin/activate
+pip install --upgrade --quiet pip
+pip install --upgrade --quiet python-openstackclient python-heatclient
+pip freeze
+
+cat > $WORKSPACE/docs/cloud-images.rst << EOF
+Following are the list of published images available to be used with Jenkins jobs.
+
+EOF
+# Blank line before EOF is on purpose to ensure there is spacing.
+
+IFS=$'\n'
+IMAGES=(`openstack --os-cloud odlpriv image list --public -f value -c Name`)
+for i in ${IMAGES[@]}; do
+    echo "* $i" >> $WORKSPACE/docs/cloud-images.rst
+done
+
+git add docs/cloud-images.rst
