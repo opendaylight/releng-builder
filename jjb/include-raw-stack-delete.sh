e70edd61bd0a0b2bca18486dae6169fab4d2ab41
Guy Belotzerkovsky <guy@hpe.com>
2017-01-25 13:47:05 +0200

    move inline code to a shell script
    Change-Id: Id75f9143e93b30b94dccbc5eb3760aadb570420d
    Signed-off-by: Guy Belotzerkovsky <guy@hpe.com>

---
+++ jjb/include-raw-stack-delete.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -d "$WORKSPACE/.venv-openstack" ]; then
+	source $WORKSPACE/.venv-openstack/bin/activate
+	OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
+	if [ "$OS_STATUS" == "CREATE_COMPLETE" ] || [ "$OS_STATUS" == "CREATE_FAILED" ]; then
+		echo "Deleting $STACK_NAME"
+		openstack --os-cloud rackspace stack delete --yes $STACK_NAME
+	fi
+fi
