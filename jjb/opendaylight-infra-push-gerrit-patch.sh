ced8056ad126c3d625bf86af8a59839889a0cc31
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-01-25 15:13:27 -0800

    Add a macro opendaylight-infra-push-gerrit-patch
    This macro allows projects to push an automated patch to their project
    repo via Jenkins.
    
    This script requires the following JJB variables to be passed in:
         {project}       Gerrit project-name
         {gerrit-topic}  Gerrit topic, please make a unique topic.
         {gerrit-commit-message}  Commit message to assign to commit.
    
    Change-Id: I97a4eea5196ee512e33ee17cfe24307c04704ec9
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/opendaylight-infra-push-gerrit-patch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CHANGE_ID=`ssh -p 29418 jenkins-$SILO@git.opendaylight.org gerrit query \
+               limit:1 owner:self is:open project:{project} \
+               message:'{gerrit-commit-message}' \
+               topic:{gerrit-topic} | \
+               grep 'Change-Id:' | \
+               awk '{{ print $2 }}'`
+
+if [ -z "$CHANGE_ID" ]; then
+    git commit -sm "{gerrit-commit-message}"
+else
+    git commit -sm "{gerrit-commit-message}" -m "Change-Id: $CHANGE_ID"
+fi
+
+git status
+git remote add gerrit ssh://jenkins-$SILO@git.opendaylight.org:29418/releng/builder.git
+
+# Don't fail the build if this command fails because it's possible that there
+# is no changes since last update.
+git review --yes -t {gerrit-topic} || true
