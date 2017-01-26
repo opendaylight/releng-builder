0273cf6577adc02ffa3fc9e9863fd319380cf60c
Marcus G K Williams <marcus.williams@intel.com>
2017-01-25 17:48:07 -0800

    Add insert-csit-tests script
    Part of refactor of distribution jobs
    workflow. This auto updates
    csit to run in integration-test-jobs.yaml
    
    Change-Id: Ib989ce0cc1b206596c8b3cc4f069e5c714309f7f
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>

---
+++ jjb/integration/include-raw-integration-insert-csit-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+start='csit-list-carbon: >'
+finish='csit-list-boron: >'
+output=$(sed -e "/$start/,/$finish/{ /$start/{p; r csit_jobs_carbon.lst
+        }; /$finish/p; d }"  jjb/releng-defaults.yaml)
+echo "$output" > jjb/releng-defaults.yaml
+
+start='csit-list-boron: >'
+finish='csit-list-beryllium: >'
+output=$(sed -e "/$start/,/$finish/{ /$start/{p; r csit_jobs_boron.lst
+        }; /$finish/p; d }"  jjb/releng-defaults.yaml)
+echo "$output" > jjb/releng-defaults.yaml
+
+start='csit-list-beryllium: >'
+finish='# CSIT TESTS END SED MARKER'
+output=$(sed -e "/$start/,/$finish/{ /$start/{p; r csit_jobs_beryllium.lst
+        }; /$finish/p; d }"  jjb/releng-defaults.yaml)
+echo "$output" > jjb/releng-defaults.yaml
