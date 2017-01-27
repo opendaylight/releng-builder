8dae69af3e1f0361feb58f1d3687ab6c322a45da
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-01-26 17:26:09 -0800

    Display API limit before stacking
    Change-Id: I4f239b568ec46825e0ab7675deb423d3bf1f232b
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/opendaylight-infra-stack.sh
+++ jjb/opendaylight-infra-stack.sh
@@ -9,6 +9,7 @@ cd /builder/openstack-hot
 
 JOB_SUM=`echo $JOB_NAME | sum | awk '{{ print $1 }}'`
 VM_NAME="$JOB_SUM-$BUILD_NUMBER"
+openstack --os-cloud rackspace limits show --rate
 openstack --os-cloud rackspace stack create --wait --timeout 15 -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
 OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
 if [ "$OS_STATUS" != "CREATE_COMPLETE" ]; then
