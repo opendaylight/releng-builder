0f1b481e189909327b6b604281c8f5a907cf2a44
Anil Belur <abelur@linuxfoundation.org>
2017-02-01 07:36:19 +1000

    Improve Heat implementation error handling
    * Wait and query status in 1 minute loops when stack create
      status is CREATE_IN_PROGRESS with a timeout to 15 minutes
    * Continue with the job once stack create returns CREATE_COMPLETE
    * Notify publisher not delete stack when CREATE_IN_PROGRESS
    
    Change-Id: Ifdc927f601c07e519cdc502a2fb56fca138c659e
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/opendaylight-infra-stack.sh
+++ jjb/opendaylight-infra-stack.sh
@@ -10,8 +10,21 @@ cd /builder/openstack-hot
 JOB_SUM=`echo $JOB_NAME | sum | awk '{{ print $1 }}'`
 VM_NAME="$JOB_SUM-$BUILD_NUMBER"
 openstack --os-cloud rackspace stack create --wait --timeout 15 -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
-OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
-if [ "$OS_STATUS" != "CREATE_COMPLETE" ]; then
-    echo "Failed to initialize infrastructure. Quitting..."
-    exit 1
-fi
+
+for i in `seq 15`; do
+    OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
+    if [ "$OS_STATUS" == "CREATE_COMPLETE" ]; then
+        echo "Stack initialized on infrastructure successful."
+        break
+    elif [ "$OS_STATUS" == "CREATE_FAILED" ]; then
+        echo "ERROR: Failed to initialize infrastructure. Quitting..."
+        break
+    elif [ "$OS_STATUS" == "CREATE_IN_PROGRESS" ]; then
+        echo "Waiting to initialize infrastructure."
+        sleep 60
+        continue
+    fi
+done
+
+# notify publisher the stack create status
+echo OS_STATUS="$OS_STATUS" > $WORKSPACE/openstack_env.sh
