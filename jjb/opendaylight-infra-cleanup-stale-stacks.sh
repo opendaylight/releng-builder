329690941d0aaf1b6c554d1d42f5ddd16a6125f1
Anil Belur <abelur@linuxfoundation.org>
2017-02-01 07:36:19 +1000

    Improve Heat implementation error handling
    Using Heat has increased our ability to spawn 33% more robot builders
    but we're now hitting some issues with Rackspace that we will need to
    the error handling of our scripts for when Rackspace returns a status
    such as Error: None, or CREATE_IN_PROGRESS due to stacks taking longer
    than expected to spawn. Rackspace has told us we cannot delete stacks
    that are in CREATE_IN_PROGRESS or DELETE_IN_PROGRESS states and that we
    should exit our code carefully.
    
    This patch makes the following changes:
    
    * Wait and query stack create status every minute for 15 minutes
    * Check status is CREATE_IN_PROGRESS within timeout
    * Continue with the job once stack create returns CREATE_COMPLETE
    * Fail job on CREATE_FAILED and cleanup stack
    * Notify publisher not delete stack when CREATE_IN_PROGRESS
      or DELETE_IN_PROGRESS
    * Improve delete-stale-stacks to search for inactive stacks not being
      used by either releng or sandbox siloes and remove them.
    * Delete stacks job will now run every hour to cleanup orphaned systems
    
    Change-Id: Ifdc927f601c07e519cdc502a2fb56fca138c659e
    Also-by: Thanh Ha <thanh.ha@linuxfoundation.org>
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/opendaylight-infra-cleanup-stale-stacks.sh
+++ jjb/opendaylight-infra-cleanup-stale-stacks.sh
@@ -5,12 +5,40 @@ pip install --upgrade pip
 pip install --upgrade python-openstackclient python-heatclient
 pip freeze
 
-DELETE_LIST=(`openstack --os-cloud rackspace stack list -f json | \
-              jq -r '.[] | \
-                     select((."Stack Status" == "CREATE_FAILED") or \
-                            (."Stack Status" == "DELETE_FAILED")) | \
-                     ."Stack Name"'`)
-for i in "${DELETE_LIST[@]}"; do
-    echo "Deleting stack $i"
-    openstack --os-cloud rackspace stack delete --yes $i
+#########################
+## FETCH ACTIVE BUILDS ##
+#########################
+# Make sure we fetch active builds on both the releng and sandbox silos
+wget -q -O releng_builds.json https://jenkins.opendaylight.org/releng/computer/api/json?tree=computer[executors[currentExecutable[url]],oneOffExecutors[currentExecutable[url]]]&xpath=//url&wrapper=builds
+sleep 1  # Need to sleep for 1 second otherwise next line causes script to stall
+ACTIVE_BUILDS_RELENG=(` \
+    jq -r '.computer[].executors[].currentExecutable.url' releng_builds.json \
+    | grep -v null | awk -F'/' '{print $6 "-" $7}'`)
+
+wget -q -O sandbox_builds.json https://jenkins.opendaylight.org/sandbox/computer/api/json?tree=computer[executors[currentExecutable[url]],oneOffExecutors[currentExecutable[url]]]&xpath=//url&wrapper=builds
+sleep 1  # Need to sleep for 1 second otherwise next line causes script to stall
+ACTIVE_BUILDS_SANDBOX=(` \
+    jq -r '.computer[].executors[].currentExecutable.url' sandbox_builds.json \
+    | grep -v null | awk -F'/' '{print $6 "-" $7}'`)
+
+##########################
+## DELETE UNUSED STACKS ##
+##########################
+# Search for stacks taht are not in use by either releng or sandbox silos and
+# delete them.
+ACTIVE_BUILDS=(${ACTIVE_BUILDS_RELENG[@]} ${ACTIVE_BUILDS_SANDBOX[@]})
+OS_STACKS=(`openstack --os-cloud rackspace stack list \
+            -f json -c "Stack Name" -c "Stack Status" \
+            --property "stack_status=CREATE_COMPLETE" \
+            --property "stack_status=DELETE_FAILED" \
+            --property "stack_status=CREATE_FAILED" \
+            | jq -r '.[] | ."Stack Name"'`)
+for stack in ${OS_STACKS[@]}; do
+    if [[ "${ACTIVE_BUILDS[@]}" =~ $stack ]]; then
+        # No need to delete stacks if there exists an active build for them
+        continue
+    else
+        echo "Deleting orphaned stack: $stack"
+        openstack --os-cloud rackspace stack delete --yes $stack
+    fi
 done
