f1fc1f8308e2e5c1b5660e56a2589888da77af11
Anil Belur <abelur@linuxfoundation.org>
2017-02-01 07:36:19 +1000

    Improve Heat script to handle errors
    * Add a time wait for 5s to spawn up the nodes
    * Fail on Timeout
    * Fail on CREATE_FAILED
    * Continue on CREATE_IN_PROGRESS
    * Return on CREATE_COMPLETE
    
    Change-Id: Ifdc927f601c07e519cdc502a2fb56fca138c659e
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/opendaylight-infra-stack.sh
+++ jjb/opendaylight-infra-stack.sh
@@ -10,8 +10,20 @@ cd /builder/openstack-hot
 JOB_SUM=`echo $JOB_NAME | sum | awk '{{ print $1 }}'`
 VM_NAME="$JOB_SUM-$BUILD_NUMBER"
 openstack --os-cloud rackspace stack create --wait --timeout 15 -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
-OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
-if [ "$OS_STATUS" != "CREATE_COMPLETE" ]; then
-    echo "Failed to initialize infrastructure. Quitting..."
-    exit 1
-fi
+
+
+WAIT_TIME=0
+MAX_WAIT=5
+until OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'` || [ $WAIT_TIME -eq $MAX_WAIT ]; do
+   sleep $(( WAIT_TIME++ ))
+   if [ "$OS_STATUS" == "CREATE_COMPLETED" ]; then
+       echo "Stack initialized on infrastructure successful."
+       exit 0
+   elif [ "$OS_STATUS" == "CREATE_FAILED" ]; then
+       echo "ERROR: Failed to initialize infrastructure. Quitting..."
+       exit 1
+   elif [ "$OS_STATUS" == "CREATE_IN_PROGRESS" ]; then
+       echo "Waiting to initialize infrastructure ${{MAX_WAIT-WAIT_TIME}}"
+       continue
+   fi
+done
