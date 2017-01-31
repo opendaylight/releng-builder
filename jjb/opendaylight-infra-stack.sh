84a34267878276d751fa2e0b0a19c5813f3cc0d6
Anil Belur <abelur@linuxfoundation.org>
2017-02-01 07:36:19 +1000

    Improve Heat implementation error handling
    * Wait until the stack created or timed out
    * Fail on Timeout or CREATE_FAILED
    * Continue on CREATE_IN_PROGRESS
    * Return on CREATE_COMPLETE
    * Notify publisher not to run stack delete on failure
    
    Change-Id: Ifdc927f601c07e519cdc502a2fb56fca138c659e
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/opendaylight-infra-stack.sh
+++ jjb/opendaylight-infra-stack.sh
@@ -10,8 +10,29 @@ cd /builder/openstack-hot
 JOB_SUM=`echo $JOB_NAME | sum | awk '{{ print $1 }}'`
 VM_NAME="$JOB_SUM-$BUILD_NUMBER"
 openstack --os-cloud rackspace stack create --wait --timeout 15 -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
-OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
-if [ "$OS_STATUS" != "CREATE_COMPLETE" ]; then
-    echo "Failed to initialize infrastructure. Quitting..."
-    exit 1
+
+MAX_WAIT=30
+WAIT_TIME=1
+while true; do
+    ((WAIT_TIME++))
+    if [ $WAIT_TIME -lt $MAX_WAIT ]; then
+        OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
+        if [ "$OS_STATUS" == "CREATE_COMPLETE" ]; then
+            echo "Stack initialized on infrastructure successful."
+            exit 0
+        elif [ "$OS_STATUS" == "CREATE_FAILED" ]; then
+            echo "ERROR: Failed to initialize infrastructure. Quitting..."
+            break
+        elif [ "$OS_STATUS" == "CREATE_IN_PROGRESS" ]; then
+            let WAIT=${{MAX_WAIT-WAIT_TIME}}/2
+            echo "Waiting to initialize infrastructure: $WAIT minutes"
+            sleep 30
+            continue
+        fi
+    fi
+done
+
+# notify publisher on timeout or failure
+if [[ $? -ne 0 ]] || [[ $WAIT_TIME -eq $MAX_WAIT ]]; then
+    echo > "1" $WORKSPACE/tmp_stack_create_status
 fi
