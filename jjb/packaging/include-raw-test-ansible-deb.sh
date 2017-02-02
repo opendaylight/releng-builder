961a5dd08eecd82b4c622702781a08357d3b906e
Akshita Jha <zenith158@gmail.com>
2017-02-02 12:02:08 +0530

    Add job to install ODL via ansible-opendaylight
    Change-Id: I95d5b4dc4b943d86f451771e9553a155435253d7
    Signed-off-by: Akshita Jha <zenith158@gmail.com>

---
+++ jjb/packaging/include-raw-test-ansible-deb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+# Install required packages
+virtualenv rpm_build
+source rpm_build/bin/activate
+pip install --upgrade pip
+sudo apt-get install -y ansible
+
+# https://github.com/dfarrell07/vagrant-opendaylight#ansible-deployments
+git clone https://github.com/dfarrell07/vagrant-opendaylight.git
+pushd vagrant-opendaylight
+
+ansible-galaxy install -r requirements.yml -p provisioning/roles/ --force
+
+# Add more tests
