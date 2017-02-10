cfa95e1980f66d872f3aefbaeb04bd6fdd0b63a5
Sam Hague <shague@redhat.com>
2017-02-10 11:44:50 -0500

    virtualenv workaround to avoid 128 char length limitation
    This fix is based off a similar solution that OpenStack used.
    The solution is to call the python scripts via python
    itself. Doing this ignores the shebang line in the scripts.
    
    [1] https://review.openstack.org/399754
    
    Change-Id: Id38275dd9352bd5cdbcddf7a096b04902ee496c6
    Signed-off-by: Sam Hague <shague@redhat.com>
    
    virtualenv workaround to avoid 128 char length limitation
    
    Change-Id: I3b9cbf1c1a8dc49842fb615c166c4a383aebd7ec
    Signed-off-by: Sam Hague <shague@redhat.com>
    
    virtualenv workaround to avoid 128 char length limitation
    
    Change-Id: I3aaad756106adef07e50454ce344d803d7c92d11
    Signed-off-by: Sam Hague <shague@redhat.com>

--- jjb/opendaylight-infra-stack.sh
+++ jjb/opendaylight-infra-stack.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 virtualenv $WORKSPACE/.venv-openstack
 source $WORKSPACE/.venv-openstack/bin/activate
-pip install --upgrade pip
-pip install --upgrade python-openstackclient python-heatclient
-pip freeze
+PYTHON="$WORKSPACE/.venv-openstack/bin/python"
+OPENSTACK=$WORKSPACE/.venv-openstack/bin/openstack
+$PYTHON -m pip install --upgrade pip
+$PYTHON -m pip install --upgrade python-openstackclient python-heatclient
+$PYTHON -m pip freeze
 
 cd /builder/openstack-hot
 
@@ -13,13 +15,13 @@ VM_NAME="$JOB_SUM-$BUILD_NUMBER"
 # seq X refers to waiting for X minutes for OpenStack to return
 # a status that is not CREATE_IN_PROGRESS before giving up.
 OS_TIMEOUT=15  # Minutes to wait for OpenStack VM to come online
-openstack --os-cloud rackspace limits show --absolute
-openstack --os-cloud rackspace limits show --rate
-openstack --os-cloud rackspace stack create --timeout $OS_TIMEOUT -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
+$PYTHON $OPENSTACK --os-cloud rackspace limits show --absolute
+$PYTHON $OPENSTACK --os-cloud rackspace limits show --rate
+$PYTHON $OPENSTACK --os-cloud rackspace stack create --timeout $OS_TIMEOUT -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
 echo "Waiting for $OS_TIMEOUT minutes to create $STACK_NAME."
 for i in `seq $OS_TIMEOUT`; do
     sleep 60
-    OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
+    OS_STATUS=`$PYTHON $OPENSTACK --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
 
     case "$OS_STATUS" in
         CREATE_COMPLETE)
@@ -42,4 +44,4 @@ for i in `seq $OS_TIMEOUT`; do
 done
 
 # capture stack info in console logs
-openstack --os-cloud rackspace stack show $STACK_NAME
+$PYTHON $OPENSTACK --os-cloud rackspace stack show $STACK_NAME
