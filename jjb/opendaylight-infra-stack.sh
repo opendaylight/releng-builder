2e9b7d534b052b5e8bcdc88c1094f3cfde56b9b4
Jamo Luhrsen <jluhrsen@redhat.com>
2017-02-10 14:14:19 -0800

    Add retries to stacking
    Change-Id: I9d1c119ea4b60ee057b63ecb83192ff7fbbadf0f
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/opendaylight-infra-stack.sh
+++ jjb/opendaylight-infra-stack.sh
@@ -10,36 +10,62 @@ cd /builder/openstack-hot
 JOB_SUM=`echo $JOB_NAME | sum | awk '{{ print $1 }}'`
 VM_NAME="$JOB_SUM-$BUILD_NUMBER"
 
+OS_TIMEOUT=10  # Minutes to wait for OpenStack VM to come online
+STACK_RETRIES=3  # Number of times to retry creating a stack before fully giving up
+STACK_SUCCESSFUL=false
 # seq X refers to waiting for X minutes for OpenStack to return
 # a status that is not CREATE_IN_PROGRESS before giving up.
-OS_TIMEOUT=15  # Minutes to wait for OpenStack VM to come online
 openstack --os-cloud rackspace limits show --absolute
 openstack --os-cloud rackspace limits show --rate
 openstack --os-cloud rackspace stack create --timeout $OS_TIMEOUT -t {stack-template} -e $WORKSPACE/opendaylight-infra-environment.yaml --parameter "job_name=$VM_NAME" --parameter "silo=$SILO" $STACK_NAME
-echo "Waiting for $OS_TIMEOUT minutes to create $STACK_NAME."
-for i in `seq $OS_TIMEOUT`; do
-    sleep 60
-    OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
+echo "Trying up to $STACK_RETRIES times to create $STACK_NAME."
+for try in `seq $STACK_RETRIES`; do
+    echo "Waiting for $OS_TIMEOUT minutes to create $STACK_NAME."
+    for i in `seq $OS_TIMEOUT`; do
+        sleep 60
+        OS_STATUS=`openstack --os-cloud rackspace stack show -f json -c stack_status $STACK_NAME | jq -r '.stack_status'`
 
-    case "$OS_STATUS" in
-        CREATE_COMPLETE)
-            echo "Stack initialized on infrastructure successful."
-            break
-        ;;
-        CREATE_FAILED)
-            echo "ERROR: Failed to initialize infrastructure. Quitting..."
-            exit 1
-        ;;
-        CREATE_IN_PROGRESS)
-            echo "Waiting to initialize infrastructure."
-            continue
-        ;;
-        *)
-            echo "Unexpected status: $OS_STATUS"
-            exit 1
-        ;;
-    esac
+        case "$OS_STATUS" in
+            CREATE_COMPLETE)
+                echo "Stack initialized on infrastructure successful."
+                STACK_SUCCESSFUL=true
+                break
+            ;;
+            CREATE_FAILED)
+                echo "ERROR: Failed to initialize infrastructure. Deleting stack and possibly retrying to create..."
+                openstack --os-cloud rackspace stack delete --yes $STACK_NAME
+                # after stack delete, poll for 5m to know when stack is fully removed
+                for i in `seq 20`; do
+                  sleep 30;
+                  STACK_LIST=(`openstack --os-cloud rackspace stack list`)
+                  echo $STACK_LIST
+                  if [[ $STACK_LIST != "*$STACK_NAME*" ]]; then
+                    break
+                  fi
+                done
+                if [[ $STACK_LIST == "*$STACK_NAME*" ]]; then
+                  exit 1
+                fi
+                break
+            ;;
+            CREATE_IN_PROGRESS)
+                echo "Waiting to initialize infrastructure."
+                continue
+            ;;
+            *)
+                echo "Unexpected status: $OS_STATUS"
+                exit 1
+            ;;
+        esac
+    done
+    if $STACK_SUCCESSFUL; then
+        break
+    fi
 done
 
 # capture stack info in console logs
 openstack --os-cloud rackspace stack show $STACK_NAME
+
+if ! $STACK_SUCCESSFUL; then
+    exit 1
+fi
