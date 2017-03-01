6f8499b631fc2b21ca2fc0c5f96b146133f3c33d
Vratko Polak <vrpolak@cisco.com>
2017-03-01 17:43:38 +0100

    Bug 6932: Fix *-library job
    The fix is to store values sensitive to copypasting
    into (hopefully self-explainig) variables.
    
    In order to prevent future breakages,
    BRANCH variable has been renamed to DISTROBRANCH,
    and the corresponding macro
    has beem renamed to integration-distribution-branch.
    
    Change-Id: I4d81266b7069244ee336a9735f3cdf2d091f3fe6
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- jjb/integration/include-raw-integration-rebase-gerrit-patch.sh
+++ jjb/integration/include-raw-integration-rebase-gerrit-patch.sh
@@ -6,5 +6,5 @@ cd "${GERRIT_PROJECT}"
 export EMAIL="sandbox@jenkins.opendaylight.org"
 # TODO: Is there a more appropriate e-mail?
 export GIT_COMMITTER_NAME="Rebase Macro"
-git rebase "origin/${BRANCH}"
+git rebase "origin/${DISTROBRANCH}"
 cd "${WORKSPACE}"
