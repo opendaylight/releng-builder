9db29a1d66621fa8f500cab76ae381b4d4a8e00a
Vratko Polak <vrpolak@cisco.com>
2017-03-06 14:02:25 +0100

    Distribution check: Improve error reporting
    Change-Id: I5b7e9383c9e17dca294c1149893197cf78686b1e
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- jjb/integration-distribution-delete-snaphot-artifacts.sh
+++ jjb/integration-distribution-delete-snaphot-artifacts.sh
@@ -4,6 +4,6 @@ set +e  # To avoid failures in projects which generate zero snapshot artifacts.
 find /tmp/r/org/opendaylight/$GERRIT_PROJECT/ -path *-SNAPSHOT* -delete
 find /tmp/r/ -regex '.*/_remote.repositories\|.*/maven-metadata-local\.xml\|.*/maven-metadata-fake-nexus\.xml\|.*/resolver-status\.properties' -delete
 find /tmp/r/ -type d -empty -delete
-echo "# INFO: A listing of project related files left in local repository follows."
+echo "INFO: A listing of project related files left in local repository follows."
 find /tmp/r/org/opendaylight/$GERRIT_PROJECT/
 true  # To prevent the possibly non-zero return code from failing the job.
