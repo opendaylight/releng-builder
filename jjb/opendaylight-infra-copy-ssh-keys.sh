8548ae15b76e0b48f757b0aa9eea032cf717f332
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-08 17:09:52 -0500

    Fix ShellCheck issues in jjb/*.sh
    Change-Id: Id5868ed62501ba39f13552d72af0e6adb1631535
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/opendaylight-infra-copy-ssh-keys.sh
+++ jjb/opendaylight-infra-copy-ssh-keys.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 echo "----------> Copy ssh public keys to csit lab"
 
-source $WORKSPACE/.venv-openstack/bin/activate
+# shellcheck disable=SC1090
+source "$WORKSPACE/.venv-openstack/bin/activate"
 
 function copy-ssh-keys-to-slave() {
     RETRIES=60
     for j in $(seq 1 $RETRIES); do
-        if `ssh-copy-id -i /home/jenkins/.ssh/id_rsa.pub "jenkins@${i}" > /dev/null 2>&1`; then
-            ssh jenkins@${i} 'echo "$(facter ipaddress_eth0) $(/bin/hostname)" | sudo tee -a /etc/hosts'
+        if ssh-copy-id -i /home/jenkins/.ssh/id_rsa.pub "jenkins@${i}" > /dev/null 2>&1; then
+            ssh "jenkins@${i}" 'echo "$(facter ipaddress_eth0) $(/bin/hostname)" | sudo tee -a /etc/hosts'
             echo "Successfully copied public keys to slave ${i}"
             break
-        elif [ $j -eq $RETRIES ]; then
+        elif [ "$j" -eq $RETRIES ]; then
             echo "SSH not responding on ${i} after $RETIRES tries. Giving up."
             exit 1
         else
@@ -19,7 +20,7 @@ function copy-ssh-keys-to-slave() {
         fi
 
         # ping test to see if connectivity is available
-        if ping -c1 ${i} &> /dev/null; then
+        if ping -c1 "${i}" &> /dev/null; then
             echo "Ping to ${i} successful."
         else
             echo "Ping to ${i} failed."
@@ -29,12 +30,12 @@ function copy-ssh-keys-to-slave() {
 
 # Print the Stack outputs parameters so that we can identify which IPs belong
 # to which VM types.
-openstack --os-cloud rackspace stack show -c outputs $STACK_NAME
+openstack --os-cloud rackspace stack show -c outputs "$STACK_NAME"
 
-ADDR=(`openstack --os-cloud rackspace stack show -f json -c outputs $STACK_NAME | \
-       jq -r '.outputs[] | \
-              select(.output_key | match("^vm_[0-9]+_ips\$")) | \
-              .output_value | .[]'`)
+ADDR=$(openstack --os-cloud rackspace stack show -f json -c outputs "$STACK_NAME" | \
+       jq -r '.outputs[] |
+              select(.output_key | match("^vm_[0-9]+_ips\$")) |
+              .output_value | .[]')
 pids=""
 for i in "${ADDR[@]}"; do
     ( copy-ssh-keys-to-slave ) &
@@ -44,7 +45,7 @@ done
 
 # Detect when a process failed to copy ssh keys and fail build
 for p in $pids; do
-    if wait $p; then
+    if wait "$p"; then
         echo "Process $p successfully copied ssh keys."
     else
         echo "Process $p failed to copy ssh keys."
