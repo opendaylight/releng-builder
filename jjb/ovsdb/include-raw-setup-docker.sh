37e85de45e6ed5b8c7258e7a6a3a39016452cab2
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-08 16:35:34 -0500

    Fix ShellCheck issues in jjb/ovsdb
    Change-Id: I34a8bfee5d3c4205bce739f607836f07f3e949c3
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/ovsdb/include-raw-setup-docker.sh
+++ jjb/ovsdb/include-raw-setup-docker.sh
@@ -19,12 +19,12 @@ done
 
 
 echo "---> Starting OVS $OVS_VERSION"
-/usr/bin/docker pull vpickard/openvswitch:$OVS_VERSION
-CID=$(/usr/bin/docker run -p 6641:6640 --privileged=true -d -i -t vpickard/openvswitch:$OVS_VERSION /usr/bin/supervisord)
-REALCID=`echo $CID | rev | cut -d ' ' -f 1 | rev`
+/usr/bin/docker pull "vpickard/openvswitch:$OVS_VERSION"
+CID=$(/usr/bin/docker run -p 6641:6640 --privileged=true -d -i -t "vpickard/openvswitch:$OVS_VERSION" /usr/bin/supervisord)
+REALCID=$(echo "$CID" | rev | cut -d ' ' -f 1 | rev)
 echo "CID=$REALCID" > env.properties
 echo "OVS_VERSION=${OVS_VERSION}" >> env.properties
-CONTROLLER_IP=`facter ipaddress`
+CONTROLLER_IP=$(facter ipaddress)
 echo "CONTROLLER_IP=${CONTROLLER_IP}" >> env.properties
 
 echo "---> Waiting..."
