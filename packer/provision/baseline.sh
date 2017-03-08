c0b357c57f72e95de58760a9bd8a72f4df3adf2d
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-08 16:59:38 +0000

    Revert "Add workaround to Kernel panic issue on CentOS images"
    This reverts commit f77652640ed99537c4ebc5eb3199dc886c9d5b32.
    
    Change-Id: Ib498a610fdd6f082575495d1b56a57b5f1802321
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- packer/provision/baseline.sh
+++ packer/provision/baseline.sh
@@ -50,12 +50,7 @@ EOF
     echo "---> Updating operating system"
     yum clean all
     yum install -y deltarpm
-
-    # Workaround for kernel panic issue that appears sometimes after kernel update
-    #     https://www.centos.org/forums/viewtopic.php?t=22425
-    yum remove -y kernel
     yum update -y
-    yum install -y kernel
 
     # add in components we need or want on systems
     echo "---> Installing base packages"
