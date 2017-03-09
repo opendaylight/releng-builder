d47ae33235868691c7e0cc49844079616eaabea0
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-08 22:11:46 -0500

    Fix ShellCheck issues in cleanup scripts
    Change-Id: I58f020ba7d34d2cfeffaba5a3a30c3c2087e57b2
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/integration/include-raw-integration-cleanup-workspace.sh
+++ jjb/integration/include-raw-integration-cleanup-workspace.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 echo "Cleaning up the workspace..."
 
 # Leftover files from previous runs could be wrongly copied as results.
 # Keep the cloned integration/test repository!
-for file_or_dir in `ls -A -1 -I "test"`
+for file_or_dir in *
 # FIXME: Make this compatible with multipatch and other possible build&run jobs.
 do
-  rm -vrf "$file_or_dir"
+    if [ "$file_or_dir" != "test" ]; then
+        rm -vrf "$file_or_dir"
+    fi
 done
