5f9418413d156e8fcb0261b5ecdf6a2c80ed2ba3
Anil Belur <abelur@linuxfoundation.org>
2017-03-10 10:00:37 +1000

    WIP: Improve autorelease notify
    - Search console log mvn goals to retrive failed ARIFACT_ID
    - Determine project mailing list with xmlstarlet using xpaths
    - If ARTIFACT_ID is same across project groups, then determine
      all project groups list
    
    Change-Id: Id2b9f1abd08792cc772f2c375c21d0321de419ad
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/autorelease/include-raw-autorelease-notify-build-failure.sh
+++ jjb/autorelease/include-raw-autorelease-notify-build-failure.sh
@@ -17,36 +17,45 @@ STREAM=${JOB_NAME#*-*e-}
 # get console logs
 wget -O "$CONSOLE_LOG" "${BUILD_URL}consoleText"
 
-# extract the failing project or artifactid
-REACTOR_INFO=$(awk '/Reactor Summary:/ { flag=1 }
-          flag {
-             if ( sub(/^\[(INFO)\]/,"") && sub(/FAILURE \[.*/,"") ) {
-                 gsub(/[[:space:]]*::[[:space:]]*/,"::")
-                 gsub(/^[[:space:]]+|[[:space:]]+$|[.]/,"")
-                 print
-             }
-          }
-          /Final Memory:/ { flag=0 }' $CONSOLE_LOG)
+# determine ARTIFACT_ID
+ARTIFACT_ID=$(awk -F: '/\[ERROR\].*mvn <goals> -rf :/ { print $2}' $CONSOLE_LOG)
 
-# check for project format
-if [[ ${REACTOR_INFO} =~ .*::*.*::*. ]]; then
-    # extract project and artifactid from full format
-    ODL=$(echo "${REACTOR_INFO}" | awk -F'::' '{ gsub(/^[ \t]+|[ \t]+$/, "", $1); print $1 }')
-    PROJECT=$(echo "${REACTOR_INFO}" | awk -F'::' '{ gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2 }')
-    ARTIFACTID=$(echo "${REACTOR_INFO}" | awk -F'::' '{ gsub(/^[ \t]+|[ \t]+$/, "", $3); print $3 }')
+# determine project mailing list using xpaths
+# if project.groupId:
+#     project.groupId is set and is not inherited
+# else if project.parent.groupId:
+#     project.groupId is not set but IS inherited from project.parent.groupId
+# else
+#     exclude project mailing list
+grouplist=()
+for p in $(find . -name pom.xml); do
+    GROUP=$(xmlstarlet sel\
+          -N "x=http://maven.apache.org/POM/4.0.0"\
+          -t -m "/x:project[x:artifactId='$ARTIFACT_ID']"\
+          --if "/x:project/x:groupId"\
+          -v "/x:project/x:groupId"\
+          --elif "/x:project/x:parent/x:groupId"\
+          -v "/x:project/x:parent/x:groupId"\
+          --else -o ""\
+          "$p" 2>/dev/null)
+    if [ ! -z "${GROUP}" ]; then
+        grouplist+=($(echo "${GROUP}" | awk -F'.' '{ print $3 }'))
+    fi
+done
+
+if [ "${#grouplist[@]}" -eq 1 ]; then
+    PROJECT="${grouplist[0]}"
 else
-    # set ARTIFACTID to partial format
-    ODL=""
-    PROJECT=""
-    ARTIFACTID=$(echo "${REACTOR_INFO}" | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+    GROUPLIST="NOTE: The artifactId:$ARTIFACT_ID is used for projects:\
+              ${grouplist[*]}."
 fi
 
 # Construct email subject & body
 PROJECT_STRING=${PROJECT:+" from $PROJECT"}
-SUBJECT="[release] Autorelease $STREAM failed to build $ARTIFACTID$PROJECT_STRING"
+SUBJECT="[release] Autorelease $STREAM failed to build $ARTIFACT_ID$PROJECT_STRING"
 BODY="Attention ${PROJECT:-"OpenDaylight"}-devs,
 
-Autorelease $STREAM failed to build $ARTIFACTID$PROJECT_STRING in build
+Autorelease $STREAM failed to build $ARTIFACT_ID$PROJECT_STRING in build
 $BUILD_NUMBER. Attached is a snippet of the error message related to the
 failure that we were able to automatically parse as well as console logs.
 
@@ -55,7 +64,7 @@ https://logs.opendaylight.org/$SILO/$ARCHIVES_DIR
 
 Jenkins Build:
 $BUILD_URL
-
+${PROJECT:+"$GROUPLIST"}
 Please review and provide an ETA on when a fix will be available.
 
 Thanks,
@@ -66,11 +75,11 @@ ODL releng/autorelease team
 BUILD_STATUS=$(awk '/\[INFO\] Remote staging finished/{flag=1;next}/Total time:/{flag=0}flag' $CONSOLE_LOG \
                    | grep '\] BUILD' | awk '{print $3}')
 
-if [ ! -z "${ARTIFACTID}" ] && [[ "${BUILD_STATUS}" != "SUCCESS" ]]; then
+if [ ! -z "${ARTIFACT_ID}" ] && [[ "${BUILD_STATUS}" != "SUCCESS" ]]; then
     # project search pattern should handle both scenarios
-    # 1. Full format:    ODL :: $PROJECT :: $ARTIFACTID
-    # 2. Partial format: Building $ARTIFACTID
-    sed -e "/\[INFO\] Building \(${ARTIFACTID} \|${ODL} :: ${PROJECT} :: ${ARTIFACTID} \)/,/Reactor Summary:/!d;//d" \
+    # 1. Full format:    ODL :: $PROJECT :: $ARTIFACT_ID
+    # 2. Partial format: Building $ARTIFACT_ID
+    sed -e "/\[INFO\] Building \(${ARTIFACT_ID} \|ODL :: ${PROJECT} :: ${ARTIFACT_ID} \)/,/Reactor Summary:/!d;//d" \
           $CONSOLE_LOG > /tmp/error.txt
 
     if [ -n "${PROJECT}" ]; then
