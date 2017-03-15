ce52a3130d038c23aa07abbdd5e4d9b8e3f95122
Daniel Farrell <dfarrell@redhat.com>
2017-03-14 20:32:49 -0400

    Only send autorelease failure email in releng silo
    Don't send emails to release and project lists during sandbox testing.
    
    TODO: Don't force-fail and uncomment email send before merging.
    
    Change-Id: I595b23f1314f9c6ab2e92b6de4658842ab679400
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- jjb/autorelease/include-raw-autorelease-notify-build-failure.sh
+++ jjb/autorelease/include-raw-autorelease-notify-build-failure.sh
@@ -77,9 +77,20 @@ if [ ! -z "${ARTIFACTID}" ] && [[ "${BUILD_STATUS}" != "SUCCESS" ]]; then
         RELEASE_EMAIL="${RELEASE_EMAIL}, ${PROJECT}-dev@opendaylight.org"
     fi
 
-    echo "${BODY}" | mail -a /tmp/error.txt \
-        -r "Jenkins <jenkins-dontreply@opendaylight.org>" \
-        -s "${SUBJECT}" "${RELEASE_EMAIL}"
+    # Only send emails in production (releng), not testing (sandbox)
+    if [ "${SILO}" == "releng" ]; then
+        # TODO: Uncomment this before committing, don't want to email during testing
+        echo "Would actually email here, skipping for testing"
+        #echo "${BODY}" | mail -a /tmp/error.txt \
+        #    -r "Jenkins <jenkins-dontreply@opendaylight.org>" \
+        #    -s "${SUBJECT}" "${RELEASE_EMAIL}"
+    elif [ "${SILO}" == "sandbox" ]; then
+        echo "Running in sandbox, not actually sending notification emails"
+        echo "Subject: ${SUBJECT}"
+        echo "Body: ${BODY}"
+    else
+        echo "Not sure how to notify in \"${SILO}\" Jenkins silo"
+    fi
 fi
 
 rm $CONSOLE_LOG
