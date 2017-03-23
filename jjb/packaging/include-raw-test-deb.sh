8de384d055b9387d126b34a0956e085b5aae3843
Akshita Jha <zenith158@gmail.com>
2017-03-23 14:58:48 +0530

    Add jenkins job to test deb
    Change-Id: I33896dc3cfae5a05733bb6adab85307f914f538a
    Signed-off-by: Akshita Jha <zenith158@gmail.com>

---
+++ jjb/packaging/include-raw-test-deb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+# Install ODL from .deb link or .repo url
+if [[ $URL == *.deb ]]
+then
+  sudo apt-get install -y "$URL"
+elif [[ $URL == ppa:* ]]
+then
+  sudo add-apt-repository "$REPO_FILE"
+  sudo apt-get update
+  sudo apt-get install -y opendaylight
+else
+  echo "URL is not a link to a PPA repo or .deb"
+fi
+
+# Install expect to interact with karaf shell
+sudo apt-get install -y expect
+
+# Start OpenDaylight
+sudo systemctl start opendaylight
+
+# Check status of OpenDaylight
+sudo systemctl status opendaylight
+
+# Get process id of Java
+pgrep java
