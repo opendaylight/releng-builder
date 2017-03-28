538393148763029a8ab62c22358314895eff422e
rahul.agarwal <rahul.agarwal3@tcs.com>
2017-03-28 22:42:07 +0530

    Change for creating images with quagga.
    This contains the following images-
    java-builder image with l3vpn functionality for quagga
    java-builder image with evpn functionality for quagga
    Tool System with l3vpn functionality for quagga
    Tool System with evpn functionality for quagga
    
    Change-Id: Ibfe5b941ea2f31731ad10fbafa54daeb66f18878
    Signed-off-by: rahul.agarwal <rahul.agarwal3@tcs.com>

---
+++ packer/provision/java-builder-quagga-evpn.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# vim: sw=4 ts=4 sts=4 et tw=72 :
+
+# Force any errors to cause the script and job to end in failure
+set -xeu -o pipefail
+
+# The following packages are not needed by all projects, but they are
+# needed by enough to make them useful everywhere
+yum install -y @development perl-{Digest-SHA,ExtUtils-MakeMaker} \
+    ant {boost,gtest,json-c,libcurl,libxml2,libvirt,openssl}-devel \
+    {readline,unixODBC}-devel yum-utils fedora-packager \
+    libxslt-devel crudini
+
+# Install python3 and dependencies, needed for Coala linting at least
+yum install -y python34
+yum install -y python34-{devel,virtualenv,setuptools,pip}
+
+# Install python dependencies, useful generally
+yum install -y python-{devel,virtualenv,setuptools,pip}
+
+# Needed by autorelease scripts
+yum install -y xmlstarlet
+
+# Needed by docs project
+yum install -y graphviz
+
+# Needed by deploy test
+yum install -y sshpass
+
+# tcpmd5 is wanting to do 32bit ARM cross-compilation and is specifically
+# requesting the following be installed (note the kernel headers are
+# going to be the x86_64 package as there aren't separate 32bit and
+# x86_64 packages for them
+yum install -y glibc-devel.i686 kernel-headers
+
+# Needed by opendove
+yum install -y {jansson,libevent,libnl,libuuid}-devel
+
+# Needed for vsemprovider build in vtn project to enable C# compilation.
+rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+# Add the mono tools repository
+yum-config-manager -y --add-repo http://download.mono-project.com/repo/centos/
+# Install the mono toolchain and nuget
+yum -y install mono-complete nuget
+
+# Needed by TSDR
+echo "Installing the Hbase Server..."
+mkdir /tmp/Hbase
+cd /tmp/Hbase
+wget --no-verbose http://apache.osuosl.org/hbase/hbase-0.94.27/hbase-0.94.27.tar.gz
+tar -xvf hbase-0.94.27.tar.gz
+
+# Needed by TSDR
+echo "Installing the Cassandra Server..."
+mkdir /tmp/cassandra
+cd /tmp/cassandra
+wget --no-verbose http://apache.osuosl.org/cassandra/2.1.16/apache-cassandra-2.1.16-bin.tar.gz
+tar -xvf apache-cassandra-2.1.16-bin.tar.gz
+
+# Generally useful for all projects
+echo "Installing the Elasticsearch node..."
+mkdir /tmp/elasticsearch
+cd /tmp/elasticsearch
+wget --no-verbose https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.5.tar.gz
+tar -xvzf elasticsearch-1.7.5.tar.gz
+
+# Installs Hashicorp's Packer binary, required for {verify,merge}-packer jobs
+mkdir /tmp/packer
+cd /tmp/packer
+wget https://releases.hashicorp.com/packer/0.12.2/packer_0.12.2_linux_amd64.zip
+unzip packer_0.12.2_linux_amd64.zip -d /usr/local/bin/
+# rename packer to avoid conflict with binary in cracklib
+mv /usr/local/bin/packer /usr/local/bin/packer.io
+
+# Check out, compile and install quagga for EVPN functionalities
+git clone https://github.com/6WIND/zrpcd.git
+cd zrpcd
+chmod 777 pkgsrc/dev_compile_script.sh
+bash pkgsrc/dev_compile_script.sh -d -b -t -v 2
---
+++ packer/provision/mininet-ovs-2.5-quagga-l3vpn.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -x
+
+# vim: sw=4 ts=4 sts=4 et tw=72 :
+
+# force any errors to cause the script and job to end in failure
+set -xeu -o pipefail
+
+# Ensure that necessary variables are set to enable noninteractive mode in
+# commands.
+export DEBIAN_FRONTEND=noninteractive
+
+# To handle the prompt style that is expected all over the environment
+# with how use use robotframework we need to make sure that it is
+# consistent for any of the users that are created during dynamic spin
+# ups
+echo 'PS1="[\u@\h \W]> "' >> /etc/skel/.bashrc
+
+echo '---> Install OpenVSwitch 2.5.0'
+apt-get update -m
+apt-get install openvswitch-switch openvswitch-vtep
+
+echo '---> Installing mininet'
+apt-get install mininet
+
+echo '---> Installing build pre-requisites'
+apt-get install build-essential snmp libsnmp-dev snmpd libpcap-dev \
+autoconf make automake libtool libconfig-dev libssl-dev libffi-dev libssl-doc pkg-config
+
+git clone https://github.com/intracom-telecom-sdn/mtcbench.git
+mtcbench/deploy/docker/provision.sh
+# TODO: remove workaround for build issue with mtcbench
+# when mtcbench dependency build correctly
+# https://github.com/intracom-telecom-sdn/mtcbench/issues/10
+mtcbench/build_mtcbench.sh || true
+cd mtcbench/oflops/cbench
+make
+cp cbench /usr/local/bin/
+
+echo '---> Installing exabgp'
+apt-get install exabgp
+
+echo '---> All Python package installation should happen in virtualenv'
+apt-get install python-virtualenv python-pip
+
+# Install vlan for vlan based tests in VTN suites
+apt-get install vlan
+
+# Install netaddr package which is needed by some custom mininet topologies
+apt-get install python-netaddr
+
+# Check out, compile and install quagga for L3VPN functionalities
+git clone https://github.com/6WIND/zrpcd.git
+cd zrpcd
+chmod 777 pkgsrc/dev_compile_script.sh
+bash pkgsrc/dev_compile_script.sh -d -b -t
