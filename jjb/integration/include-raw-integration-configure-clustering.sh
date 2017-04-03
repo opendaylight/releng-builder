135f8b2aac253ef830fd878a586648ad23606669
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-04-03 00:30:29 -0700

    Fix typo in non-verbose parameter
    Change-Id: I978c68fe7b1acc456e7e5f30d41dbe291de69b81
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/integration/include-raw-integration-configure-clustering.sh
+++ jjb/integration/include-raw-integration-configure-clustering.sh
@@ -64,7 +64,7 @@ echo "Changing to /tmp"
 cd /tmp
 
 echo "Downloading the distribution from ${ACTUALBUNDLEURL}"
-wget --no-verbose --show-progress --progress=dot:giga '${ACTUALBUNDLEURL}'
+wget --non-verbose --show-progress --progress=dot:giga '${ACTUALBUNDLEURL}'
 
 echo "Extracting the new controller..."
 unzip -q ${BUNDLE}
