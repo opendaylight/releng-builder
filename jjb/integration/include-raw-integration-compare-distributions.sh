ac7255fd0ea6501ff99b851aad94ede96ea2a727
Vratko Polak <vrpolak@cisco.com>
2017-04-17 16:18:51 +0200

    Make jobs karaf-version specific, some duplicities
    + Affected jobs: distribution-check, CSIT (all kinds), deploy, offline.
    + Carbon CSIT picks one version, other affected run both.
    + Introduced KARAF_VERSION bash variable.
    + Macro to set that variable.
    + Default value set to 'karaf4'.
    + Pre-carbon branches use 'karaf3'.
    + Scripts -bundle-vars, -distribution-check, -deploy, -offline renamed.
    + Macros which include (some of) them also renamed to match.
    + The bundle script sets artifact name and path from karaf version.
    + concurrent:false for deploy and offline.
    + Description comments moved inside to not repeat the name.
    + Used by field dropped, Argumens added (TODOs left).
    + Random bash linting improvements.
    - Code de-duplication left for another time.
    Blocker:
    - Timed trigger disable, triggering job not in place yet.
    
    FIXME: Update commit message to recent edits.
    
    Change-Id: I85c71a7ea94b9383b932d65b496056865bcecf78
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- jjb/integration/include-raw-integration-compare-distributions.sh
+++ jjb/integration/include-raw-integration-compare-distributions.sh
@@ -16,7 +16,7 @@ wget "http://${GERRIT_PATH}/gitweb?p=integration/distribution.git;a=blob_plain;f
 BUNDLEVERSION=$(xpath pom.xml '/project/version/text()' 2> /dev/null)
 echo "Bundle version is ${BUNDLEVERSION}"
 # Acquire the timestamp information from maven-metadata.xml
-NEXUSPATH="${NEXUSURL_PREFIX}/${ODL_NEXUS_REPO}/org/opendaylight/integration/distribution-karaf"
+NEXUSPATH="${NEXUSURL_PREFIX}/${ODL_NEXUS_REPO}/org/opendaylight/integration/${KARAF_ARTIFACT}"
 wget ${NEXUSPATH}/${BUNDLEVERSION}/maven-metadata.xml
 
 if [ $? -ne 0 ]; then
@@ -27,11 +27,11 @@ fi
 less maven-metadata.xml
 TIMESTAMP=$(xpath maven-metadata.xml "//snapshotVersion[extension='zip'][1]/value/text()" 2>/dev/null)
 echo "Nexus timestamp is ${TIMESTAMP}"
-BUNDLEFOLDER="distribution-karaf-${BUNDLEVERSION}"
-BUNDLE="distribution-karaf-${TIMESTAMP}.zip"
-ACTUALBUNDLEURL="${NEXUSPATH}/${BUNDLEVERSION}/${BUNDLE}"
+BUNDLEFOLDER="${KARAF_ARTIFACT}-${BUNDLEVERSION}"
+BUNDLE="${KARAF_ARTIFACT}-${TIMESTAMP}.zip"
+ACTUAL_BUNDLE_URL="${NEXUSPATH}/${BUNDLEVERSION}/${BUNDLE}"
 
-wget --progress=dot:mega $ACTUALBUNDLEURL
+wget --progress=dot:mega $ACTUAL_BUNDLE_URL
 echo "Extracting the last distribution found on nexus..."
 unzip -q $BUNDLE
 mv $BUNDLEFOLDER /tmp/distro_old
@@ -54,5 +54,5 @@ mkdir -p $WORKSPACE/archives
 # The file/report to be archived will only list the distribution in the comparison and the patches that
 # are different.
 python distcompare.py -r ssh://jenkins-$SILO@git.opendaylight.org:29418 | tee /tmp/dist_diff.txt
-echo -e "Patch differences listed are in comparison to:\n\t$ACTUALBUNDLEURL\n\n" > $WORKSPACE/archives/distribution_differences.txt
+echo -e "Patch differences listed are in comparison to:\n\t$ACTUAL_BUNDLE_URL\n\n" > $WORKSPACE/archives/distribution_differences.txt
 sed -ne '/Patch differences/,$ p' /tmp/dist_diff.txt >> $WORKSPACE/archives/distribution_differences.txt
