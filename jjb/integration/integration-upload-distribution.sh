710c7c61b5b8b87bd2595b60225489c048b907f4
Vratko Polak <vrpolak@cisco.com>
2017-04-17 13:06:02 +0200

    Make jobs karaf-version specific, some duplicities
    + Affected jobs: distribution-check, CSIT (all kinds), deploy, offline.
    + Carbon CSIT picks one version, other affected run both.
    + Introduced KARAF_VERSION bash variable.
    + Macro to set that variable.
    + Default value set to 'karaf4'.
    + Pre-carbon branches use 'karaf3'.
    + Scripts -bundle-vars, -distribution-check, -deploy, -offline renamed.
    + Macros which include (some of) them also renamed to match.
    + The bundle script sets artifact name and path from karaf version.
    + concurrent:false for deploy and offline.
    + Description comments moved inside to not repeat the name.
    + Used by field dropped, Argumens added (TODOs left).
    + Random bash linting improvements.
    - Code de-duplication left for another time.
    Blocker:
    - Timed trigger disable, triggering job not in place yet.
    
    FIXME: Update commit message to recent edits.
    
    Change-Id: I85c71a7ea94b9383b932d65b496056865bcecf78
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- jjb/integration/integration-upload-distribution.sh
+++ jjb/integration/integration-upload-distribution.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -xeu -o pipefail
 
+BUNDLE_VERSION=$(xpath "${BUNDLE_POM}" '/project/version/text()' 2> /dev/null)
+BUNDLE="${KARAF_ARTIFACT}-${BUNDLE_VERSION}.zip"
+BUNDLE_URL="/tmp/r/org/opendaylight/integration/${KARAF_ARTIFACT}/${BUNDLE_VERSION}/${BUNDLE}"
+
 echo "Uploading distribution to Nexus..."
 "$MVN" deploy:deploy-file \
-    --log-file deploy-karaf-distribution.log \
+    --log-file integration-upload-distribution.log \
     --global-settings "$GLOBAL_SETTINGS_FILE" \
     --settings "$SETTINGS_FILE" \
     -Dfile="$BUNDLE_URL" \
     -DrepositoryId=opendaylight-snapshot \
     -Durl="$ODLNEXUSPROXY/content/repositories/opendaylight.snapshot/" \
     -DgroupId="org.opendaylight.integration.${GERRIT_PROJECT//\//.}" \
-    -DartifactId=distribution-karaf \
+    -DartifactId=${KARAF_ARTIFACT} \
     -Dversion="$BUNDLE_VERSION" \
     -Dpackaging=zip
 
-BUNDLE_URL=$(grep "Uploaded.*distribution-karaf/$BUNDLE_VERSION.*.zip" deploy-karaf-distribution.log | awk '{print $3}')
+BUNDLE_URL=$(grep "Uploaded.*${KARAF_ARTIFACT}/$BUNDLE_VERSION.*.zip" integration-upload-distribution.log | awk '{print $3}')
 echo "Bundle uploaded to $BUNDLE_URL"
 
 # Re-inject the new BUNDLE_URL for downstream jobs to pull from Nexus
-env | grep BUNDLE_ | sort | tee deploy-distribution.env
+env | grep BUNDLE_ | sort | tee integration-upload-distribution.env
