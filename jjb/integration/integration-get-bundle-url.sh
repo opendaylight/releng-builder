93b5ebd181583e8cd555e57bc5d3b1de7ae54406
Vratko Polak <vrpolak@cisco.com>
2017-04-17 11:20:56 +0200

    Make jobs karaf-version specific, some duplicities
    + Affected jobs: distribution-check, CSIT (all kinds), deploy, offline.
    + Carbon CSIT picks one version, other affected run both.
    + Introduced KARAF_VERSION bash variable.
    + Macro to set that variable.
    + Default value set to 'karaf4'.
    + Pre-carbon branches use 'karaf3'.
    + Scripts -bundle-vars, -distribution-check, -deploy, -offline renamed.
    + Macros which include (some of) them also renamed to match.
    + The bundle script sets artifact name and path from karaf version.
    + concurrent:false for deploy and offline.
    + Description comments moved inside to not repeat the name.
    + Used by field dropped, Argumens added (TODOs left).
    + Random bash linting improvements.
    - Code de-duplication left for another time.
    Blocker:
    - Timed trigger disable, triggering job not in place yet.
    
    FIXME: Review recent rebase.
    FIXME: Update commit message to recent edits.
    
    Change-Id: I85c71a7ea94b9383b932d65b496056865bcecf78
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ jjb/integration/integration-get-bundle-url.sh
@@ -0,0 +1,16 @@
+
+# See the macro which included this file.
+
+# Extract the BUNDLEVERSION from the pom.xml
+BUNDLEVERSION=`xpath distribution/pom.xml '/project/version/text()' 2> /dev/null`
+echo "Bundle version is ${BUNDLEVERSION}"
+
+BUNDLEURL=${BUILD_URL}org.opendaylight.integration\$${KARAF_ARTIFACT}/artifact/org.opendaylight.integration/${KARAF_ARTIFACT}/${BUNDLEVERSION}/${KARAF_ARTIFACT}-${BUNDLEVERSION}.zip
+echo "Bundle url is ${BUNDLEURL}"
+
+# Set BUNDLEVERSION & BUNDLEURL
+echo BUNDLEVERSION=${BUNDLEVERSION} > bundle.txt
+echo BUNDLEURL=${BUNDLEURL} >> bundle.txt
+
+# NOTE: BUNDLEVERSION & BUNDLEURL will be re-imported back into the environment with the
+# Inject environment variables plugin (next step)
