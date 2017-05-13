71b6d674e7f95b6c7f5d604b56f8ab4c8c059756
Anil Belur <abelur@linuxfoundation.org>
2017-05-13 12:47:02 +1000

    WIP: Delete stale nodes older than 24H
    All jobs are expected to complete within 24H atmost, this job removes
    nodes on RS with creation time older than 24H.
    
    Change-Id: Id37a73ce8b1f785cdfd52507bafdfbeda00f3c9f
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ jjb/opendaylight-infra-cleanup-stale-nodes.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+virtualenv "$WORKSPACE/.venv"
+# shellcheck disable=SC1090
+source "$WORKSPACE/.venv/bin/activate"
+pip install --upgrade pip
+pip install --upgrade python-openstackclient python-heatclient
+pip freeze
+
+# Get list of all nodes on RS
+OS_NODES=()
+OS_NODES=($(openstack server list \
+                      -f json -c "Name" -c "ID" -c "Status" \
+                      | jq -r '.[] | ."ID"'))
+
+# All jobs are expected to complete within 24H atmost, therefore
+# delete nodes on RS by checking the creation time. if the
+# node creation time is older than 24H then delete that node from RS.
+for node in "${OS_NODES[@]}"; do
+    NODE_CREATE_TIME=$(openstack server show \
+                                -f json -c "created" -c "id" -c "name" \
+                                "$node" \
+                                | jq -r '."created"')
+    NODE_CREATE_EPOCH=$(date -d "${NODE_CREATE_TIME}" +%s)
+    BEFORE=$(date -d "-1 day" +%s)
+    if [[ -z "$NODE_CREATE_EPOCH" || "$NODE_CREATE_EPOCH" > "$BEFORE" ]]; then
+        continue
+    else
+        echo "$node creation time is $NODE_CREATE_TIME, delete node"
+        openstack server delete $node --wait
+    fi
+done
