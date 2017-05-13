fbaf34c2ec47c37330c0cd1ded9dacb407c476a5
Anil Belur <abelur@linuxfoundation.org>
2017-05-13 12:47:02 +1000

    Add job to delete stale nodes older than 24H on RS
    - All jobs are expected to complete within 24H atmost, this job
    removes nodes on RS with creation time older than 24H.
    - Reorder the jobs alphabetically.
    
    Note: this change requires an upstream change to be merged
    in lftools for running successfully.
    https://gerrit.linuxfoundation.org/infra/4861
    Change-Id: Id37a73ce8b1f785cdfd52507bafdfbeda00f3c9f
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ jjb/opendaylight-infra-cleanup-stale-nodes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+global-jjb/shell/lftools-install.sh
+# shellcheck disable=SC1090
+source "$LFTOOLS_DIR/bin/activate"
+
+lftools openstack --os-cloud rackspace \
+    server list --days=1
+lftools openstack --os-cloud rackspace \
+    server cleanup --days=1
