572f2516e02632438289e69be0788bf19e83fc78
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-06-07 22:35:23 -0400

    Remove baked packages from spinup script
    These packages are built as part of the packer build process so no need
    to additionally install them during system spinup.
    
    Change-Id: I8223279cd057124c66b3449ad92857ceb282eea1
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/builder.sh
+++ jenkins-scripts/builder.sh
@@ -10,19 +10,10 @@ yum clean all
 # Install software for building RPMs
 yum install -y fedora-packager
 
-# Install software for building docs
-yum install -y libxslt-devel
-
 # disable the firewall
 /bin/bash ./disable_firewall.sh
-
-# install sshpass
-yum install -y sshpass
 
 cat <<EOF >/etc/sudoers.d/89-jenkins-user-defaults
 Defaults:jenkins !requiretty
 jenkins     ALL = NOPASSWD: ALL
 EOF
-
-# install crudini command line tool for editing config files
-yum install -y crudini
