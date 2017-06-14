08379dd6f575fdb12d96fc9f43d99216c33ea03d
Jamo Luhrsen <jluhrsen@redhat.com>
2017-06-13 22:08:20 -0700

    Trigger CSIT from netvirt nitro merge job
    This is temporary functionality to allow netvirt to
    auto-run CSIT using a karaf4 distribution they are
    creating directly with their project. They want to
    have easier visibility in to the health of their karaf4
    migration without manually running jobs, or locally
    testing.
    
    Change-Id: Ic0eb568f4c3b398d9621ff37dfe5dfd90fd17ce0
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

---
+++ jjb/netvirt/upload-vpnservice-distribution.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -xeu -o pipefail
+
+MVN="/w/tools/hudson.tasks.Maven_MavenInstallation/mvn33/bin/mvn"
+KARAF_ARTIFACT="vpnservice-karaf"
+BUNDLE_VERSION="0.5.0-SNAPSHOT"
+BUNDLE="${KARAF_ARTIFACT}-${BUNDLE_VERSION}.zip"
+BUNDLE_FILEPATH="/tmp/r/org/opendaylight/netvirt/vpnservice-karaf/${BUNDLE_VERSION}/${BUNDLE}.zip"
+
+ls -l "${BUNDLE_FILEPATH}"
+LOG_FILE='integration-upload-distribution.log'
+echo "Uploading distribution to Nexus..."
+"$MVN" deploy:deploy-file \
+    --log-file ${LOG_FILE} \
+    --global-settings "$GLOBAL_SETTINGS_FILE" \
+    --settings "$SETTINGS_FILE" \
+    -Dfile="${BUNDLE_FILEPATH}" \
+    -DrepositoryId=opendaylight-snapshot \
+    -Durl="$ODLNEXUSPROXY/content/repositories/opendaylight.snapshot/" \
+    -DgroupId="org.opendaylight.integration.${GERRIT_PROJECT//\//.}" \
+    -DartifactId=${KARAF_ARTIFACT} \
+    -Dversion="${BUNDLE_VERSION}" \
+    -Dpackaging=zip \
+    || true  # Sandbox is not allowed to uplad to Nexus.
+
+cat "${LOG_FILE}"
+
+BUNDLE_URL=$(grep "Uploaded.*${KARAF_ARTIFACT}/${BUNDLE_VERSION}.*.zip" ${LOG_FILE} | awk '{print $3}') || true
+echo "Bundle uploaded to ${BUNDLE_URL}"
+
+# Re-inject the new BUNDLE_URL for downstream jobs to pull from Nexus
+cat > "${WORKSPACE}/vpnservice-upload-distribution.env" <<EOF
+BUNDLE_URL=${BUNDLE_URL}
+EOF
