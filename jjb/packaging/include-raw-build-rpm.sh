3d202541df6de5508083d2f98687d8be8b07b11f
Anil Belur <abelur@linuxfoundation.org>
2017-06-20 17:28:17 +1000

    WIP: Upload rpms to nexus repo
    The change enabled rpms built by packaging job to be uploaded
    onto nexus repositroy.
    
    Change-Id: I86cca89c1f2b8feb2dd229b04796c623ce0c0990
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/packaging/include-raw-build-rpm.sh
+++ jjb/packaging/include-raw-build-rpm.sh
@@ -22,3 +22,8 @@ download_url="${{DOWNLOAD_URL//zip/tar.gz}}"
 "$WORKSPACE/packaging/rpm/build.py" --download_url "$download_url" \
                                   --changelog_name "$CHANGELOG_NAME" \
                                   --changelog_email "$CHANGELOG_EMAIL"
+
+# Copy the rpm to be upload
+UPLOAD_FILES_PATH="$WORKSPACE/archives/upload_files"
+mkdir -p "$UPLOAD_FILES_PATH"
+cp "$HOME/rpmbuild/*.rpm" "$_" || true
