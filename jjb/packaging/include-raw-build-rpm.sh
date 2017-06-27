eb3349263554ce981d9ac62f6df5f16456c5f8d3
Alok Anand <alok4nand@gmail.com>
2017-06-27 17:27:02 +0530

    Updated rpm jobs for refactored package build logic
    The refactoring of deb and rpm build logic is under
    progress in #59320. This job reflects changes according to
    the refactored logic and will assist in testing it.
    
    Change-Id: I5a54d158cf7f0c673394dd76e15e92ab9e5fe41f
    Signed-off-by: Alok Anand <alok4nand@gmail.com>

--- jjb/packaging/include-raw-build-rpm.sh
+++ jjb/packaging/include-raw-build-rpm.sh
@@ -10,15 +10,14 @@ set -ex -o pipefail
 virtualenv rpm_build
 # shellcheck disable=SC1091
 source rpm_build/bin/activate
-PYTHON="rpm_build/bin/python"
-$PYTHON -m pip install --upgrade pip
-$PYTHON -m pip install -r "$WORKSPACE/packaging/rpm/requirements.txt"
+pip install --upgrade pip
+pip install -r "$WORKSPACE/packaging/packages/requirements.txt"
 
 # Make a URL for the tarball artifact from DOWNLOAD_URL (a zip)
 # shellcheck disable=SC2154
 download_url="${{DOWNLOAD_URL//zip/tar.gz}}"
 
 # Build release specified by build params
-"$WORKSPACE/packaging/rpm/build.py" --download_url "$download_url" \
+"$WORKSPACE/packaging/packages/build.py" --rpm --download_url "$download_url" \
                                   --changelog_name "$CHANGELOG_NAME" \
                                   --changelog_email "$CHANGELOG_EMAIL"
