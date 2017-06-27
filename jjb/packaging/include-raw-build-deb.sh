3bcab26fa0c980b2317809cf721a94dfe4cf95a3
Anil Belur <abelur@linuxfoundation.org>
2017-06-27 21:35:52 +1000

    Upload ODL packaging debs to nexus repository
    - Packaging jobs are modified to upload debs & dsc files onto nexus
      repositroy.
    - A separate Nexus repository is created for each '{stream}'. The
      repos are enabled to generate yum metadata along with scheduled
      tasks for devel repositories to purge packages from daily builds after
      30 days.
    - Nexus repositories format:
      Release: opendaylight-{stream}-epel-7-x86_64
      Testing/Staging: opendaylight-{stream}-epel-7-x86_64-devel
      Release: opendaylight-{stream}-ubuntu-1604-x86_64
      Testing/Staging: opendaylight-{stream}-ubuntu-1604-x86_64-devel
    - Modify autorelease jobs to trigger stream specific downstream jobs.
    
    Change-Id: I3164223ed47ddbc76d27da0446437c580ce6c015
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/packaging/include-raw-build-deb.sh
+++ jjb/packaging/include-raw-build-deb.sh
@@ -19,3 +19,9 @@ set -ex -o pipefail
                                   --changelog_time "$CHANGELOG_TIME" \
                                   --changelog_name "$CHANGELOG_NAME" \
                                   --changelog_email "$CHANGELOG_EMAIL"
+
+# Copy the debs to be upload
+UPLOAD_FILES_PATH="$WORKSPACE/upload_files"
+mkdir -p "$UPLOAD_FILES_PATH"
+cp "$WORKSPACE/packaging/deb/opendaylight/"*.deb "$_"
+cp "$WORKSPACE/packaging/deb/opendaylight/"*.dsc "$_"
