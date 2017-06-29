7291f6b11ab9bf68feddf536bede35f27f2c7985
Alok Anand <alok4nand@gmail.com>
2017-06-29 16:58:37 +0530

    Build deb job for refactored package build logic
    The refactoring of deb and rpm build logic is under
    progress in #59320. This job reflects changes according to
    the refactored logic and will assist in testing it.
    
    Change-Id: I65f5e4094621fbc18183e60cfc3c2304fb447b5e
    Signed-off-by: Alok Anand <alok4nand@gmail.com>

--- jjb/packaging/include-raw-build-deb.sh
+++ jjb/packaging/include-raw-build-deb.sh
@@ -6,26 +6,24 @@
 #   -o pipefail: Fail on errors in scripts this calls, give stacktrace
 set -ex -o pipefail
 
+# Wait for any background apt processes to finish
+# There seems to be a backgroud apt process that locks /var/lib/dpkg/lock
+# and causes our apt commands to fail.
+while pgrep apt > /dev/null; do sleep 1; done
+
 # Install required packages
 sudo apt-get install -y --force-yes \
                     build-essential \
                     devscripts \
                     equivs \
                     dh-systemd \
-                    python-yaml \
                     python-jinja2 \
+                    python-bs4 \
+                    python-requests \
+                    python-tzlocal \
                     gdebi
 
 # Build release specified by build params
-"$WORKSPACE/packaging/deb/build.py" --major "$VERSION_MAJOR" \
-                                  --minor "$VERSION_MINOR" \
-                                  --patch "$VERSION_PATCH" \
-                                  --deb "$PKG_VERSION" \
-                                  --sysd_commit "$SYSD_COMMIT" \
-                                  --codename "$CODENAME" \
-                                  --java_version "$JAVA_VERSION" \
-                                  --download_url "$DOWNLOAD_URL" \
-                                  --changelog_date "$CHANGELOG_DATE" \
-                                  --changelog_time "$CHANGELOG_TIME" \
+"$WORKSPACE/packaging/packages/build.py" --deb --download_url "$DOWNLOAD_URL" \
                                   --changelog_name "$CHANGELOG_NAME" \
                                   --changelog_email "$CHANGELOG_EMAIL"
