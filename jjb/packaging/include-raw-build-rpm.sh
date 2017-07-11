27c617bdda6164848f7a740c4f29e0c15b00ea10
Daniel Farrell <dfarrell@redhat.com>
2017-07-11 06:08:37 -0400

    Mv vs cp RPMs to upload dir to avoid duplication
    When we do a copy, both copies are hosted on Jenkins.
    
    Change-Id: Iaf71b1ce6a17161836f71829dca18e0d9a019c63
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- jjb/packaging/include-raw-build-rpm.sh
+++ jjb/packaging/include-raw-build-rpm.sh
@@ -20,11 +20,11 @@ download_url="${{DOWNLOAD_URL//zip/tar.gz}}"
 
 # Build release specified by build params
 "$WORKSPACE/packaging/rpm/build.py" --download_url "$download_url" \
-                                  --changelog_name "$CHANGELOG_NAME" \
-                                  --changelog_email "$CHANGELOG_EMAIL"
+                                    --changelog_name "$CHANGELOG_NAME" \
+                                    --changelog_email "$CHANGELOG_EMAIL"
 
-# Copy the rpm to be upload
+# Move RPMs (SRPM and noarch) to dir of files that will be uploaded to Nexus
 UPLOAD_FILES_PATH="$WORKSPACE/upload_files"
 mkdir -p "$UPLOAD_FILES_PATH"
-cp "/home/$USER/rpmbuild/RPMS/noarch/"*.rpm "$_"
-cp "/home/$USER/rpmbuild/SRPMS/"*.rpm "$_"
+mv "/home/$USER/rpmbuild/RPMS/noarch/"*.rpm "$_"
+mv "/home/$USER/rpmbuild/SRPMS/"*.rpm "$_"
