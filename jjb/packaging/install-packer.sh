f5f1edca2c1fd368345ffabb80c94c576ceec99d
Sri Sanketh U <sanketh.uppalapati@research.iiit.ac.in>
2017-07-16 23:50:51 +0530

    Add Packer libvirt, docker, vbox build jobs
    Change-Id: I2c78bff93e4de2d8e01d7d6ec45ded1641ca8730
    Signed-off-by: Sri Sanketh U <sanketh.uppalapati@research.iiit.ac.in>
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ jjb/packaging/install-packer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+set +e
+
+command -v packer.io || true
+
+# Install Packer
+wget --quiet --directory-prefix=/tmp https://releases.hashicorp.com/packer/1.0.3/packer_1.0.3_linux_amd64.zip
+sudo unzip /tmp/packer_1.0.3_linux_amd64.zip -d /usr/local/bin/
+sudo chown jenkins:jenkins /usr/local/bin/packer
