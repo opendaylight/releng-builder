54b1cceaf12525b25990b3bc444758aea5482c39
Sam Hague <shague@redhat.com>
2017-07-18 17:54:00 -0400

    Use smaller vms for devstack jobs
    Change-Id: If50d7af2338b7a63bc0194b9d0a83b5128d36cc3
    Signed-off-by: Sam Hague <shague@redhat.com>

--- jjb/integration/include-raw-integration-get-slave-addresses.sh
+++ jjb/integration/include-raw-integration-get-slave-addresses.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Get the Controller and Tools VM slave addresses
 
+set -x
+
 ODL_SYSTEM=()
 TOOLS_SYSTEM=()
 OPENSTACK_SYSTEM=()
+OPENSTACK_CONTROLLERS=()
 [ "$NUM_OPENSTACK_SITES" ] || NUM_OPENSTACK_SITES=1
 
 OPENSTACK_VENV="/tmp/v/openstack"
@@ -13,6 +16,20 @@ source $OPENSTACK_VENV/bin/activate
 ADDR=($(openstack stack show -f json -c outputs "$STACK_NAME" | \
        jq -r '.outputs[] | select(.output_key | match("^vm_[0-9]+_ips$")) | .output_value | .[]'))
 
+# The next two blocks of code will parse the list of vm IP's hostnames to determine which type of node
+# the vm is: odl, devstack controller or compute, ha_proxy or tools. For the odl node's the hsotname will contain
+# java in the name. The tools nodes are anything left after odl and devstack nodes have been found.
+#
+# The devstack nodes are identified with devstack in the hostname but require more checks to determine the controller
+# node. Heat names the vms as devstack-<index> with index starting at 0. The vms are created in groups of which
+# the openstack jobs create a vm_1_group for the controller and vm_2_group for the other vms such as the compute
+# and ha_proxy nodes. The list of IP addresses for all the created vms is returned in reverse order of the group
+# creation, but ordered within the group. I.E vm_1_group will be created first and named devstack-0, followed by
+# vm_2_group and named devstack-0, devstack-1 and devstack-2. The list of IPs will be: devstack-0 (vm_2_group, compute),
+# devstack-1, devstack-2, devstack-0 (vm_1_group, controller). Notice both the compute and first control node are both
+# named devstack-0. We know the controller because it would be last in the list of IPs. This first block of code will
+# produce two lists: one for the list of potential controllers and the second is a list of all devstack nodes.
+
 for i in "${ADDR[@]}"
 do
     REMHOST=$(ssh "${i}" hostname)
@@ -21,6 +38,11 @@ do
        ODL_SYSTEM=( "${ODL_SYSTEM[@]}" "${i}" )
        ;;
     *devstack*)
+       # track potential controllers which would have -0 at the end of the hostname
+       if [[ ${REMHOST: -2} == "-0" ]]; then
+          OPENSTACK_CONTROLLERS=( "${OPENSTACK_CONTROLLERS[@]}" "${i}" )
+       fi
+
        OPENSTACK_SYSTEM=( "${OPENSTACK_SYSTEM[@]}" "${i}" )
        ;;
     *)
@@ -39,6 +61,21 @@ else
    NUM_OPENSTACK_SYSTEM=${#OPENSTACK_SYSTEM[@]}
 fi
 echo "NUM_OPENSTACK_SYSTEM=${NUM_OPENSTACK_SYSTEM}" >> slave_addresses.txt
+
+# Rearrange the devstack node list to place the controller at the beginning of the list. The later code expects
+# the list to be ordered with controller first followed by other types.
+#
+# At this point there is a list of potential devstack controllers as: devstack-0 (vm_2_group, compute),
+# devstack-0 (vm_1_group, controller) and there is the full list with other compute or ha_proxy nodes in the middle.
+# We know the controller is at the end of the devstack nodes list based on the ordering described above. Swap that entry
+# with the first entry.
+if [ ${#OPENSTACK_CONTROLLERS[@]} -eq 2 ]; then
+    ctrl_index=${#OPENSTACK_SYSTEM[@]}
+    ctrl_index=$((ctrl_index -1))
+    tmp_addr=${OPENSTACK_SYSTEM[0]}
+    OPENSTACK_SYSTEM[0]=${OPENSTACK_SYSTEM[$ctrl_index]}
+    OPENSTACK_SYSTEM[$ctrl_index]=$tmp_addr
+fi
 
 # Add alias for ODL_SYSTEM_1_IP as ODL_SYSTEM_IP
 echo "ODL_SYSTEM_IP=${ODL_SYSTEM[0]}" >> slave_addresses.txt
@@ -78,4 +115,6 @@ for i in $(seq 0 $((NUM_OPENSTACK_HAPROXY_NODES - 1)))
 do
     echo "OPENSTACK_HAPROXY_$((i+1))_IP=${OPENSTACK_SYSTEM[$((openstack_index++))]}" >> slave_addresses.txt
 done
+echo "Contents of slave_addresses.txt:"
+cat slave_addresses.txt
 # vim: sw=4 ts=4 sts=4 et ft=sh :
