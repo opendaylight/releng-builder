1d67b61bc9ddb9add0d4ff05d6e4048f6cef63bf
Jamo Luhrsen <jluhrsen@redhat.com>
2017-07-31 15:37:43 -0700

    Add pike combo to carbon and nitrogen jobs
    Change-Id: Ib250188374232c47b3ad1b911945e16600ae1e35
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/integration-deploy-openstack-run-test.sh
+++ jjb/integration/integration-deploy-openstack-run-test.sh
@@ -20,6 +20,27 @@ echo "#################################################"
 
 SSH="ssh -t -t"
 
+function create_etc_hosts {
+NODE_IP=$1
+CTRL_IP=$2
+> ${WORKSPACE}/hosts_file
+for iter in `seq 1 ${NUM_OPENSTACK_COMPUTE_NODES}`
+do
+    COMPUTE_IP=OPENSTACK_COMPUTE_NODE_${iter}_IP
+    if [ "${!COMPUTE_IP}" == "${NODE_IP}" ]; then
+       CONTROL_HNAME=$(${SSH}  ${CTRL_IP}  "hostname")
+       echo "${CTRL_IP}   ${CONTROL_HNAME}" >> ${WORKSPACE}/hosts_file
+    else
+       COMPUTE_HNAME=$(${SSH}  ${!COMPUTE_IP}  "hostname")
+       echo "${!COMPUTE_IP}   ${COMPUTE_HNAME}" >> ${WORKSPACE}/hosts_file
+    fi
+done
+
+cat ${WORKSPACE}/hosts_file
+echo "Created the hosts file for ${NODE_IP}"
+}
+
+
 function create_control_node_local_conf {
 HOSTIP=$1
 MGRIP=$2
@@ -75,7 +96,7 @@ enable_plugin ${plugin_name} ${ENABLE_PLUGIN_ARGS}
 EOF
 done
 unset IFS
-if [ "${OPENSTACK_BRANCH}" == "master" ] || [ "${OPENSTACK_BRANCH}" == "stable/ocata" ]; then # Ocata+
+if [ "${OPENSTACK_BRANCH}" == "stable/pike" ] || [ "${OPENSTACK_BRANCH}" == "stable/ocata" ]; then # Ocata+
     # placement is mandatory for nova since Ocata, note that this requires computes to enable placement-client
     # this should be moved into enabled_services for each job (but only for Ocata)
     echo "enable_service placement-api" >> ${local_conf_file_name}
@@ -168,7 +189,12 @@ disable_service q-l3
 PUBLIC_INTERFACE=br100
 EOF
 
-SERVICE_PLUGINS="networking_odl.l3.l3_odl.OpenDaylightL3RouterPlugin"
+if [ "${ODL_ML2_DRIVER_VERSION}" == "v2" ]; then
+   SERVICE_PLUGINS="odl-router_v2"
+else
+   SERVICE_PLUGINS="odl-router"
+fi
+
 if [ "${ENABLE_NETWORKING_L2GW}" == "yes" ]; then
   SERVICE_PLUGINS+=", networking_l2gw.services.l2gateway.plugin.L2GatewayPlugin"
 fi #check for ENABLE_NETWORKING_L2GW
@@ -203,6 +229,9 @@ enable_isolated_metadata = True
 [DEFAULT]
 force_config_drive = False
 
+[scheduler]
+discover_hosts_in_cells_interval = 30
+
 EOF
 
 echo "local.conf Created...."
@@ -220,7 +249,7 @@ if [ "${ODL_ML2_BRANCH}" != "stable/ocata" ]; then
 else
    RECLONE=yes
 fi
-if [ "${OPENSTACK_BRANCH}" == "master" ] || [ "${OPENSTACK_BRANCH}" == "stable/ocata" ]; then # Ocata+
+if [ "${OPENSTACK_BRANCH}" == "stable/pike" ] || [ "${OPENSTACK_BRANCH}" == "stable/ocata" ]; then # Ocata+
     # placement is mandatory for nova since Ocata, note that this requires controller to enable placement-api
     ENABLED_SERVICES="n-cpu,placement-client"
 else
@@ -294,6 +323,17 @@ PUBLIC_PHYSICAL_NETWORK=${PUBLIC_PHYSICAL_NETWORK}
 ODL_PROVIDER_MAPPINGS=${ODL_PROVIDER_MAPPINGS}
 EOF
 fi
+cat >> ${local_conf_file_name} << EOF
+
+[[post-config|/etc/nova/nova.conf]]
+[api]
+auth_strategy = keystone
+[DEFAULT]
+vif_plugging_timeout = 300
+use_neutron = True
+
+
+EOF
 echo "local.conf Created...."
 cat ${local_conf_file_name}
 }
@@ -434,7 +474,6 @@ do
     mkdir -p ${OS_CTRL_FOLDER}
     scp ${!OS_CTRL_IP}:/opt/stack/devstack/nohup.out ${OS_CTRL_FOLDER}/stack.log
     scp ${!OS_CTRL_IP}:/var/log/openvswitch/ovs-vswitchd.log ${OS_CTRL_FOLDER}/ovs-vswitchd.log
-    scp ${!OS_CTRL_IP}:/var/log/openvswitch/ovsdb-server.log ${OS_CTRL_FOLDER}/ovsdb-server.log
     scp ${!OS_CTRL_IP}:/etc/neutron/neutron.conf ${OS_CTRL_FOLDER}/neutron.conf
     scp ${!OS_CTRL_IP}:/etc/nova/nova.conf ${OS_CTRL_FOLDER}/nova.conf
     scp ${!OS_CTRL_IP}:/etc/kuryr/kuryr.conf ${OS_CTRL_FOLDER}/kuryr.conf
@@ -510,17 +549,16 @@ sudo systemctl stop NetworkManager
 sudo killall dhclient
 sudo killall dnsmasq
 #Workaround for mysql failure
-echo "127.0.0.1    localhost \${HOSTNAME}" > /tmp/hosts
+echo "127.0.0.1    localhost \${HOSTNAME}" >> /tmp/hosts
 echo "::1   localhost  \${HOSTNAME}" >> /tmp/hosts
 sudo mv /tmp/hosts /etc/hosts
 sudo /usr/sbin/brctl addbr br100
-#sudo ifconfig eth0 mtu 2000
+# FIXME: why is this line commented out? can we just remove it?
+# sudo ifconfig eth0 mtu 2000
 sudo mkdir /opt/stack
 sudo chmod 777 /opt/stack
 cd /opt/stack
-git clone https://git.openstack.org/openstack-dev/devstack
-cd devstack
-git checkout $OPENSTACK_BRANCH
+git clone https://git.openstack.org/openstack-dev/devstack --branch $OPENSTACK_BRANCH
 EOF
 
 cat > "${WORKSPACE}/setup_host_cell_mapping.sh" << EOF
@@ -570,6 +608,8 @@ for i in `seq 1 ${NUM_OPENSTACK_CONTROL_NODES}`
 do
     echo "Stack the Control Node"
     CONTROLIP=OPENSTACK_CONTROL_NODE_${i}_IP
+    create_etc_hosts ${!CONTROLIP}
+    scp ${WORKSPACE}/hosts_file ${!CONTROLIP}:/tmp/hosts
     scp ${WORKSPACE}/get_devstack.sh ${!CONTROLIP}:/tmp
     ${SSH} ${!CONTROLIP} "bash /tmp/get_devstack.sh"
     create_control_node_local_conf ${!CONTROLIP} ${ODLMGRIP[$i]} "${ODL_OVS_MGRS[$i]}"
@@ -592,6 +632,8 @@ do
     SITE_INDEX=$((((i - 1) / NUM_COMPUTES_PER_SITE) + 1)) # We need the site index to infer the control node IP for this compute
     COMPUTEIP=OPENSTACK_COMPUTE_NODE_${i}_IP
     CONTROLIP=OPENSTACK_CONTROL_NODE_${SITE_INDEX}_IP
+    create_etc_hosts ${!COMPUTEIP} ${!CONTROLIP}
+    scp ${WORKSPACE}/hosts_file ${!CONTROLIP}:/tmp/hosts
     scp ${WORKSPACE}/get_devstack.sh  ${!COMPUTEIP}:/tmp
     ${SSH} ${!COMPUTEIP} "bash /tmp/get_devstack.sh"
     create_compute_node_local_conf ${!COMPUTEIP} ${!CONTROLIP} ${ODLMGRIP[$SITE_INDEX]} "${ODL_OVS_MGRS[$SITE_INDEX]}"
@@ -658,6 +700,20 @@ for i in `seq 1 ${NUM_OPENSTACK_SITES}`
 do
     echo "Configure the Control Node"
     CONTROLIP=OPENSTACK_CONTROL_NODE_${i}_IP
+    # Gather Compute IPs for the site
+    for j in `seq 1 ${NUM_COMPUTES_PER_SITE}`
+    do
+        COMPUTE_INDEX=$(((i-1) * NUM_COMPUTES_PER_SITE + j))
+        IP_VAR=OPENSTACK_COMPUTE_NODE_${COMPUTE_INDEX}_IP
+        COMPUTE_IPS[$((j-1))]=${!IP_VAR}
+    done
+
+    # Need to disable firewalld and iptables in compute nodes as well
+    for ip in ${COMPUTE_IPS[*]}
+    do
+        scp ${WORKSPACE}/disable_firewall.sh "${ip}:/tmp"
+        ${SSH} "${ip}" "sudo bash /tmp/disable_firewall.sh"
+    done
 
     #Need to disable firewalld and iptables in control node
     echo "Stop Firewall in Control Node for compute nodes to be able to reach the ports and add to hypervisor-list"
@@ -666,6 +722,13 @@ do
 
     echo "sleep for 60s and print hypervisor-list"
     sleep 60
+    # In anything after Newton, if we do not enable the n-cpu in control node
+    # We need to discover hosts manually and ensure that they are mapped to cells.
+    # reference: https://ask.openstack.org/en/question/102256/how-to-configure-placement-service-for-compute-node-on-ocata/
+    if [ "${OPENSTACK_BRANCH}" != "stable/newton" ]; then
+        scp ${WORKSPACE}/setup_host_cell_mapping.sh  ${!CONTROLIP}:/tmp
+        ${SSH} ${!CONTROLIP} "sudo bash /tmp/setup_host_cell_mapping.sh"
+    fi
     ${SSH} ${!CONTROLIP} "cd /opt/stack/devstack; source openrc admin admin; nova hypervisor-list"
     # in the case that we are doing openstack (control + compute) all in one node, then the number of hypervisors
     # will be the same as the number of openstack systems. However, if we are doing multinode openstack then the
@@ -683,13 +746,6 @@ do
         exit 1
     fi
 
-    # For Ocata, if we do not enable the n-cpu in control node
-    # We need to discover hosts manually and ensure that they are mapped to cells.
-    # reference: https://ask.openstack.org/en/question/102256/how-to-configure-placement-service-for-compute-node-on-ocata/
-    if [ "${OPENSTACK_BRANCH}" == "stable/ocata" ]; then
-        scp ${WORKSPACE}/setup_host_cell_mapping.sh  ${!CONTROLIP}:/tmp
-        ${SSH} ${!CONTROLIP} "sudo bash /tmp/setup_host_cell_mapping.sh"
-    fi
 
     # upgrading pip, urllib3 and httplib2 so that tempest tests can be run on openstack control node
     # this needs to happen after devstack runs because it seems devstack is pulling in specific versions
@@ -825,14 +881,20 @@ if [ -z "${SUITES}" ]; then
     SUITES=`egrep -v '(^[[:space:]]*#|^[[:space:]]*$)' testplan.txt | tr '\012' ' '`
 fi
 
-#Environment Variables Needed to execute Openstack Client for NEtvirt Jobs
+if [ "${OPENSTACK_BRANCH}" == "stable/pike" ]; then # Pike
+   AUTH="http://${!CONTROLIP}/identity"
+else
+   AUTH="http://${!CONTROLIP}:35357/v3"
+fi
+
+#Environment Variables Needed to execute Openstack Client for Netvirt Jobs
 cat > /tmp/os_netvirt_client_rc << EOF
 export OS_USERNAME=admin
 export OS_PASSWORD=admin
 export OS_PROJECT_NAME=admin
 export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_DOMAIN_NAME=default
-export OS_AUTH_URL="http://${!CONTROLIP}:35357/v3"
+export OS_AUTH_URL=${AUTH}
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
 export OS_TENANT_NAME=admin
