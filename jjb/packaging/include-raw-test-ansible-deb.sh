4a8ae191a8e45011ef81c551cb07c48464316d06
root <sanketh.uppalapati@research.iiit.ac.in>
2017-07-31 16:56:56 +0530

    Make ansible-odl jobs use the proposed code
    Change-Id: I07efef74c20b4db82f6c0a66480f55b0c807124d
    Signed-off-by: root <sanketh.uppalapati@research.iiit.ac.in>

--- jjb/packaging/include-raw-test-ansible-deb.sh
+++ jjb/packaging/include-raw-test-ansible-deb.sh
@@ -22,8 +22,7 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 
-git clone https://github.com/dfarrell07/ansible-opendaylight.git
-cd ansible-opendaylight
+cd $WORKSPACE/ansible
 sudo ansible-galaxy install -r requirements.yml
 sudo ansible-playbook -i "localhost," -c local examples/deb_repo_install_playbook.yml
 
