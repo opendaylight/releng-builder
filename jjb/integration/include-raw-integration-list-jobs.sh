ec5468e4166344e36b5f404985ff7c6152b55e4c
Anil Belur <abelur@linuxfoundation.org>
2017-08-10 15:37:41 +1000

    Fix shellcheck issues for integration scripts
    These fixes are required for change
    I9d0139e7c05743ed9f3b5f2f1ced21c94b86758c
    
    Change-Id: I9d8cc971c3b239d8f7c425c576772b012acf16f0
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/integration/include-raw-integration-list-jobs.sh
+++ jjb/integration/include-raw-integration-list-jobs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# shellcheck disable=SC2034,SC2154
 # Script to filter Jenkins jobs against a blacklist
 # output: newline & comma-separated list
 
 system="releng"
 search_string="{search_string}"
 blacklist_in="{blacklist}"
+# shellcheck disable=SC1083
 blacklist=( $(echo ${{blacklist_in}}) )
 stream="{stream}"
 
@@ -12,10 +14,14 @@ wget --quiet -O jenkins-jobs.xml https://jenkins.opendaylight.org/$system/api/xm
 jobs=$(xmlstarlet sel -t -m '//hudson/job' \
     -n -v 'name' jenkins-jobs.xml | grep $search_string | grep $stream)
 
+# shellcheck disable=SC1083
+{
+# shellcheck disable=SC2124
 bl_len=${{#blacklist[@]}}
-for (( i = 0; i < ${{bl_len}}; i++ )); do
-    jobs="$(echo "$jobs" | grep -v ${{blacklist[$i]}} )"
+for i in $(seq 0 ${{bl_len}}); do
+    jobs="$(echo "$jobs" | grep -v "${{blacklist[$i]}}")"
 done
+}
 # output as comma-separated list with 8 spaces before each item
+# shellcheck disable=SC1083
 echo $jobs | sed 's: :,\n:g' | sed 's:^\(.*\):        \1:g' > {jobs-filename}
-
