d3761f87f510c83d288ff75940b9ce334e00b92a
Anil Belur <abelur@linuxfoundation.org>
2017-08-10 15:37:41 +1000

    Fix shellcheck issues for integration scripts
    These fixes are required for change
    I9d0139e7c05743ed9f3b5f2f1ced21c94b86758c
    
    Change-Id: I9d8cc971c3b239d8f7c425c576772b012acf16f0
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/integration/include-raw-integration-list-jobs.sh
+++ jjb/integration/include-raw-integration-list-jobs.sh
@@ -3,19 +3,22 @@
 # output: newline & comma-separated list
 
 system="releng"
-search_string="{search_string}"
-blacklist_in="{blacklist}"
-blacklist=( $(echo ${{blacklist_in}}) )
-stream="{stream}"
+declare -a blacklist=( $(echo "$BLACKLIST") )
+echo "$BLACKLIST" > "$WORKSPACE/archives/integration-list-job-blacklist.log" 2>&1
 
 wget --quiet -O jenkins-jobs.xml https://jenkins.opendaylight.org/$system/api/xml
 jobs=$(xmlstarlet sel -t -m '//hudson/job' \
-    -n -v 'name' jenkins-jobs.xml | grep $search_string | grep $stream)
+    -n -v 'name' jenkins-jobs.xml | grep $SEARCH_STRING | grep $STREAM;)
 
-bl_len=${{#blacklist[@]}}
-for (( i = 0; i < ${{bl_len}}; i++ )); do
-    jobs="$(echo "$jobs" | grep -v ${{blacklist[$i]}} )"
+# SC2154 expects var is referenced but not assigned. However the blacklist array
+# is returns the length of the array and not the array itself, therefore disable
+# the check.
+# shellcheck disable=SC2154
+bl_len="${#blacklist[*]}"
+for i in $(seq 0 "$bl_len"); do
+    # shellcheck disable=SC2154
+    jobs="$(echo "$jobs" | grep -v "${blacklist[$i]}")"
 done
-# output as comma-separated list with 8 spaces before each item
-echo $jobs | sed 's: :,\n:g' | sed 's:^\(.*\):        \1:g' > {jobs-filename}
 
+# output as comma-separated list with 8 spaces before each item
+echo $jobs | sed 's: :,\n:g' | sed 's:^\(.*\):        \1:g' > "${JOBS_FILENAME}"
