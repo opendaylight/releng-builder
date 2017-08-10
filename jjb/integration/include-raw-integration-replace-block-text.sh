5cee3ec3bd11b752b93b96b72f5c932795a9f392
Anil Belur <abelur@linuxfoundation.org>
2017-08-10 15:37:41 +1000

    Fix shellcheck issues for integration scripts
    These fixes are required for change
    I9d0139e7c05743ed9f3b5f2f1ced21c94b86758c
    
    Change-Id: I9d8cc971c3b239d8f7c425c576772b012acf16f0
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/integration/include-raw-integration-replace-block-text.sh
+++ jjb/integration/include-raw-integration-replace-block-text.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# shellcheck disable=SC1083
+
 starting_regex={starting-regex}
 ending_regex={ending-regex}
+# shellcheck disable=SC2034
 file_with_changes_to_insert={file-with-changes-to-insert}
+# shellcheck disable=SC2034
 file_to_change={file-to-change}
+# shellcheck disable=SC2154
 output=$(sed -e "/$starting_regex/,/$ending_regex/{{ /$starting_regex/{{p; r ${{file_with_changes_to_insert}}
         }}; /$ending_regex/p; d }}"  ${{file_to_change}})
 echo "$output" > ${{file_to_change}}
