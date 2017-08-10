b8506c0b5fcd7367d981503f32e464ad08023bfe
Anil Belur <abelur@linuxfoundation.org>
2017-08-10 15:37:41 +1000

    Fix shellcheck issues for integration scripts
    These fixes are required for change
    I9d0139e7c05743ed9f3b5f2f1ced21c94b86758c
    
    Change-Id: I9d8cc971c3b239d8f7c425c576772b012acf16f0
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/integration/include-raw-integration-replace-block-text.sh
+++ jjb/integration/include-raw-integration-replace-block-text.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-starting_regex={starting-regex}
-ending_regex={ending-regex}
-file_with_changes_to_insert={file-with-changes-to-insert}
-file_to_change={file-to-change}
-output=$(sed -e "/$starting_regex/,/$ending_regex/{{ /$starting_regex/{{p; r ${{file_with_changes_to_insert}}
-        }}; /$ending_regex/p; d }}"  ${{file_to_change}})
-echo "$output" > ${{file_to_change}}
+
+starting_regex="${STARTING_REGEX:?}"
+ending_regex="${ENDING_REGEX:?}"
+file_with_changes_to_insert="${FILE_WITH_CHANGES_TO_INSERT:?}"
+file_to_change="${FILE_TO_CHANGE:?}"
+
+echo "starting_regex: $STARTING_REGEX"
+echo "ending_regex: $ENDING_REGEX"
+echo "files_with_changes_to_insert: $FILE_WITH_CHANGES_TO_INSERT"
+echo "file_to_change: $FILE_TO_CHANGE"
+
+output=$(sed -e "/$starting_regex/,/$ending_regex/{{ /$starting_regex/{{p; r ${file_with_changes_to_insert}
+        }}; /$ending_regex/p; d }}"  "${file_to_change}")
+echo "$output" > "${file_to_change}"
