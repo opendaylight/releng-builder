b03f81fdf00f1d4a3303a64740e39ab9d798d780
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-08-15 00:06:38 -0400

    Simplify csit-auto-update job
    Refactor the job into a more simplified job stored in the integration
    project directory. This job generates csit-jobs-{stream}.lst files which
    can simply be !include in a job template file.
    
    This makes releng-defaults.yaml smaller and moves auto-generated files
    into their own files for easier maintenance.
    
    Change-Id: Ief999fd72f6340d7aba1bf83a54550e4ebe46351
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/integration/update-csit-tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> update-csit-tests.sh"
+
+# Ensure we fail the job if any steps fail.
+# CSIT_BLACKLIST, SILO, STREAM are expected environment variables
+set -e -o pipefail
+set +u
+
+jobs_file=$(mktemp)
+search_string="csit"
+
+wget --quiet -O "$jobs_file" https://jenkins.opendaylight.org/$SILO/api/xml
+jobs=($(xmlstarlet sel -t -m '//hudson/job' \
+    -n -v 'name' "$jobs_file" | grep $search_string | grep $STREAM))
+
+jobs=($(echo "${jobs[@]}" | grep -v "${CSIT_BLACKLIST// /\|}"))
+
+# output as comma-separated list
+job_list="${WORKSPACE}/jjb/integration/csit-jobs-${STREAM}.lst"
+rm "$job_list"
+for job in "${jobs[@]}"; do
+    echo "${job}," | tee -a "$job_list"
+done
