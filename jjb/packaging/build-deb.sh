334d9da2b46898db9abf61ce27a66b5a35b78a43
Alok Anand <alok4nand@gmail.com>
2017-08-17 00:21:45 +0530

    Build deb using refactored logic
    This commit updates build deb job for the refactored logic.
    
    Change-Id: I879db3533047eabe67fc39d864a3152292af1975
    Signed-off-by: Alok Anand <alok4nand@gmail.com>

--- jjb/packaging/build-deb.sh
+++ jjb/packaging/build-deb.sh
@@ -6,17 +6,16 @@
 #   -o pipefail: Fail on errors in scripts this calls, give stacktrace
 set -ex -o pipefail
 
+# Install required packages
+virtualenv deb_build
+# shellcheck disable=SC1091
+source deb_build/bin/activate
+PYTHON="deb_build/bin/python"
+$PYTHON -m pip install --upgrade pip
+$PYTHON -m pip install -r "$WORKSPACE/packaging/packages/requirements.txt"
+
 # Build release specified by build params
-"$WORKSPACE/packaging/deb/build.py" --major "$VERSION_MAJOR" \
-                                  --minor "$VERSION_MINOR" \
-                                  --patch "$VERSION_PATCH" \
-                                  --deb "$PKG_VERSION" \
-                                  --sysd_commit "$SYSD_COMMIT" \
-                                  --codename "$CODENAME" \
-                                  --java_version "$JAVA_VERSION" \
-                                  --download_url "$DOWNLOAD_URL" \
-                                  --changelog_date "$CHANGELOG_DATE" \
-                                  --changelog_time "$CHANGELOG_TIME" \
+"$WORKSPACE/packaging/packages/build.py" --deb --download_url "$DOWNLOAD_URL" \
                                   --changelog_name "$CHANGELOG_NAME" \
                                   --changelog_email "$CHANGELOG_EMAIL"
 
