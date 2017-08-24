3682aac824a80021de61732a6b2ad82bbabe62bd
Alok Anand <alok4nand@gmail.com>
2017-08-24 20:39:41 +0530

    Build latest snapshots into debs
    Lastest snapshots have been packaged into rpms for some time.
    This commit uses the recent refactoring done in Int/Pack to add
    job for building deb packages of latest snapshots.
    
    Change-Id: I6d2f18d37113e8f2c98dda04e4adba54e90ee4e1
    Signed-off-by: Alok Anand <alok4nand@gmail.com>

---
+++ jjb/packaging/build-deb-snap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+# Install required packages
+virtualenv deb_build
+# shellcheck disable=SC1091
+source deb_build/bin/activate
+PYTHON="deb_build/bin/python"
+$PYTHON -m pip install --upgrade pip
+$PYTHON -m pip install -r "$WORKSPACE/packaging/packages/requirements.txt"
+
+# Build snapshot specified by build params
+"$WORKSPACE/packaging/packages/build.py" --deb --build-latest-snap \
+                                  --major "$VERSION_MAJOR" \
+                                  --minor "$VERSION_MINOR" \
+                                  --changelog_name "$CHANGELOG_NAME" \
+                                  --changelog_email "$CHANGELOG_EMAIL"
+
+# Copy the debs to be upload
+UPLOAD_FILES_PATH="$WORKSPACE/upload_files"
+mkdir -p "$UPLOAD_FILES_PATH"
+# Note: no source packages are available, since the debs are not built
+# from the actual source
+mv "$WORKSPACE/packaging/packages/deb/opendaylight/"*.deb "$_"
