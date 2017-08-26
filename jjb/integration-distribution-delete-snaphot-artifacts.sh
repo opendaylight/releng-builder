1dd01bdeefd70510cf4427514d3ce1457fbfd645
Anil Belur <abelur@linuxfoundation.org>
2017-08-26 17:55:52 +1000

    Add missing license headers for releng scripts
    Change-Id: If11d71b9b3fe300c5d1d31555a026d584ba22d8e
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/integration-distribution-delete-snaphot-artifacts.sh
+++ jjb/integration-distribution-delete-snaphot-artifacts.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
 
 set +e  # To avoid failures in projects which generate zero snapshot artifacts.
 find "/tmp/r/org/opendaylight/$GERRIT_PROJECT/" -path "*-SNAPSHOT*" -delete
--- jjb/opendaylight-infra-copy-ssh-keys.sh
+++ jjb/opendaylight-infra-copy-ssh-keys.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
 echo "----------> Copy ssh public keys to csit lab"
 
 # shellcheck source=/tmp/v/openstack/bin/activate disable=SC1091
