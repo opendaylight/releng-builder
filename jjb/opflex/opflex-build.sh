ca0e922227866b6e3910438c59d0902fdd2adb90
Anil Belur <abelur@linuxfoundation.org>
2017-08-29 12:16:38 +1000

    Refactor OpFlex jobs to push to nexus
    OpFlex project builds and archives dependencies (libuv, rapidjson
    and ovs) on Jenkins.
    
    Now the jobs are modified to build and push the dependcies to
    nexus thridparty repositories. The merge and release jobs are
    modified to download the deps from nexus while building opflex
    artifacts.
    
    Jira: releng-3
    Change-Id: Icf6f8d1a16f7cebfb5fe37bf8c6f8f350d7eb9fa
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/opflex/opflex-build.sh
+++ jjb/opflex/opflex-build.sh
@@ -15,9 +15,33 @@ cleanup
 mkdir -p "$ROOT"
 trap cleanup EXIT
 
-tar -xz -C "$ROOT" --strip-components=2 -f libuv.tgz
-tar -xz -C "$ROOT" --strip-components=2 -f rapidjson.tgz
-tar -xz -C "$ROOT" --strip-components=2 -f openvswitch.tgz
+# Determine the version to download
+if [[ $OPENVSWITCH_BUILD =~ v1 ]]; then
+    openvswitch_version="noiro"
+else
+    openvswitch_version="2.6.0"
+fi
+
+if [[ $RAPIDJSON_BUILD =~ v1 ]]; then
+    rapidjson_version="1.0.1"
+else
+    rapidjson_version="1.0.2"
+fi
+
+if [[ $LIBUV_BUILD =~ v1 ]]; then
+    libuv_version="1.7.5"
+else
+    libuv_version="1.8.0"
+fi
+
+# Download the artifacts from nexus thirdparty
+wget -nv ${NEXUS_URL}/service/local/repositories/thirdparty/content/openvswitch/openvswitch/${openvswitch_version}/openvswitch-${openvswitch_version}.tar.gz
+wget -nv ${NEXUS_URL}/service/local/repositories/thirdparty/content/rapidjson/rapidjson/${rapidjson_version}/rapidjson-${rapidjson_version}.tar.gz
+wget -nv ${NEXUS_URL}/service/local/repositories/thirdparty/content/libuv/libuv/${libuv_version}/libuv-${libuv_version}.tar.gz
+
+tar -xz -C "$ROOT" --strip-components=2 -f libuv-${libuv_version}.tar.gz
+tar -xz -C "$ROOT" --strip-components=2 -f rapidjson-${rapidjson_version}.tar.gz
+tar -xz -C "$ROOT" --strip-components=2 -f openvswitch-${openvswitch_version}.tar.gz
 
 export PATH="$ROOT/bin:$PATH"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ROOT/lib"
