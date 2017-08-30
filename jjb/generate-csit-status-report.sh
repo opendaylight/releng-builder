8d406064fc7beaa076d6cb2c56b0dc22641af072
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-08-30 10:44:31 -0400

    Add step to grab csit test status and archive it
    This makes it easier for us to copy paste the info into the tracking
    spreadsheet.
    
    Change-Id: I5dba17df7e7359a6f3626ff57e4f93bae22d9b43
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/generate-csit-status-report.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Create a python script to parse a Jenkins build for sub-project status
+
+script=$(mktemp)
+
+cat > $script <<EOF
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+__author__ = 'Thanh Ha'
+
+
+import sys
+
+from bs4 import BeautifulSoup
+import requests
+
+
+build_url = sys.argv[1]
+urlparse = requests.utils.urlparse(build_url)
+jenkins_url = "{}://{}".format(urlparse.scheme, urlparse.netloc)
+
+page = requests.get(build_url)
+soup = BeautifulSoup(page.text, 'html.parser')
+links = soup.findAll("a", { "class" : "model-link" })
+
+_file = open('csit_failed_tests.txt', 'w+')
+
+for link in links:
+    if link.img and (link.img['alt'] == 'Unstable' or
+                     link.img['alt'] == 'Failed' or
+                     link.img['alt'] == 'Aborted'):
+
+        url = link['href']
+        project = url.split('/')[3].split('-')[0]
+        _file.write("{}\t{}{}\n".format(project, jenkins_url, url))
+
+_file.close()
+EOF
+
+virtualenv --quiet "/tmp/v/jenkins"
+# shellcheck source=/tmp/v/jenkins/bin/activate disable=SC1091
+source "/tmp/v/jenkins/bin/activate"
+pip install --quiet --upgrade pip
+pip install --quiet --upgrade tox beautifulsoup4 requests
+
+echo python "$script" "$BUILD_URL"
+python "$script" "$BUILD_URL"
+
+mkdir -p "$WORKSPACE/archives"
+mv csit_failed_tests.txt "$WORKSPACE/archives"
