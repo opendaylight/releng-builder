d0352b80617b48072aa4c1ea87a0f88048790122
Anil Belur <abelur@linuxfoundation.org>
2017-09-26 15:27:49 +1000

    Build 6Wind/quagga pkgs for Centos7 and Ubuntu
    Build source and binary packages for CentOS7 and Ubuntu 16.04.
    The packages are pushed into the Nexus thirdparty repo which can
    be later pulled in and used by netvirt jobs for testing.
    
    JIRA: RELENG-461
    Change-Id: I16377e5092f065148e47e3a406c7f84d509547f6
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ jjb/netvirt/quagga-build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the term s of the Eclipse Public License
+# v1.0 accompanies testing this distribution with Netvirt jobs, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> quagga-build.sh"
+
+set -e -x
+
+# The script builds 6wind/quagga source and binary packages from zrpcd
+# repository for testing router functionalities with Netvirt jobs, .
+
+pushd "$(pwd)/zrpcd"
+
+FACTER_OS=$(/usr/bin/facter operatingsystem | tr '[:upper:]' '[:lower:]')
+FACTER_OSVER=$(/usr/bin/facter operatingsystemrelease)
+
+chmod a+x "$(pwd)/pkgsrc/dev_compile_script.sh"
+sudo "$(pwd)/pkgsrc/dev_compile_script.sh" -p -d -b -v "$QUAGGA_VERSION" || true
+
+# Move packages into a that will be uploaded to Nexus
+UPLOAD_FILES_PATH="$WORKSPACE/upload_files"
+mkdir -p "$UPLOAD_FILES_PATH"
+
+ls -R $(pwd)/pkgsrc/
+mv $(pwd)/pkgsrc/*.rpm "$UPLOAD_FILES_PATH" || true
+mv $(pwd)/pkgsrc/*.deb "$UPLOAD_FILES_PATH" || true
+mv "/home/$USER/rpmbuild/RPMS/noarch/"*.rpm "$_" || true
+mv "/home/$USER/rpmbuild/SRPMS/"*.rpm "$_" || true
+mv "/root/rpmbuild/SRPMS/"*.rpm "$_" || true
+popd
