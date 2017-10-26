8b38ae5624829cea5a9a31397103c80b8162d165
Anil Belur <abelur@linuxfoundation.org>
2017-10-27 08:05:48 +1000

    Set $(hostname) in /etc/hosts during node spinup
    Remove the line where we add $(hostname) into /etc/hosts
    from the packer provision scripts, since the node name
    is obtained during spinup and not during image build time.
    
    Update the Jenkins startup scripts so that the $(hostname)
    is updated into /etc/hosts file.
    
    Note this workaround is required only for Ubuntu flavour.
    
    Change-Id: Ia1822426a9c61456cf7b07831289244f103f9d39
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jenkins-scripts/mininet-ubuntu.sh
+++ jenkins-scripts/mininet-ubuntu.sh
@@ -8,3 +8,9 @@ EOF
 
 # disable the firewall
 /bin/bash ./disable_firewall.sh
+
+# Add 'hostname' into /etc/hosts during node spinup time to avoid sudo returning
+# an 'unable to resolve host' message or some Java API's returning an unknown
+# host exception. The workaround on adding "myhostname" into /etc/nss-switch.conf
+# does not work on Ubuntu flavours.
+sed -i "/127.0.0.1/s/$/\t$(hostname)/" /etc/hosts
