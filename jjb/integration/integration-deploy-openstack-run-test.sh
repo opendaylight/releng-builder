54be37b93682ef983b5eaefb824b10d8c3d3b754
Jamo Luhrsen <jluhrsen@redhat.com>
2017-11-29 17:34:30 -0800

    Make robot report logs better
    - use more specific name for each pybot iteration which
      will help when looking at results via jenkins robot
      plugin
    
    - start creating the full single log file again. We
      stopped doing this because our small 2G VMs could not
      handle the large log files. But, we are now using 4G
      VMs and it should be fine
    
    Change-Id: I8bc6628544c9684a2527fc7c12f8a27d7df31983
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/integration-deploy-openstack-run-test.sh
+++ jjb/integration/integration-deploy-openstack-run-test.sh
@@ -1167,9 +1167,13 @@ source /tmp/os_netvirt_client_rc
 
 echo "Starting Robot test suites ${SUITES} ..."
 # please add pybot -v arguments on a single line and alphabetized
+suite_num=1
 for suite in ${SUITES}; do
+    # prepend a incrmental counter to the suite name so that the full robot log combining all the suites as is done
+    # in the rebot step below will list all the suites in chronological order as rebot seems to alphabatize them
+    ((suite_num++))
     log_name_ext=$(basename ${suite} | cut -d. -f1)
-    pybot -N ${TESTPLAN} --removekeywords wuks -c critical -e exclude -e skip_if_${DISTROSTREAM} \
+    pybot -N "$(printf %02d ${suite_num})_${log_name_ext}" --removekeywords wuks -c critical -e exclude -e skip_if_${DISTROSTREAM} \
     --log log_${log_name_ext}.html --report None --output output_${log_name_ext}.xml \
     -v BUNDLEFOLDER:${BUNDLEFOLDER} \
     -v BUNDLE_URL:${ACTUAL_BUNDLE_URL} \
@@ -1219,7 +1223,7 @@ for suite in ${SUITES}; do
     ${TESTOPTIONS} ${suite} || true
 done
 #rebot exit codes seem to be different
-rebot --output ${WORKSPACE}/output.xml --log None --report None output_*.xml || true
+rebot --output ${WORKSPACE}/output.xml --log full_log.html --report None -N openstack_csit output_*.xml || true
 
 echo "Examining the files in data/log and checking file size"
 ssh ${ODL_SYSTEM_IP} "ls -altr /tmp/${BUNDLEFOLDER}/data/log/"
