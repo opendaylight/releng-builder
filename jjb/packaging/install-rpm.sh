921da1fe4f05d51b81f2f241e01aeb94b1791337
Daniel Farrell <dfarrell@redhat.com>
2017-12-12 16:36:04 -0500

    Do RPM tests in RPM build job to gate publish
    We currently only run our RPM tests after the RPM build job finishes.
    This isn't good because by the time we've tested the RPM it has already
    been pushed to Nexus and is being consumed by downstreams. Instead we
    should fail the RPM build job if the tests fail and not publish.
    
    Change-Id: I215de288923a308b95ebd242578d2a90d0b8e6ec
    Fixes: INTPAK-115
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- jjb/packaging/install-rpm.sh
+++ jjb/packaging/install-rpm.sh
@@ -9,12 +9,15 @@ set -ex -o pipefail
 # Update mirror list to avoid slow/hung one
 sudo yum update -y yum-plugin-fastestmirror
 
-# Install ODL from .rpm link or .repo url
-if [[ $URL == *.rpm ]]
-then
+# Install ODL from RPM path, RPM URL or .repo file url
+# NB: Paths must be anchored at root
+if [[ $URL == /*  ]]; then
+  # If path is globbed (/path/to/*.rpm), expand it
+  path=$(sudo find / -wholename $URL)
+  sudo yum install -y "$path"
+elif [[ $URL == *.rpm ]]; then
   sudo yum install -y "$URL"
-elif [[ $URL == *.repo ]]
-then
+elif [[ $URL == *.repo ]]; then
   # shellcheck disable=SC2154
   repo_file="${{URL##*/}}"
   sudo curl --silent -o /etc/yum.repos.d/"$repo_file" "$URL"
