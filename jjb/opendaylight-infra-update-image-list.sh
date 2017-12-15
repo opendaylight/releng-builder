566849d2c2b088a970f137645c16284c01843fcf
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-12-15 11:27:07 -0500

    Fix auto-update image docs script
    Should pick up the new Vexxhost images now.
    
    LF-Issue: RELENG-724
    Change-Id: Ic0f898e9f84c7adc4bdc2346c52a1169d5b4705a
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/opendaylight-infra-update-image-list.sh
+++ jjb/opendaylight-infra-update-image-list.sh
@@ -24,8 +24,10 @@ EOF
 # Blank line before EOF is on purpose to ensure there is spacing.
 
 IFS=$'\n'
-IMAGES=($(openstack image list --public -f value -c Name))
+IMAGES=($(openstack image list --long -f value -c Name -c Protected \
+    | grep 'ZZCI.*True' | sed 's/ True//'))
 for i in "${IMAGES[@]}"; do
+    echo "Adding image $i"
     echo "* $i" >> "$WORKSPACE/docs/cloud-images.rst"
 done
 
