d82f7b0a697ece3e96d725ceef17567d33970276
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-01-05 12:06:49 -0500

    Add check for max robot node types
    We do not allow more than 1 robot node type to be in use at the same
    time. This ensures that we can properly limit the number of
    simultanious robot builds properly due to the way the Jenkins
    OpenStack cloud plugin works. All templates should always use the
    same Robot VM so any updates to change this needs to be changed in
    all robot templates.
    
    Issue: RELENG-75
    Change-Id: Id2087bb92a860379fc2437a1a58cac578e853be5
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ check_robot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Ensures that we are only ever using one robot system
+#
+# Due to the way the Jenkins OpenStack Cloud plugin works we can only limit
+# max parallel robot systems by the VM. So having multiple VM types makes it
+# very difficult for us to properly limit the amount of parallel robot runs.
+
+robots=$(find . -name "*.yaml" -print0 \
+    | xargs -0 grep centos7-robot | awk '{print $NF}' \
+    | sort | uniq | wc -l)
+
+if [ "$robots" -gt 1 ]; then
+    echo "ERROR: More than one robot system type definition detected."
+    echo "Please ensure that ALL templates use the same robot nodes."
+    echo "Infra does not support more than 1 robot node type in use."
+    exit 1
+fi
