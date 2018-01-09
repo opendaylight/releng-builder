b623e8ac4c19506cd394a72d5da658a02d49456d
Jamo Luhrsen <jluhrsen@redhat.com>
2018-01-09 14:34:43 -0800

    Use > system prompt as that's robots expectation
    Change-Id: I479bdb17c1eab5cbda1f1c41aa773cc5bf77e366
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jenkins-scripts/basic_settings.sh
+++ jenkins-scripts/basic_settings.sh
@@ -31,4 +31,10 @@ cat <<EOKNOWN >  /etc/ssh/ssh_known_hosts
 [140.211.169.26]:29418,[git.opendaylight.org]:29418 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAyRXyHEw/P1iZr/fFFzbodT5orVV/ftnNRW59Zh9rnSY5Rmbc9aygsZHdtiWBERVVv8atrJSdZool75AglPDDYtPICUGWLR91YBSDcZwReh5S9es1dlQ6fyWTnv9QggSZ98KTQEuE3t/b5SfH0T6tXWmrNydv4J2/mejKRRLU2+oumbeVN1yB+8Uau/3w9/K5F5LgsDDzLkW35djLhPV8r0OfmxV/cAnLl7AaZlaqcJMA+2rGKqM3m3Yu+pQw4pxOfCSpejlAwL6c8tA9naOvBkuJk+hYpg5tDEq2QFGRX5y1F9xQpwpdzZROc5hdGYntM79VMMXTj+95dwVv/8yTsw==
 EOKNOWN
 
+# To handle the prompt style that is expected all over the environment
+# with how use use robotframework we need to make sure that it is
+# consistent for any of the users that are created during dynamic spin
+# ups
+echo 'PS1="[\u@\h \W]> "' >> /etc/skel/.bashrc
+
 # vim: sw=2 ts=2 sts=2 et :
