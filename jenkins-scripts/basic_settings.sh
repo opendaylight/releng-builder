fdf3eaf4f5e63085ec8ae385b40208ddb762b25d
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-01-10 13:24:47 -0500

    Add devvexx hostkey to spinup scripts
    Change-Id: I26d0d7a26875a1c51f201a40c885f911bdcb050a
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/basic_settings.sh
+++ jenkins-scripts/basic_settings.sh
@@ -29,6 +29,7 @@ EOSSH
 
 cat <<EOKNOWN >  /etc/ssh/ssh_known_hosts
 [140.211.169.26]:29418,[git.opendaylight.org]:29418 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAyRXyHEw/P1iZr/fFFzbodT5orVV/ftnNRW59Zh9rnSY5Rmbc9aygsZHdtiWBERVVv8atrJSdZool75AglPDDYtPICUGWLR91YBSDcZwReh5S9es1dlQ6fyWTnv9QggSZ98KTQEuE3t/b5SfH0T6tXWmrNydv4J2/mejKRRLU2+oumbeVN1yB+8Uau/3w9/K5F5LgsDDzLkW35djLhPV8r0OfmxV/cAnLl7AaZlaqcJMA+2rGKqM3m3Yu+pQw4pxOfCSpejlAwL6c8tA9naOvBkuJk+hYpg5tDEq2QFGRX5y1F9xQpwpdzZROc5hdGYntM79VMMXTj+95dwVv/8yTsw==
+[199.204.45.87]:22,[devvexx.opendaylight.org]:22 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLnAeCuBj9Bab0xKpRevOHSh8Jmlc5lWXtfHLQpZzMz8lsSD3VBuK69AEg3xiavj+rMjyY33JyDg1YOxYWrvfjg=
 EOKNOWN
 
 # To handle the prompt style that is expected all over the environment
