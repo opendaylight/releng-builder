a6e80ceacf5d79cc0e38b3b3ae110c03256fc5ea
Jamo Luhrsen <jluhrsen@redhat.com>
2018-01-17 09:06:10 -0800

    Add common-functions.sh bash lib
    tired of looking at duplicated code and having to remember to
    search for all instances of it. This can be the start of some
    cleanup, if others agree it's a good idea.
    
    first round: move the Java vars setup code to a common
    function.
    
    Change-Id: If03949e16aae3561ac8636bc4de0b1a17dad01ae
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/integration-configure-clustering.sh
+++ jjb/integration/integration-configure-clustering.sh
@@ -3,6 +3,7 @@
 # ${ROBOT_VENV} comes from the integration-install-robotframework.sh
 # script.
 # shellcheck source=${ROBOT_VENV}/bin/activate disable=SC1091
+source ./common-functions.sh
 source ${ROBOT_VENV}/bin/activate
 
 echo "#################################################"
@@ -110,22 +111,7 @@ if [ "${ODL_ENABLE_L3_FWD}" == "yes" ]; then
   cat ${CUSTOMPROP}
 fi
 
-echo "Configure java home and max memory..."
-sed -ie 's%^# export JAVA_HOME%export JAVA_HOME="\${JAVA_HOME:-${JAVA_HOME}}"%g' ${MEMCONF}
-sed -ie 's/JAVA_MAX_MEM="2048m"/JAVA_MAX_MEM="${CONTROLLERMEM}"/g' ${MEMCONF}
-cat ${MEMCONF}
-
-echo "Set Java version"
-sudo /usr/sbin/alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
-sudo /usr/sbin/alternatives --set java ${JAVA_HOME}/bin/java
-echo "JDK default version ..."
-java -version
-
-echo "Set JAVA_HOME"
-export JAVA_HOME="${JAVA_HOME}"
-# Did you know that in HERE documents, single quote is an ordinary character, but backticks are still executing?
-JAVA_RESOLVED=\`readlink -e "\${JAVA_HOME}/bin/java"\`
-echo "Java binary pointed at by JAVA_HOME: \${JAVA_RESOLVED}"
+set_java_vars
 
 # Copy shard file if exists
 if [ -f /tmp/custom_shard_config.txt ]; then
