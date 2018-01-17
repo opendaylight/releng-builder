b84250dbabc7848a3aa51a13e4f41f008f1d0d50
Jamo Luhrsen <jluhrsen@redhat.com>
2018-01-17 09:06:10 -0800

    Add common-functions.sh bash lib
    tired of looking at duplicated code and having to remember to
    search for all instances of it. This can be the start of some
    cleanup, if others agree it's a good idea.
    
    first round: move the Java vars setup code to a common
    function.
    
    Change-Id: If03949e16aae3561ac8636bc4de0b1a17dad01ae
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/integration-deploy-controller-run-test.sh
+++ jjb/integration/integration-deploy-controller-run-test.sh
@@ -106,25 +106,10 @@ if [ -n "${CONTROLLERDEBUGMAP}" ]; then
 fi
 cat ${LOGCONF}
 
-echo "Configure java home and max memory..."
-sed -ie 's%^# export JAVA_HOME%export JAVA_HOME="\${JAVA_HOME:-${JAVA_HOME}}"%g' ${MEMCONF}
-sed -ie 's/JAVA_MAX_MEM="2048m"/JAVA_MAX_MEM="${CONTROLLERMEM}"/g' ${MEMCONF}
-cat ${MEMCONF}
+set_java_vars
 
 echo "Listing all open ports on controller system..."
 netstat -pnatu
-
-echo "Set Java version"
-sudo /usr/sbin/alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
-sudo /usr/sbin/alternatives --set java ${JAVA_HOME}/bin/java
-echo "JDK default version..."
-java -version
-
-echo "Set JAVA_HOME"
-export JAVA_HOME="${JAVA_HOME}"
-# Did you know that in HERE documents, single quote is an ordinary character, but backticks are still executing?
-JAVA_RESOLVED=\`readlink -e "\${JAVA_HOME}/bin/java"\`
-echo "Java binary pointed at by JAVA_HOME: \${JAVA_RESOLVED}"
 
 if [ "${ENABLE_HAPROXY_FOR_NEUTRON}" == "yes" ]; then
 
