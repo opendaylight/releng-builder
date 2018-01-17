29f0589e3fb157193e0a0538c3483c65dd5f2295
Jamo Luhrsen <jluhrsen@redhat.com>
2018-01-17 09:06:10 -0800

    Add common-functions.sh bash lib
    tired of looking at duplicated code and having to remember to
    search for all instances of it. This can be the start of some
    cleanup, if others agree it's a good idea.
    
    first round: move the Java vars setup code to a common
    function.
    
    Change-Id: If03949e16aae3561ac8636bc4de0b1a17dad01ae
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

---
+++ jjb/integration/common-functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Setup JAVA_HOME and MAX_MEM Value in ODL startup config file
+function set_java_vars() {
+
+    echo "Configure java home and max memory..."
+    sed -ie 's%^# export JAVA_HOME%export JAVA_HOME="\${JAVA_HOME:-${JAVA_HOME}}"%g' ${MEMCONF}
+    sed -ie 's/JAVA_MAX_MEM="2048m"/JAVA_MAX_MEM="${CONTROLLERMEM}"/g' ${MEMCONF}
+    cat ${MEMCONF}
+
+    echo "Set Java version"
+    sudo /usr/sbin/alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
+    sudo /usr/sbin/alternatives --set java ${JAVA_HOME}/bin/java
+    echo "JDK default version ..."
+    java -version
+
+    echo "Set JAVA_HOME"
+    export JAVA_HOME="${JAVA_HOME}"
+    # Did you know that in HERE documents, single quote is an ordinary character, but backticks are still executing?
+    JAVA_RESOLVED=\`readlink -e "\${JAVA_HOME}/bin/java"\`
+    echo "Java binary pointed at by JAVA_HOME: \${JAVA_RESOLVED}"
+} # set_java_vars()
