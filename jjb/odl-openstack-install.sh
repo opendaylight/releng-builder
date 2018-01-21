ba90c6c509ee17f0e68b393c4bd9b0a3586a28d7
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-01-21 18:56:25 -0500

    Add a new odl-openstack-install macro
    Creates a reusable macro for getting openstack cli installed.
    
    Change-Id: I9ebcd9a9d6e338ef5d5b3c58d70d4af42bc128ea
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/odl-openstack-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017, 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Installs the openstack cli to /tmp/v/openstack
+
+virtualenv "/tmp/v/openstack"
+# shellcheck source=/tmp/v/openstack/bin/activate disable=SC1091
+source "/tmp/v/openstack/bin/activate"
+pip install --upgrade pip
+pip install --upgrade python-openstackclient python-heatclient
+pip install --upgrade pipdeptree
+pipdeptree
