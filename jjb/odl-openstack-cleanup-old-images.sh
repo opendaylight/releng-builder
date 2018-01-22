376e31e48e516e876e08b2057c412f111fc117d3
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-01-21 19:41:29 -0500

    Move cleanup-old-images job into openstack-cron
    Moves the cleanup-old-images script into the new openstack-cron job
    to consolidate these jobs to save OpenStack resources.
    
    Change-Id: Ie390939485bdf731d48d7eb2e0d71ef3665d2f9e
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/odl-openstack-cleanup-old-images.sh
+++ jjb/odl-openstack-cleanup-old-images.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # SPDX-License-Identifier: EPL-1.0
 ##############################################################################
-# Copyright (c) 2017 The Linux Foundation and others.
+# Copyright (c) 2017 - 2018 The Linux Foundation and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
+# Removes openstack images older than 30 days in the cloud
+echo "---> Cleanup old images"
 
-lftools openstack --os-cloud vex \
-    image cleanup --days=30
+# shellcheck source=/tmp/v/lftools/bin/activate disable=SC1091
+source "/tmp/v/lftools/bin/activate"
+lftools openstack --os-cloud vex image cleanup --days=30
