6a8f20746c390a97d36e7cf4709817ab10216b1b
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-01-21 19:06:03 -0500

    Move image-protect into new openstack-cron job
    Creates a new 'builder-openstack-cron' job which will be used to
    consolidate all of the regularly scheduled cron jobs currently
    deployed in ODL. This will save some resources of having multiple
    VM spinups for jobs that take less than a minute to run.
    
    Change-Id: I28246a0a216101f20d44e20184116109ca0a6966
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/odl-openstack-check-image-protection.sh
+++ jjb/odl-openstack-check-image-protection.sh
@@ -17,13 +17,8 @@
 # image protect setting to "True" to prevent the image from getting purged
 # by the cleanup old images job.
 
-virtualenv "/tmp/v/openstack"
 # shellcheck source=/tmp/v/openstack/bin/activate disable=SC1091
 source "/tmp/v/openstack/bin/activate"
-pip install --upgrade pip
-pip install --upgrade python-openstackclient
-pip install --upgrade pipdeptree
-pipdeptree
 
 declare -a images
 readarray -t images <<< "$(grep -r _system_image: --include \*.yaml \
