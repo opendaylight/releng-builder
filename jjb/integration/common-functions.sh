c097fd4a39111776f3c661e03ab5c5d100b9d1e9
Jamo Luhrsen <jluhrsen@redhat.com>
2018-01-30 17:46:10 -0800

    Add common functions to openstack deploy script
    Change-Id: I03816aeaa41b0c8376f431f70da94e9b9260bd21
    Signed-off-by: Sam Hague <shague@redhat.com>

--- jjb/integration/common-functions.sh
+++ jjb/integration/common-functions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "common-functions.sh is being sourced"
+
+BUNDLEFOLDER=$1
+
 # Basic controller configuration settings
 export MAVENCONF=/tmp/${BUNDLEFOLDER}/etc/org.ops4j.pax.url.mvn.cfg
 export FEATURESCONF=/tmp/${BUNDLEFOLDER}/etc/org.apache.karaf.features.cfg
@@ -13,12 +17,31 @@ export AKKACONF=/tmp/${BUNDLEFOLDER}/configuration/initial/akka.conf
 export MODULESCONF=/tmp/${BUNDLEFOLDER}/configuration/initial/modules.conf
 export MODULESHARDSCONF=/tmp/${BUNDLEFOLDER}/configuration/initial/module-shards.conf
 
+function print_common_env() {
+    cat << EOF
+common-functions environment:
+MAVENCONF: ${MAVENCONF}
+FEATURESCONF: ${FEATURESCONF}
+CUSTOMPROP: ${CUSTOMPROP}
+LOGCONF: ${LOGCONF}
+MEMCONF: ${MEMCONF}
+CONTROLLERMEM: ${CONTROLLERMEM}
+AKKACONF: ${AKKACONF}
+MODULESCONF: ${MODULESCONF}
+MODULESHARDSCONF: ${MODULESHARDSCONF}
+
+EOF
+}
+print_common_env
+
 # Setup JAVA_HOME and MAX_MEM Value in ODL startup config file
 function set_java_vars() {
+    local JAVA_HOME=$1
 
     echo "Configure java home and max memory..."
-    sed -ie 's%^# export JAVA_HOME%export JAVA_HOME="\${JAVA_HOME:-${JAVA_HOME}}"%g' ${MEMCONF}
-    sed -ie 's/JAVA_MAX_MEM="2048m"/JAVA_MAX_MEM="${CONTROLLERMEM}"/g' ${MEMCONF}
+    sed -ie 's/^# export JAVA_HOME/export JAVA_HOME=${JAVA_HOME:-'"${JAVA_HOME}"'}/g' ${MEMCONF}
+    sed -ie 's/JAVA_MAX_MEM="2048m"/JAVA_MAX_MEM='"${CONTROLLERMEM}"'/g' ${MEMCONF}
+    echo "cat ${MEMCONF}"
     cat ${MEMCONF}
 
     echo "Set Java version"
@@ -30,6 +53,6 @@ function set_java_vars() {
     echo "Set JAVA_HOME"
     export JAVA_HOME="${JAVA_HOME}"
     # shellcheck disable=SC2037
-    JAVA_RESOLVED=\`readlink -e "\${JAVA_HOME}/bin/java"\`
-    echo "Java binary pointed at by JAVA_HOME: \${JAVA_RESOLVED}"
+    JAVA_RESOLVED=$(readlink -e "${JAVA_HOME}/bin/java")
+    echo "Java binary pointed at by JAVA_HOME: ${JAVA_RESOLVED}"
 } # set_java_vars()
