7b2a3f580a542d0e2347e5d0238e51d5ee19e825
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-02-07 22:08:56 -0500

    Add global-vars-[releng|sandbox] cfg file
    New Global Jenkins configuration file used by new jenkins-cfg job
    to manage Global environment variables. This patch requires
    global-jjb v0.16.0.
    
    Issue: https://jira.linuxfoundation.org/browse/RELENG-753
    Change-Id: I7b5683a0ebc2f17ac5fbed86610b82e88253e374
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jenkins-config/global-vars-sandbox.sh
@@ -0,0 +1,9 @@
+GERRIT_URL=https://git.opendaylight.org/gerrit
+GIT_BASE=git://devvexx.opendaylight.org/mirror/$PROJECT
+GIT_URL=git://devvexx.opendaylight.org/mirror
+JENKINS_HOSTNAME=vex-yul-odl-jenkins-2
+LOGS_SERVER=https://logs.opendaylight.org
+NEXUS_URL=https://nexus.opendaylight.org
+ODLNEXUSPROXY=https://nexus.opendaylight.org
+SILO=sandbox
+SONAR_URL=https://sonar.opendaylight.org
