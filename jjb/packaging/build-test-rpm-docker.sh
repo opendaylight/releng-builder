4508feefa62a7702b0c61106716b076b3045e11f
Manuel Buil <mbuil@suse.com>
2018-02-13 18:52:04 +0100

    WIP - Add suse support to the test scripts
    suse rpm build and test happens inside a docker container
    
    Change-Id: I31ee0558f3cc76e27f462f1d3ac528b5906a122c
    Signed-off-by: Manuel Buil <mbuil@suse.com>

---
+++ jjb/packaging/build-test-rpm-docker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+docker_id=$(sudo docker run -di --cap-add=SYS_ADMIN -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $WORKSPACE/builder/jjb/packaging:/home/scripts opensuse /usr/lib/systemd/systemd)
+
+sudo docker exec $docker_id ping 8.8.8.8 -c 4
+
+sudo docker exec $docker_id ping www.google.de -c 4
+
+sudo docker exec $docker_id /usr/bin/zypper -n install curl expect nmap sudo openssh python-virtualenv python rpmdevtools rpmbuild
+
+sudo docker exec $docker_id /bin/bash /home/scripts/build-rpm-snap.sh
+
+sudo docker exec $docker_id /bin/bash /home/scripts/test-rpm-deps.sh
+
+sudo docker exec $docker_id /bin/bash /home/scripts/install-rpm.sh
+
+sudo docker exec $docker_id /bin/bash /home/scripts/start-odl.sh
+
+sudo docker exec $docker_id /bin/bash /home/scripts/test-ports-nofeature.sh
+
+sudo docker exec $docker_id /usr/bin/expect /home/scripts/test-karaf.expect
+
+sudo docker exec $docker_id /bin/bash /home/scripts/test-rest-ok.sh
+
+sudo docker exec $docker_id /bin/bash /home/scripts/stop-odl.sh
+
+sudo docker exec $docker_id /bin/bash /home/scripts/uninstall-rpm.sh
