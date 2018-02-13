d5619be08992954778b79b031b8e4a25920e6755
Manuel Buil <mbuil@suse.com>
2018-02-13 18:52:04 +0100

    WIP - Add suse support to the test scripts
    suse rpm build and test happens inside a docker container
    
    Change-Id: I31ee0558f3cc76e27f462f1d3ac528b5906a122c
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- jjb/packaging/test-rpm-deps.sh
+++ jjb/packaging/test-rpm-deps.sh
@@ -15,40 +15,76 @@ else
     echo 1
 fi
 
-# If path is globbed (/path/to/*.rpm), expand it
-path=$(sudo find / -wholename /home/$USER/rpmbuild/RPMS/noarch/*.rpm)
+if [ -f /usr/bin/yum ]; then
+  # If path is globbed (/path/to/*.rpm), expand it
+  path=$(sudo find / -wholename /home/$USER/rpmbuild/RPMS/noarch/*.rpm)
 
-# If no RPM found, fail clearly
-if [ -z $path ]; then
-    echo "RPM not found"
-    exit 1
+  # If no RPM found, fail clearly
+  if [ -z $path ]; then
+      echo "RPM not found"
+      exit 1
+  fi
+
+  # Requirements for package where SRPM was built into noarch on CentOS CBS
+  # rpm -qp opendaylight-8.0.0-0.1.20171125rel2049.el7.noarch.rpm --requires
+  # shellcheck disable=SC2034
+  declare -a expected_deps=( "/bin/bash"
+                             "/bin/sh"
+                             "java >= 1:1.8.0"
+                             "rpmlib(CompressedFileNames) <= 3.0.4-1"
+                             "rpmlib(FileDigests) <= 4.6.0-1"
+                             "rpmlib(PartialHardlinkSets) <= 4.0.4-1"
+                             "rpmlib(PayloadFilesHavePrefix) <= 4.0-1"
+                             "shadow-utils"
+                             "rpmlib(PayloadIsXz) <= 5.2-1" )
+
+  # Karaf 4 distros also have a /usr/bin/env requirement INTPAK-120
+  if [[ ! $path == *opendaylight-6*  ]]; then
+      expected_deps+=( "/usr/bin/env" )
+  fi
+
+  # shellcheck disable=SC2034
+  mapfile -t actual_deps < <( rpm -qp /home/$USER/rpmbuild/RPMS/noarch/*.rpm --requires )
+  # shellcheck disable=SC2154 disable=SC2145
+  printf 'Dependency found: %s\n' "${actual_deps[@]}"
+
+  # shellcheck disable=SC2154,SC2145,SC2034,SC2207
+  diff_deps=(`echo "${expected_deps[@]}" "${actual_deps[@]}" | tr ' ' '\n' | sort | uniq -u`)
+
+
+elif [ -f /usr/bin/zypper ]; then
+  # If path is globbed (/path/to/*.rpm), expand it
+  path=$(find /root/rpmbuild/RPMS -wholename *.rpm)
+
+  # If no RPM found, fail clearly
+  if [ -z $path ]; then
+      echo "RPM not found"
+      exit 1
+  fi
+
+  declare -a expected_deps=( "/bin/bash"
+                             "/bin/sh"
+                             "java >= 1.8.0"
+                             "rpmlib(CompressedFileNames) <= 3.0.4-1"
+                             "rpmlib(PayloadFilesHavePrefix) <= 4.0-1"
+                             "shadow"
+                             "rpmlib(PayloadIsLzma) <= 4.4.6-1" )
+
+  # Karaf 4 distros also have a /usr/bin/env requirement INTPAK-120
+  if [[ ! $path == *opendaylight-6*  ]]; then
+      expected_deps+=( "/usr/bin/env" )
+  fi
+
+  # shellcheck disable=SC2034
+  mapfile -t actual_deps < <( rpm -qp /root/rpmbuild/RPMS/noarch/*.rpm --requires )
+  # shellcheck disable=SC2154 disable=SC2145
+  printf 'Dependency found: %s\n' "${actual_deps[@]}"
+
+  # shellcheck disable=SC2154,SC2145,SC2034,SC2207
+  diff_deps=(`echo "${expected_deps[@]}" "${actual_deps[@]}" | tr ' ' '\n' | sort | uniq -u`)
+
 fi
 
-# Requirements for package where SRPM was built into noarch on CentOS CBS
-# rpm -qp opendaylight-8.0.0-0.1.20171125rel2049.el7.noarch.rpm --requires
-# shellcheck disable=SC2034
-declare -a expected_deps=( "/bin/bash"
-                           "/bin/sh"
-                           "java >= 1:1.8.0"
-                           "rpmlib(CompressedFileNames) <= 3.0.4-1"
-                           "rpmlib(FileDigests) <= 4.6.0-1"
-                           "rpmlib(PartialHardlinkSets) <= 4.0.4-1"
-                           "rpmlib(PayloadFilesHavePrefix) <= 4.0-1"
-                           "shadow-utils"
-                           "rpmlib(PayloadIsXz) <= 5.2-1" )
-
-# Karaf 4 distros also have a /usr/bin/env requirement INTPAK-120
-if [[ ! $path == *opendaylight-6*  ]]; then
-    expected_deps+=( "/usr/bin/env" )
-fi
-
-# shellcheck disable=SC2034
-mapfile -t actual_deps < <( rpm -qp /home/$USER/rpmbuild/RPMS/noarch/*.rpm --requires )
-# shellcheck disable=SC2154 disable=SC2145
-printf 'Dependency found: %s\n' "${actual_deps[@]}"
-
-# shellcheck disable=SC2154,SC2145,SC2034,SC2207
-diff_deps=(`echo "${expected_deps[@]}" "${actual_deps[@]}" | tr ' ' '\n' | sort | uniq -u`)
 # shellcheck disable=SC2154 disable=SC2145 disable=SC2068 disable=SC2170 disable=SC1083
 if [ ${#diff_deps[*]} -eq 0 ]; then
     echo "RPM requirements are as expected"
