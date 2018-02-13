c540ba2baa103ff1d512ce9ea500889eabbc3af4
Manuel Buil <mbuil@suse.com>
2018-02-13 18:52:04 +0100

    Add suse support for the snaps build
    suse snaps rpm build and test happens inside a docker container
    This patch requires the following patch to be merged:
    
    https://git.opendaylight.org/gerrit/#/c/68114/
    
    It reuses most of the building and testing scripts. Those scripts
    for epel will be executed inside a VM (as it was done before) and
    for suse, those will be executed inside a docker container.
    
    It avoids duplicating the packaging-build-rpm-snap job using the
    variable {distro} to differentiate between suse and epel. However,
    to reuse most building and testing scripts, we inject the DISTRO
    variable into the scripts to detect if we are inside the VM or the
    container. When building a suse rpm, all scripts will be executed
    twice and thus if we detect that it is a suse rpm and we are inside
    a VM, it will exit the script
    
    Change-Id: I31ee0558f3cc76e27f462f1d3ac528b5906a122c
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- jjb/packaging/test-rpm-deps.sh
+++ jjb/packaging/test-rpm-deps.sh
@@ -6,8 +6,22 @@
 #   -o pipefail: Fail on errors in scripts this calls, give stacktrace
 set -ex -o pipefail
 
+#-------------------------------------------------------------------------------
+# Exit if opensuse and in a VM
+#-------------------------------------------------------------------------------
+# Jenkins template and scripts are shared between suse and red hat to build and
+# test the rpms. However, all the suse processing is done in a container whereas
+# redhat processing is done in a VM. We should exit if we detect that this
+# script is going to test a opensuse rpm inside a VM. DISTRO variable only
+# exists when the script is executed in the VM.
+#-------------------------------------------------------------------------------
+if [ "$DISTRO" == "opensuse-42" ]; then
+  echo "We are in a VM, nothing to do for opensuse"
+  exit 0
+fi
+
 # Verify exactly 1 RPM is in the path we expect
-set -- /home/$USER/rpmbuild/RPMS/noarch/*.rpm
+set -- $HOME/rpmbuild/RPMS/noarch/*.rpm
 if [ $# -eq 1 ]; then
     echo "Found one RPM in build out dir, as expected"
 else
@@ -16,7 +30,7 @@ else
 fi
 
 # If path is globbed (/path/to/*.rpm), expand it
-path=$(sudo find / -wholename /home/$USER/rpmbuild/RPMS/noarch/*.rpm)
+path=$(sudo find $HOME/rpmbuild/RPMS/noarch -wholename *.rpm)
 
 # If no RPM found, fail clearly
 if [ -z $path ]; then
@@ -24,31 +38,59 @@ if [ -z $path ]; then
     exit 1
 fi
 
-# Requirements for package where SRPM was built into noarch on CentOS CBS
-# rpm -qp opendaylight-8.0.0-0.1.20171125rel2049.el7.noarch.rpm --requires
-# shellcheck disable=SC2034
-declare -a expected_deps=( "/bin/bash"
-                           "/bin/sh"
-                           "java >= 1:1.8.0"
-                           "rpmlib(CompressedFileNames) <= 3.0.4-1"
-                           "rpmlib(FileDigests) <= 4.6.0-1"
-                           "rpmlib(PartialHardlinkSets) <= 4.0.4-1"
-                           "rpmlib(PayloadFilesHavePrefix) <= 4.0-1"
-                           "shadow-utils"
-                           "rpmlib(PayloadIsXz) <= 5.2-1" )
 
-# Karaf 4 distros also have a /usr/bin/env requirement INTPAK-120
-if [[ ! $path == *opendaylight-6*  ]]; then
-    expected_deps+=( "/usr/bin/env" )
+if [ -f /usr/bin/yum ]; then
+  # Requirements for package where SRPM was built into noarch on CentOS CBS
+  # rpm -qp opendaylight-8.0.0-0.1.20171125rel2049.el7.noarch.rpm --requires
+  # shellcheck disable=SC2034
+  declare -a expected_deps=( "/bin/bash"
+                             "/bin/sh"
+                             "java >= 1:1.8.0"
+                             "rpmlib(CompressedFileNames) <= 3.0.4-1"
+                             "rpmlib(FileDigests) <= 4.6.0-1"
+                             "rpmlib(PartialHardlinkSets) <= 4.0.4-1"
+                             "rpmlib(PayloadFilesHavePrefix) <= 4.0-1"
+                             "shadow-utils"
+                             "rpmlib(PayloadIsXz) <= 5.2-1" )
+
+  # Karaf 4 distros also have a /usr/bin/env requirement INTPAK-120
+  if [[ ! $path == *opendaylight-6*  ]]; then
+      expected_deps+=( "/usr/bin/env" )
+  fi
+
+  # shellcheck disable=SC2034
+  mapfile -t actual_deps < <( rpm -qp /home/$USER/rpmbuild/RPMS/noarch/*.rpm --requires )
+  # shellcheck disable=SC2154 disable=SC2145
+  printf 'Dependency found: %s\n' "${actual_deps[@]}"
+
+  # shellcheck disable=SC2154,SC2145,SC2034,SC2207
+  diff_deps=(`echo "${expected_deps[@]}" "${actual_deps[@]}" | tr ' ' '\n' | sort | uniq -u`)
+
+
+elif [ -f /usr/bin/zypper ]; then
+  declare -a expected_deps=( "/bin/bash"
+                             "/bin/sh"
+                             "java >= 1.8.0"
+                             "rpmlib(CompressedFileNames) <= 3.0.4-1"
+                             "rpmlib(PayloadFilesHavePrefix) <= 4.0-1"
+                             "shadow"
+                             "rpmlib(PayloadIsLzma) <= 4.4.6-1" )
+
+  # Karaf 4 distros also have a /usr/bin/env requirement INTPAK-120
+  if [[ ! $path == *opendaylight-6*  ]]; then
+      expected_deps+=( "/usr/bin/env" )
+  fi
+
+  # shellcheck disable=SC2034
+  mapfile -t actual_deps < <( rpm -qp $HOME/rpmbuild/RPMS/noarch/*.rpm --requires )
+  # shellcheck disable=SC2154 disable=SC2145
+  printf 'Dependency found: %s\n' "${actual_deps[@]}"
+
+  # shellcheck disable=SC2154,SC2145,SC2034,SC2207
+  diff_deps=(`echo "${expected_deps[@]}" "${actual_deps[@]}" | tr ' ' '\n' | sort | uniq -u`)
+
 fi
 
-# shellcheck disable=SC2034
-mapfile -t actual_deps < <( rpm -qp /home/$USER/rpmbuild/RPMS/noarch/*.rpm --requires )
-# shellcheck disable=SC2154 disable=SC2145
-printf 'Dependency found: %s\n' "${actual_deps[@]}"
-
-# shellcheck disable=SC2154,SC2145,SC2034,SC2207
-diff_deps=(`echo "${expected_deps[@]}" "${actual_deps[@]}" | tr ' ' '\n' | sort | uniq -u`)
 # shellcheck disable=SC2154 disable=SC2145 disable=SC2068 disable=SC2170 disable=SC1083
 if [ ${#diff_deps[*]} -eq 0 ]; then
     echo "RPM requirements are as expected"
