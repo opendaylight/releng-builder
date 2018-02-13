ccf7bed30826a67681831efc5941dc2032a0bab0
Manuel Buil <mbuil@suse.com>
2018-02-13 18:52:04 +0100

    Add suse support for the snaps build
    suse snaps rpm build and test happens inside a docker container
    This patch requires the following patch to be merged:
    
    https://git.opendaylight.org/gerrit/#/c/68114/
    
    Change-Id: I31ee0558f3cc76e27f462f1d3ac528b5906a122c
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- jjb/packaging/stop-odl.sh
+++ jjb/packaging/stop-odl.sh
@@ -6,6 +6,21 @@
 #   -o pipefail: Fail on errors in scripts this calls, give stacktrace
 set -ex -o pipefail
 
+#-------------------------------------------------------------------------------
+# Exit if suse and in a VM
+#-------------------------------------------------------------------------------
+# Jenkins template and scripts are shared between suse and red hat to build and
+# test the rpms. However, all the suse processing is done in a container whereas
+# redhat processing is done in a VM. We should exit if we detect that this
+# script is going to test a suse rpm inside a VM. DISTRO variable only exists
+# when the script is executed in the VM.
+#-------------------------------------------------------------------------------
+if [ "$DISTRO" == "suse" ]; then
+  echo "We are in a VM, nothing to do for suse"
+  exit 0
+fi
+
+
 # Stop OpenDaylight
 sudo systemctl stop opendaylight
 
---
+++ jjb/packaging/test-rpm-docker.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+#-------------------------------------------------------------------------------
+# Exit if epel
+#-------------------------------------------------------------------------------
+# Jenkins template and scripts are shared between suse and red hat to build and
+# test the rpms. However, all the suse processing is done in a container whereas
+# redhat processing is done in a VM. This script does the build in a VM
+#-------------------------------------------------------------------------------
+if [ "$DISTRO" == "epel-7" ]; then
+  echo "Nothing to do for epel"
+  exit 0
+fi
+
+scripts_path=/builder/jjb/packaging
+
+# A docker container must have been created by the build script
+docker_id=$(sudo docker ps -qf name=build_rpm_suse)
+
+sudo docker exec $docker_id /bin/bash $scripts_path/test-rpm-deps.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/install-rpm.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/start-odl.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/test-ports-nofeature.sh
+
+sudo docker exec $docker_id /usr/bin/expect $scripts_path/test-karaf.expect
+
+sudo docker exec $docker_id /bin/bash $scripts_path/test-rest-ok.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/stop-odl.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/uninstall-rpm.sh
+
+if [ "$SILO" == "sandbox" ]; then
+  echo "Not uploading RPMs to Nexus because we are in the Sandbox"
+elif [ "$SILO" == "releng" ]; then
+  UPLOAD_FILES_PATH="$WORKSPACE/upload_files"
+  RPM_NAME=$(sudo docker exec $docker_id find /root/rpmbuild/RPMS/noarch/ -wholename '*.rpm')
+  SRPM_NAME=$(sudo docker exec $docker_id find /root/rpmbuild/SRPMS/ -wholename '*.rpm')
+  mkdir -p "$UPLOAD_FILES_PATH"
+
+  sudo docker cp $docker_id:$RPM_NAME "$_"
+
+  sudo docker cp $docker_id:$SRPM_NAME "$_"
+fi
