4189bd3b4890b66e916bc513c4ea4da9df7f5838
Manuel Buil <mbuil@suse.com>
2018-02-13 18:52:04 +0100

    Add suse support for the snaps build
    suse snaps rpm build and test happens inside a docker container
    This patch requires the following patch to be merged:
    
    https://git.opendaylight.org/gerrit/#/c/68114/
    
    It reuses most of the building and testing scripts. Those scripts
    for epel will be executed inside a VM (as it was done before) and
    for suse, those will be executed inside a docker container.
    
    It avoids duplicating the packaging-build-rpm-snap job using the
    variable {distro} to differentiate between suse and epel. However,
    to reuse most building and testing scripts, we inject the DISTRO
    variable into the scripts to detect if we are inside the VM or the
    container. When building a suse rpm, all scripts will be executed
    twice and thus if we detect that it is a suse rpm and we are inside
    a VM, it will exit the script
    
    Change-Id: I31ee0558f3cc76e27f462f1d3ac528b5906a122c
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- jjb/packaging/build-rpm-snap-docker.sh
+++ jjb/packaging/build-rpm-snap-docker.sh
@@ -10,12 +10,19 @@ env | grep STREAM > env-file
 
 scripts_path=/builder/jjb/packaging
 
-docker_id=$(sudo docker run --env-file env-file --name build_rpm_epel -di --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos /usr/lib/systemd/systemd)
+if [ "$DISTRO" == "epel-7" ]; then
+  docker_id=$(sudo docker run --env-file env-file --name build_rpm_epel -di --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos /usr/lib/systemd/systemd)
+  sudo docker exec $docker_id /usr/bin/yum -y install sudo rpmdevtools rpmbuild git python-virtualenv python
+elif [ "$DISTRO" == "opensuse-42" ]; then
+  docker_id=$(sudo docker run --env-file env-file --name build_rpm_suse -di --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro opensuse /usr/lib/systemd/systemd)
+  sudo docker exec $docker_id /usr/bin/zypper -n install sudo rpmdevtools rpmbuild git python-virtualenv python
+fi
 
 sudo docker cp "$(pwd)"/packaging/ $docker_id:/packaging
 
-sudo docker exec $docker_id /usr/bin/yum -y install sudo rpmdevtools rpmbuild git python-virtualenv python
-
 sudo docker exec $docker_id git clone https://git.opendaylight.org/gerrit/releng/builder
+
+sudo docker exec $docker_id git -C /builder fetch https://git.opendaylight.org/gerrit/releng/builder refs/changes/01/68201/36
+sudo docker exec $docker_id git -C /builder checkout FETCH_HEAD
 
 sudo docker exec $docker_id /bin/bash $scripts_path/build-rpm-snap.sh
