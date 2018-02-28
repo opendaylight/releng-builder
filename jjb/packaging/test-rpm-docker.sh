ec2ed1d7aa7062c477cf606ab3c1faf44d77c6c5
Manuel Buil <mbuil@suse.com>
2018-02-28 10:10:06 +0100

    Build and test rpm-snap using docker
    As agreed with Daniel, it will be easier if we move epel-7 to docker
    too before merging:
    
    https://git.opendaylight.org/gerrit/#/c/68201/
    
    Change-Id: I81453fcd23e19bf0dfdbc3199d149ebf2dbbb0cb
    Signed-off-by: Manuel Buil <mbuil@suse.com>

---
+++ jjb/packaging/test-rpm-docker.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+scripts_path=/builder/jjb/packaging
+
+# A docker container must have been created by the build script
+docker_id=$(sudo docker ps -qf name=build_rpm_epel)
+
+sudo docker exec $docker_id /bin/bash $scripts_path/test-rpm-deps.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/install-rpm.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/start-odl.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/test-ports-nofeature.sh
+
+# Just to test different versions due to ODLPARENT-139 problem
+if [ $STREAM != "oxygen" ]; then
+  sudo docker exec $docker_id /usr/bin/expect $scripts_path/test-karaf.expect.nooxygen
+  sudo docker exec $docker_id /bin/bash $scripts_path/test-rest-ok.sh
+else
+  sudo docker exec $docker_id /usr/bin/expect $scripts_path/test-karaf.expect
+  #sudo docker exec $docker_id /bin/bash $scripts_path/test-rest-ok.sh
+fi
+
+sudo docker exec $docker_id /bin/bash $scripts_path/stop-odl.sh
+
+sudo docker exec $docker_id /bin/bash $scripts_path/uninstall-rpm.sh
+
+if [ "$SILO" == "sandbox" ]; then
+  echo "Not uploading RPMs to Nexus because we are in the Sandbox"
+elif [ "$SILO" == "releng" ]; then
+  UPLOAD_FILES_PATH="$WORKSPACE/upload_files"
+  RPM_NAME=$(sudo docker exec $docker_id find /root/rpmbuild/RPMS/noarch/ -wholename '*.rpm')
+  SRPM_NAME=$(sudo docker exec $docker_id find /root/rpmbuild/SRPMS/ -wholename '*.rpm')
+  mkdir -p "$UPLOAD_FILES_PATH"
+
+  sudo docker cp $docker_id:$RPM_NAME "$_"
+
+  sudo docker cp $docker_id:$SRPM_NAME "$_"
+fi
