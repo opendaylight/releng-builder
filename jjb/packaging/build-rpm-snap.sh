eabf858da7cdea513df1093ed679702c4fe6402c
Manuel Buil <mbuil@suse.com>
2018-02-28 10:10:06 +0100

    Build and test rpm-snap using docker
    As agreed with Daniel, it will be easier if we move epel-7 to docker
    too before merging:
    
    https://git.opendaylight.org/gerrit/#/c/68201/
    
    Change-Id: I81453fcd23e19bf0dfdbc3199d149ebf2dbbb0cb
    Signed-off-by: Manuel Buil <mbuil@suse.com>
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- jjb/packaging/build-rpm-snap.sh
+++ jjb/packaging/build-rpm-snap.sh
@@ -33,17 +33,3 @@ fi
                                          latest_snap \
                                          --major "$VERSION_MAJOR"
 
-# Publish RPMs to Nexus if in production Jenkins, else host on sandbox Jenkins
-if [ "$SILO" == "sandbox" ]; then
-  # TODO: Host RPMs on Jenkins temporarily
-  echo "Not uploading RPMs to Nexus because running in sandbox"
-elif  [ "$SILO" == "releng" ]; then
-  # Move RPMs (SRPM and noarch) to dir of files that will be uploaded to Nexus
-  UPLOAD_FILES_PATH="$WORKSPACE/upload_files"
-  mkdir -p "$UPLOAD_FILES_PATH"
-  cp "/home/$USER/rpmbuild/RPMS/noarch/"*.rpm "$_"
-  cp "/home/$USER/rpmbuild/SRPMS/"*.rpm "$_"
-else
-  echo "Unknown Jenkins silo: $SILO"
-  exit 1
-fi
