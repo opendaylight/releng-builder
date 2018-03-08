b000549447eab5d73e8f7331da024bf0c299c630
Dimitrios Markou <mardim@intracom-telecom.com>
2018-03-08 17:09:30 +0200

    [WIP] Add tests to build-deb jjb
    Change-Id: I55c2a3d27d27dfcc1555a945fe09593e596c6e0c
    Signed-off-by: Dimitrios Markou <mardim@intracom-telecom.com>

--- jjb/packaging/uninstall-rpm.sh
+++ jjb/packaging/uninstall-rpm.sh
@@ -30,6 +30,18 @@ elif [ -f /usr/bin/zypper ]; then
   else
     echo "OpenDaylight not installed, as expected"
   fi
+elif [ -f /usr/bin/dpkg ]; then
+  # Uninstall ODL
+  sudo dpkg --purge opendaylight
+
+  # Verify ODL not installed
+  if dpkg -s opendaylight; then
+    # Fail if exit code 0, ie ODL is still installed
+    echo "OpenDaylight unexpectedly still installed"
+    exit 1
+  else
+    echo "OpenDaylight not installed, as expected"
+  fi
 else
   echo "The package manager is not supported (not yum or zypper)"
   exit 1
