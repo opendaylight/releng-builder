2224a2a86ae566c4d6eaf51136fe7253b8c0d11f
Dimitrios Markou <mardim@intracom-telecom.com>
2018-03-08 17:09:30 +0200

    Add tests to build-deb jjb
    Change-Id: I55c2a3d27d27dfcc1555a945fe09593e596c6e0c
    Signed-off-by: Dimitrios Markou <mardim@intracom-telecom.com>

---
+++ jjb/packaging/uninstall-deb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+if [ -f /usr/bin/dpkg ]; then
+  # Uninstall ODL
+  sudo dpkg --purge opendaylight
+
+  # Verify ODL not installed
+  if dpkg -s opendaylight; then
+     # Fail if exit code 0, ie ODL is still installed
+     echo "OpenDaylight unexpectedly still installed"
+     exit 1
+  else
+     echo "OpenDaylight not installed, as expected"
+  fi
+else
+  echo "The package manager is not supported"
+  exit 1
+fi
