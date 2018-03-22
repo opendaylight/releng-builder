8b87f783074d3f413feeeba1c2042fd5770cf6a2
Jamo Luhrsen <jluhrsen@redhat.com>
2018-03-22 13:32:57 -0700

    Clean the haproxy.cfg file generation
    removed all the extra loops and additional cat <<EOF
    steps to include them all in a single place
    
    Change-Id: I3779abc90ff9b13e55c9e080df296da0b88679f7
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/integration-deploy-openstack-run-test.sh
+++ jjb/integration/integration-deploy-openstack-run-test.sh
@@ -482,43 +482,25 @@ defaults
 listen opendaylight
   bind ${MGRIP}:8080
   balance source
-EOF
 
-    odlindex=1
-    for odlip in ${ODL_IPS[*]}; do
-        cat >> ${WORKSPACE}/haproxy.cfg << EOF
-  server controller-${odlindex} ${odlip}:8080 check fall 5 inter 2000 rise 2
-EOF
-        odlindex=$((odlindex+1))
-    done
-
-    cat >> ${WORKSPACE}/haproxy.cfg << EOF
 listen opendaylight_rest
   bind ${MGRIP}:8181
   balance source
-EOF
 
-    odlindex=1
-    for odlip in ${ODL_IPS[*]}; do
-        cat >> ${WORKSPACE}/haproxy.cfg << EOF
-  server controller-rest-${odlindex} ${odlip}:8181 check fall 5 inter 2000 rise 2
-EOF
-        odlindex=$((odlindex+1))
-    done
-
-    cat >> ${WORKSPACE}/haproxy.cfg << EOF
-listen opendaylight_rest
+listen opendaylight_websocket
   bind ${MGRIP}:8185
   balance source
+
 EOF
 
     odlindex=1
     for odlip in ${ODL_IPS[*]}; do
-        cat >> ${WORKSPACE}/haproxy.cfg << EOF
-  server controller-websocket-${odlindex} ${odlip}:8185 check fall 5 inter 2000 rise 2
-EOF
+        sed -i '/listen opendaylight$/a server controller-${odlindex} ${odlip}:8080 check fall 5 inter 2000 rise 2' ${WORKSPACE}/haproxy.cfg
+        sed -i '/listen opendaylight_rest$/a server controller-rest-${odlindex} ${odlip}:8181 check fall 5 inter 2000 rise 2' ${WORKSPACE}/haproxy.cfg
+        sed -i '/listen opendaylight_websocket$/a server controller-websocket-${odlindex} ${odlip}:8185 check fall 5 inter 2000 rise 2' ${WORKSPACE}/haproxy.cfg
         odlindex=$((odlindex+1))
     done
+
 
     echo "Dump haproxy.cfg"
     cat ${WORKSPACE}/haproxy.cfg
