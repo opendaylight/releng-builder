825826932a2339563f2305e635ded229e3497fcf
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-04-17 13:34:26 -0400

    Add script to remove stale volumes
    Stale volumes are volumes that are in "Available" state and
    have been around for longer than 15 minutes long.
    
    Change-Id: Ic3d9546609d6037afbce26d822b81ed8e8b46ec3
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jjb/odl-openstack-cleanup-stale-volumes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Scans OpenStack for orphaned volumes
+
+# shellcheck source=/tmp/v/openstack/bin/activate disable=SC1091
+source "/tmp/v/openstack/bin/activate"
+mapfile -t os_volumes < <(openstack volume list -f value -c ID --status Available)
+deactivate
+
+# shellcheck source=/tmp/v/lftools/bin/activate disable=SC1091
+source "/tmp/v/lftools/bin/activate"
+echo "---> Orphaned volumes"
+if [ ${#os_volumes[@]} -eq 0 ]; then
+    echo "No orphaned volumes found."
+else
+    for volume in "${os_volumes[@]}"; do
+        echo "Removing volume $volume"
+        lftools openstack --os-cloud vex volume remove --minutes 15 "$volume"
+    done
+fi
+deactivate
