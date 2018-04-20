b5efcedc846a62237a388366303e59719e9895c9
Luis Gomez <ecelgp@gmail.com>
2018-04-19 21:38:45 -0700

    Add verification for UM distribution
    Change-Id: I64668c79d834aa154666005ec2862d7a0f164636
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- jjb/integration/distribution/distribution-check-bootup.sh
+++ jjb/integration/distribution/distribution-check-bootup.sh
@@ -1,52 +1,55 @@
-
 CONTROLLERMEM="3072m"
 ACTUALFEATURES="odl-integration-all"
-BUNDLEVERSION="$(xpath distribution/pom.xml '/project/version/text()' 2> /dev/null)"
-BUNDLEFOLDER="${KARAF_ARTIFACT}-${BUNDLEVERSION}"
-BUNDLE="${BUNDLEFOLDER}.zip"
-BUNDLE_PATH="/tmp/r/org/opendaylight/integration/${KARAF_ARTIFACT}/${BUNDLEVERSION}/${BUNDLE}"
+
+if [[ ! -z "${CONTROLLERFEATURES}" ]]; then
+    ACTUALFEATURES="odl-integration-all,${CONTROLLERFEATURES}"
+fi
 
 echo "Kill any controller running"
 ps axf | grep karaf | grep -v grep | awk '{print "kill -9 " $1}' | sh
 
 echo "Clean Existing distribution"
-rm -rf ${BUNDLEFOLDER}
+rm -rf ${BUNDLE_FOLDER}
 
-echo "Copying the distribution..."
+echo "Fetch the distribution..."
+if  [[ -z "${BUNDLE_PATH}" ]]; then
+    wget --progress=dot:mega  "${ACTUAL_BUNDLE_URL}"
+else
 cp "${BUNDLE_PATH}" .
+fi
 
 echo "Extracting the new controller..."
 unzip -q "${BUNDLE}"
 
 echo "Configuring the startup features..."
-FEATURESCONF="${WORKSPACE}/${BUNDLEFOLDER}/etc/org.apache.karaf.features.cfg"
-FEATURE_TEST_STRING="features-integration-test"
-if [[ "$KARAF_VERSION" == "karaf4" ]]; then
-    FEATURE_TEST_STRING="features-test"
+FEATURESCONF="${WORKSPACE}/${BUNDLE_FOLDER}/etc/org.apache.karaf.features.cfg"
+FEATURE_TEST_STRING="features-test"
+if [[ "$KARAF_VERSION" == "karaf3" ]]; then
+    FEATURE_TEST_STRING="features-integration-test"
 fi
 
-sed -ie "s%\(featuresRepositories=\|featuresRepositories =\)%featuresRepositories = mvn:org.opendaylight.integration/${FEATURE_TEST_STRING}/${BUNDLEVERSION}/xml/features,%g" ${FEATURESCONF}
+sed -ie "s%\(featuresRepositories=\|featuresRepositories =\)%featuresRepositories = mvn:org.opendaylight.integration/${FEATURE_TEST_STRING}/${BUNDLE_VERSION}/xml/features,%g" ${FEATURESCONF}
+
+if [[ ! -z "${REPO_URL}" ]]; then
+   sed -ie "s%featuresRepositories =%featuresRepositories = ${REPO_URL},%g" ${FEATURESCONF}
+fi
 
 # Add actual boot features.
 sed -ie "s/\(featuresBoot=\|featuresBoot =\)/featuresBoot = ${ACTUALFEATURES},/g" "${FEATURESCONF}"
 cat "${FEATURESCONF}"
 
 echo "Configuring the log..."
-LOGCONF="${WORKSPACE}/${BUNDLEFOLDER}/etc/org.ops4j.pax.logging.cfg"
+LOGCONF="${WORKSPACE}/${BUNDLE_FOLDER}/etc/org.ops4j.pax.logging.cfg"
 sed -ie 's/log4j.appender.out.maxFileSize=1MB/log4j.appender.out.maxFileSize=20MB/g' "${LOGCONF}"
 cat "${LOGCONF}"
 
 echo "Configure max memory..."
-MEMCONF="${WORKSPACE}/${BUNDLEFOLDER}/bin/setenv"
+MEMCONF="${WORKSPACE}/${BUNDLE_FOLDER}/bin/setenv"
 sed -ie "s/2048m/${CONTROLLERMEM}/g" "${MEMCONF}"
 cat "${MEMCONF}"
 
 echo "Listing all open ports on controller system"
 netstat -pnatu
-
-echo "redirected karaf console output to karaf_console.log"
-export KARAF_REDIRECT="${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf_console.log"
-mkdir -p ${WORKSPACE}/${BUNDLEFOLDER}/data/log
 
 if [ "${JDKVERSION}" == 'openjdk8' ]; then
     echo "Setting the JRE Version to 8"
@@ -64,22 +67,19 @@ echo "JDK Version should be overriden by JAVA_HOME"
 java -version
 
 echo "Redirecting karaf console output to karaf_console.log"
-export KARAF_REDIRECT="${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf_console.log"
-mkdir -p ${WORKSPACE}/${BUNDLEFOLDER}/data/log
+export KARAF_REDIRECT="${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf_console.log"
+mkdir -p ${WORKSPACE}/${BUNDLE_FOLDER}/data/log
 
 echo "Starting controller..."
-${WORKSPACE}/${BUNDLEFOLDER}/bin/start
-
-# No need for verbose printing during repeating operations.
-set +x
+${WORKSPACE}/${BUNDLE_FOLDER}/bin/start
 
 function dump_log_and_exit {
     echo "Dumping first 500K bytes of karaf log..."
-    head --bytes=500K "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf.log"
+    head --bytes=500K "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf.log"
     echo "Dumping last 500K bytes of karaf log..."
-    tail --bytes=500K "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf.log"
-    cp "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf.log" .
-    cp "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf_console.log" .
+    tail --bytes=500K "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf.log"
+    cp "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf.log" .
+    cp "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf_console.log" .
     exit 1
 }
 
@@ -107,7 +107,7 @@ else
         COUNT=$(( ${COUNT} + 5 ))
         sleep 5
         echo "already waited ${COUNT} seconds..."
-        if grep --quiet 'org.opendaylight.infrautils.ready-impl.*System ready' "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf.log"; then
+        if grep --quiet 'org.opendaylight.infrautils.ready-impl.*System ready' "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf.log"; then
             echo "Controller is UP"
             break
         elif (( "${COUNT}" > "300" )); then
@@ -117,42 +117,41 @@ else
     done
 fi
 
-set -x
-
 # echo "Checking OSGi bundles..."
 # sshpass seems to fail with new karaf version
-# sshpass -p karaf ${WORKSPACE}/${BUNDLEFOLDER}/bin/client -u karaf 'bundle:list'
+# sshpass -p karaf ${WORKSPACE}/${BUNDLE_FOLDER}/bin/client -u karaf 'bundle:list'
 
 echo "Listing all open ports on controller system"
 netstat -pnatu
 
 function exit_on_log_file_message {
     echo "looking for \"$1\" in karaf.log file"
-    if grep --quiet "$1" "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf.log"; then
+    if grep --quiet "$1" "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf.log"; then
         echo ABORTING: found "$1"
         dump_log_and_exit
     fi
 
     echo "looking for \"$1\" in karaf_console.log file"
-    if grep --quiet "$1" "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf_console.log"; then
+    if grep --quiet "$1" "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf_console.log"; then
         echo ABORTING: found "$1"
         dump_log_and_exit
     fi
 }
 
+exit_on_log_file_message 'Error installing boot feature repository'
 exit_on_log_file_message 'BindException: Address already in use'
 exit_on_log_file_message 'server is unhealthy'
 
 echo "Fetching Karaf logs"
 # TODO: Move instead of copy? Gzip?
-cp "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf.log" .
-cp "${WORKSPACE}/${BUNDLEFOLDER}/data/log/karaf_console.log" .
+cp "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf.log" .
+cp "${WORKSPACE}/${BUNDLE_FOLDER}/data/log/karaf_console.log" .
 
 echo "Kill controller"
 ps axf | grep karaf | grep -v grep | awk '{print "kill -9 " $1}' | sh
 
 echo "Bug 4628: Detecting misplaced config files"
-pushd "${WORKSPACE}/${BUNDLEFOLDER}" || exit
+pushd "${WORKSPACE}/${BUNDLE_FOLDER}" || exit
 XMLS_FOUND="$(echo *.xml)"
 popd || exit
 if [ "$XMLS_FOUND" != "*.xml" ]; then
