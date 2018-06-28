f494036d5076533c8fb5b68f635ab8f3a7bb14ae
Taseer Ahmed <taseer94@gmail.com>
2018-06-28 13:21:15 +0500

    Job for testing the ODL user configuration
    This introduces a script pointing to the test
    playbook, and a new job to execute the script
    
    Change-Id: Ib950fbb1f8952c794380af03c25f9f4987c65b73
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ jjb/packaging/test-ansible-odl-user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+# Wait for any background apt processes to finish
+# There seems to be a backgroud apt process that locks /var/lib/dpkg/lock
+# and causes our apt commands to fail.
+while pgrep apt > /dev/null; do sleep 1; done
+
+# Install Ansible
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+# Install OpenDaylight via repo using example Ansible playbook
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/tests/test-odl-users.yml -vvv
