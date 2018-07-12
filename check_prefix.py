94012264ecfc4e947056f61554f9b21a9ccf5d47
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-07-12 19:14:18 -0400

    Convert prefix script to Python
    Bash shell scripts do not work on Windows systems out of the box.
    Convert the script to Python so that we are a little more
    cross-platform compatible.
    
    Change-Id: I7654851622462588da4599d05406fd3bdae7ba41
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ check_prefix.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Ensures that the prefix MUST be set to blank.
+
+The production prefix MUST always be a blank string.
+"""
+import os
+import re
+import sys
+
+
+def check_prefix(filename):
+    with open(filename, 'r') as _file:
+        for num, line in enumerate(_file, 1):
+            if re.search('prefix:', line):
+                if "''" not in line:
+                    print(
+                        'ERROR: A non-blank prefix is defined in '
+                        'jjb/defaults.yaml. The prefix MUST be set to blank '
+                        '\'\' in production!'
+                    )
+                    sys.exit(1)
+
+
+if __name__ == "__main__":
+    check_prefix(os.path.join('jjb', 'defaults.yaml'))
