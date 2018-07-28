e4ef1b36e5ff8872d17217b11a03ac45fa68d6c9
Taseer Ahmed <taseer94@gmail.com>
2018-07-28 20:22:32 +0500

    Add test jobs for Debian
    This patch introduces testing jobs for the deb env. Follows the
    approach for setting up RPM jobs. One script contains the setup
    logic and the testing parts form a script of their own.
    
    Change-Id: I686f920b9e0ca432d52b2a7a533fb3fb992978dc
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- jjb/packaging/test-ansible-deb.sh
+++ jjb/packaging/test-ansible-deb.sh
@@ -1,35 +1,4 @@
 #!/bin/bash
 
-# Options:
-#   -x: Echo commands
-#   -e: Fail on errors
-#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
-set -ex -o pipefail
-
-# Install required packages
-virtualenv deb_build
-source deb_build/bin/activate
-PYTHON="deb_build/bin/python"
-$PYTHON -m pip install --upgrade pip
-
-# Wait for any background apt processes to finish
-# There seems to be a backgroud apt process that locks /var/lib/dpkg/lock
-# and causes our apt commands to fail.
-while pgrep apt > /dev/null; do sleep 1; done
-
-# Install Ansible
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
-
-# Install local version of ansible-opendaylight to path expected by Ansible.
-# Could almost do this by setting ANSIBLE_ROLES_PATH=$WORKSPACE, but Ansible
-# expects the dir containing the role to have the name of role. The JJB project
-# is called "ansible", which causes the cloned repo name to not match the role
-# name "opendaylight". So we need a cp/mv either way and this is simplest.
-sudo cp -R $WORKSPACE/ansible /etc/ansible/roles/opendaylight
-
 # Install OpenDaylight via repo using example Ansible playbook
-sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/examples/deb_repo.yml
-
-# Add more tests
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/examples/deb_repo_api.yml
