66cc06d0f4e2567c8f3053292a97d9a9567e81a4
Jamo Luhrsen <jluhrsen@redhat.com>
2018-08-02 11:07:14 -0700

    Use only pip3 for robot node package installs
    - removed the install of importlib as it should be
      now builtin with python3
    
    Change-Id: Iaa1b1a73db1a72cb0e6660816850294cd4e04c4e
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/integration-install-robotframework.sh
+++ jjb/integration/integration-install-robotframework.sh
@@ -9,6 +9,13 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
+#Module for elasticsearch python client
+pip3 install --upgrade pip
+pip3 install --user https://files.pythonhosted.org/packages/63/cb/6965947c13a94236f6d4b8223e21beb4d576dc72e8130bd7880f600839b8/urllib3-1.22-py2.py3-none-any.whl
+pip3 install --user https://files.pythonhosted.org/packages/b8/f7/3bb4d18c234a8ce7044d5ee2e1082b7d72bf6c550afb8d51ae266dea56f1/requests-2.9.1-py2.py3-none-any.whl
+pip3 install --user https://files.pythonhosted.org/packages/c3/e3/146b675e6d0138a49c4b817b4e68170eb9b75cee7e71fa3ec69624c4f467/elasticsearch-6.2.0-py2.py3-none-any.whl
+pip3 install --user https://files.pythonhosted.org/packages/75/5e/b84feba55e20f8da46ead76f14a3943c8cb722d40360702b2365b91dec00/PyYAML-3.11.tar.gz
+
 ROBOT_VENV="/tmp/v/robot"
 echo ROBOT_VENV="${ROBOT_VENV}" >> "${WORKSPACE}/env.properties"
 
@@ -16,6 +23,7 @@ echo ROBOT_VENV="${ROBOT_VENV}" >> "${WORKSPACE}/env.properties"
 # installed packages. This allows us to bake matplotlib which takes very long
 # to install into the image.
 virtualenv --system-site-packages "${ROBOT_VENV}"
+
 # shellcheck disable=SC1090
 source "${ROBOT_VENV}/bin/activate"
 
@@ -23,9 +31,8 @@ set -exu
 
 # Make sure pip itself us up-to-date.
 pip install --upgrade pip
-pip3 install --upgrade pip
 
-pip install --upgrade docker-py importlib requests scapy netifaces netaddr ipaddr pyhocon
+pip install --upgrade docker-py requests scapy netifaces netaddr ipaddr pyhocon
 pip install --upgrade robotframework-httplibrary \
     requests==2.15.1 \
     robotframework-requests \
@@ -55,13 +62,6 @@ pip install --upgrade jmespath
 
 # Module for backup-restore support library
 pip install --upgrade jsonpatch
-
-#Module for elasticsearch python client
-#Module for elasticsearch python client
-pip3 install --user https://files.pythonhosted.org/packages/63/cb/6965947c13a94236f6d4b8223e21beb4d576dc72e8130bd7880f600839b8/urllib3-1.22-py2.py3-none-any.whl
-pip3 install --user https://files.pythonhosted.org/packages/b8/f7/3bb4d18c234a8ce7044d5ee2e1082b7d72bf6c550afb8d51ae266dea56f1/requests-2.9.1-py2.py3-none-any.whl
-pip3 install --user https://files.pythonhosted.org/packages/c3/e3/146b675e6d0138a49c4b817b4e68170eb9b75cee7e71fa3ec69624c4f467/elasticsearch-6.2.0-py2.py3-none-any.whl
-pip3 install --user https://files.pythonhosted.org/packages/75/5e/b84feba55e20f8da46ead76f14a3943c8cb722d40360702b2365b91dec00/PyYAML-3.11.tar.gz
 
 # Print installed versions.
 pip install --upgrade pipdeptree
