bc953db68c6f526a52f3fdf5348a5211bbd40e0f
Sam Hague <shague@redhat.com>
2018-08-03 18:33:31 -0400

    Add job for reports
    Change-Id: Iaf953243c6e55c9f707f43ac46dab0afcc58846f
    Signed-off-by: Sam Hague <shague@redhat.com>

---
+++ jjb/netvirt/run-reports.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 Red Hat, Inc. and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+echo "Executing run-reports.sh"
+set -e -o pipefail # Fail on errors, give stacktrace
+set -x # Enable trace
+
+virtualenv --quiet "/tmp/v/odltools"
+source /tmp/v/odltools/bin/activate
+pip install odltools
+mkdir $REPORT_PATH
+python -m odltools csit reports --numjobs $NUM_JOBS --path $REPORT_PATH --url $LOG_URL --jobnames $JOB_NAMES
+python -m odltools csit exceptions --numjobs $NUM_JOBS --path $REPORT_PATH --url $LOG_URL --jobnames $JOB_NAMES
+mkdir -p $WORKSPACE/archives
+cp $REPORT_PATH/*.txt $WORKSPACE/archives
+exit 0
