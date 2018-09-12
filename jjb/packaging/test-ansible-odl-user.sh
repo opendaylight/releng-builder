d87e8d5caa9e15ac65d68da0513dfe46d97d1258
Taseer <taseer94@gmail.com>
2018-09-12 16:24:22 +0200

    Enhance the RPM CI scripts
    This patch removes the common parts with in the CI scripts.
    
    Change-Id: I3883d1307cc044d672e18eb7b41ff9047b728108
    Signed-off-by: Taseer <taseer94@gmail.com>

--- jjb/packaging/test-ansible-odl-user.sh
+++ jjb/packaging/test-ansible-odl-user.sh
@@ -6,29 +6,11 @@
 #   -o pipefail: Fail on errors in scripts this calls, give stacktrace
 set -ex -o pipefail
 
-# Install required packages
-virtualenv rpm_build
-source rpm_build/bin/activate
-rpm_build/bin/python -m pip install --upgrade pip
-
-# Install Ansible
-sudo yum install -y ansible
-
-# Install local version of ansible-opendaylight to path expected by Ansible.
-# Could almost do this by setting ANSIBLE_ROLES_PATH=$WORKSPACE, but Ansible
-# expects the dir containing the role to have the name of role. The JJB project
-# is called "ansible", which causes the cloned repo name to not match the role
-# name "opendaylight". So we need a cp/mv either way and this is simplest.
-sudo cp -R $WORKSPACE/ansible /etc/ansible/roles/opendaylight
-
-# Install OpenDaylight via repo using example Ansible playbook
-sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/examples/rpm_8_devel.yml
-
 # Create Ansible custom module directories
 sudo mkdir -p /usr/share/ansible/plugins/modules
 
 # Copy the custom module to the directory above
 sudo cp $WORKSPACE/ansible/library/odl_usermod.py /usr/share/ansible/plugins/modules/
 
-# Execute the tests playnook
+# Execute the odl-user-test playbook
 sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/tests/test-odl-users.yaml -vvv
