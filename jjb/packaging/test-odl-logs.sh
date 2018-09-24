73a31fa8016a9d2bb30e7291b45d247130563e68
Taseer <taseer94@gmail.com>
2018-09-24 13:13:20 +0200

    Remove duplicated parts from Ansible-ODL CI script
    This patch moves the common parts of the Ansible-ODL CI scripts
    into one script and adds each of the testing parts as separate
    builder scripts.
    
    JIRA: INTPAK-197
    
    Change-Id: Ic9e887d3c52d1cb78b523b9e1de3f6430f59a18f
    Signed-off-by: Taseer <taseer94@gmail.com>

--- jjb/packaging/test-odl-logs.sh
+++ jjb/packaging/test-odl-logs.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Options:
-#   -x: Echo commands
-#   -e: Fail on errors
-#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
-set -ex -o pipefail
-
 # Execute the test ODL logs playbook
 sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/tests/test-odl-logs.yaml -v
