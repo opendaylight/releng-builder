cb6307755bc9c50a44cb9dd289f1efe695477a46
Taseer <taseer94@gmail.com>
2018-09-24 13:13:20 +0200

    Remove duplicating parts from CI scripts
    This patch moves the common parts of the CI scripts into one script
    and adds each of the testing parts as separate builder scripts.
    
    JIRA: INTPAK-197
    
    Change-Id: Ic9e887d3c52d1cb78b523b9e1de3f6430f59a18f
    Signed-off-by: Taseer <taseer94@gmail.com>

---
+++ jjb/packaging/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
+# Install required packages
+virtualenv rpm_build
+source rpm_build/bin/activate
+PYTHON="rpm_build/bin/python"
+$PYTHON -m pip install --upgrade pip
+
+# Install Ansible
+sudo yum install -y ansible
+
+# Install local version of ansible-opendaylight to path expected by Ansible.
+# Could almost do this by setting ANSIBLE_ROLES_PATH=$WORKSPACE, but Ansible
+# expects the dir containing the role to have the name of role. The JJB project
+# is called "ansible", which causes the cloned repo name to not match the role
+# name "opendaylight". So we need a cp/mv either way and this is simplest.
+sudo cp -R $WORKSPACE/ansible /etc/ansible/roles/opendaylight
