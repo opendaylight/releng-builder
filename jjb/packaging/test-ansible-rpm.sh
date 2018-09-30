4c3b2a5262eebc78e9731ddf5779450a6cf3a4a7
Taseer <taseer94@gmail.com>
2018-09-30 16:21:54 +0200

    Change test script name
    RPM and Debian have their separate setup logic. This patch changes
    the name from 'setup-ansible.sh' to 'setup-ansible-rpm.sh' to
    signify the relevancy to the system being tested, i.e RPM or Deb.
    
    Change-Id: I1f9c846083ac92aca4b9cb13a56e16ec8b1e74f3
    Signed-off-by: Taseer <taseer94@gmail.com>

--- jjb/packaging/test-ansible-rpm.sh
+++ jjb/packaging/test-ansible-rpm.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Options:
+#   -x: Echo commands
+#   -e: Fail on errors
+#   -o pipefail: Fail on errors in scripts this calls, give stacktrace
+set -ex -o pipefail
+
 # Install OpenDaylight via repo using example Ansible playbook
 sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/examples/rpm_8_devel_odl_api.yml --extra-vars "@$WORKSPACE/ansible/examples/log_vars.json"
