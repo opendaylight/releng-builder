ebb2e4e8e9a7d9d5432415edce2001f01adf2419
Taseer <taseer94@gmail.com>
2018-10-10 22:07:16 +0200

    Increase test coverage
    Change-Id: Ic877f2453a843be630dddccbe05871d9ebb86bab
    Signed-off-by: Taseer <taseer94@gmail.com>

---
+++ jjb/packaging/test-ansible-rpm-custom-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install OpenDaylight with custom config via repo using example Ansible playbook
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/examples/rpm_8_devel_odl_api.yml --extra-vars "@$WORKSPACE/ansible/examples/log_vars.json"
+
+# Create Ansible custom module directories
+sudo mkdir -p /usr/share/ansible/plugins/modules
+
+# Copy the custom module to the directory above
+sudo cp $WORKSPACE/ansible/library/odl_usermod.py /usr/share/ansible/plugins/modules/
+
+# Execute the odl-user-test playbook
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/tests/test-odl-users.yaml -v
+
+# Test the custom log configurations
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/tests/test-odl-logs.yaml -v
