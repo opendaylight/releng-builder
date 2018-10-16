334f6a29b6cc452df4d710b9908ef711e57a740b
Taseer <taseer94@gmail.com>
2018-10-16 11:42:02 +0200

    Increase test coverage for DEB env
    Following the RPM approach, enhance test coverage by testing
    against default and custom configuration in DEB builds.
    
    Change-Id: I08c25cd320b991fc323501916064c90206326ddb
    Signed-off-by: Taseer <taseer94@gmail.com>

---
+++ jjb/packaging/test-ansible-deb-custom-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install OpenDaylight via repo using example Ansible playbook
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/examples/deb_repo_api.yml --extra-vars "@$WORKSPACE/ansible/examples/log_vars.json"
+
+# Execute the odl-user-test playbook
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/tests/test-odl-users.yaml -v
+
+# Test the custom log configurations
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/tests/test-odl-logs.yaml -e test_log_level=INFO -e test_log_mechanism=file -v
