41884a319c2451abc38408b96a1faeacc84c771b
Taseer <taseer94@gmail.com>
2018-10-24 14:02:02 +0200

    Add upgrade tests job
    Modify he upgrade script to test and assert installation of
    different versions of ODL.
    
    JIRA: INTPAK-29
    Change-Id: I09594e0c705bfc5456271c9d976c058ab860f8cc
    Signed-off-by: Taseer <taseer94@gmail.com>

---
+++ jjb/packaging/test-ansible-rpm-upgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Uninstall ODL from previous test
+# This is to ensure these tests run independant
+# from any other tests
+sudo yum remove opendaylight -y
+
+# Remove the ODL directory
+sudo rm -rf /opt/opendaylight
+
+# Execute the upgrade tests
+sudo ansible-playbook -i "localhost," -c local $WORKSPACE/ansible/tests/test-odl-upgrade.yaml -v
