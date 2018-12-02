382ba9f3f8d24c504fbcde1fcd5db14553f56283
Taseer <taseer94@gmail.com>
2018-12-02 18:14:12 +0100

    Add cluster config job
    Change-Id: I590f4d646cb55fb8d560205532f966759bba0ef6
    Signed-off-by: Taseer <taseer94@gmail.com>

---
+++ jjb/packaging/test-ansible-cluster-rpm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install the dependencies for RHEL
+sudo yum -y install docker pip
+
+# install docker-py
+sudo pip install molecule docker-py
+
+# Change directory to ODL role
+cd /etc/ansible/roles/opendaylight || return
+
+# Initialize the scenario
+molecule init scenario --role-name opendaylight --driver-name docker --verifier-name testinfra
+
+# Create the containers
+molecule create
+
+# Configure the containers
+molecule converge
