b578907a926959ed8f4761b4e74e073ecfb4694d
Daniel Farrell <dfarrell@redhat.com>
2018-12-14 12:01:18 -0500

    Conform to new RVM install GPG key requirements
    Puppet-opendaylight tests are breaking on RVM install with GPG key
    errors. It seems something has changed on their end.
    
    Add second seeminly-required public key.
    
    Change-Id: I237a5923ae178f2e4717b6814984bda522ddd90b
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- jjb/packaging/test-puppet.sh
+++ jjb/packaging/test-puppet.sh
@@ -15,7 +15,9 @@ sudo yum install -y ruby-devel gcc-c++ zlib-devel patch redhat-rpm-config make r
 
 # Install RVM to help build recent version of Ruby
 # The ruby_dep gem requires >=2.2.5, 2.0.0 is the latest pre-packaged for CentOS
-gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys \
+  409B6B1796C275462A1703113804BB82D39DC0E3 \
+  7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -L get.rvm.io | bash -s stable
 # Expected by RVM, seems required to make RVM functions (`rvm use`) available
 # Silence absurdly verbose rvm output by temporally not echoing commands
