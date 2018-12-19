6e308d434937c47fdc3a607c99ca6d7c7155d7a3
Jamo Luhrsen <jluhrsen@redhat.com>
2018-12-19 14:53:37 -0800

    Add tweakability features for ODL in apex job
    apex gets deployed with puppet and is already in
    place, so need to write out the features to install
    in that hieradata json file
    
    Change-Id: I1293777835b5c78e09647107de0fd8ed03b47f98
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/integration/integration-apex-run-tests.sh
+++ jjb/integration/integration-apex-run-tests.sh
@@ -13,6 +13,8 @@ print_job_parameters
 get_os_deploy
 
 configure_karaf_log_for_apex $OPENSTACK_CONTROL_NODE_1_IP
+get_features
+configure_odl_features_for_apex $OPENSTACK_CONTROL_NODE_1_IP
 
 # Swap out the ODL distribution
 DISTRO_UNDER_TEST=/tmp/odl.tar.gz
