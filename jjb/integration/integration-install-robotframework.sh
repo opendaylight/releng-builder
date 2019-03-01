9816b7fa0807faa428731a52ba4dcbc235d37726
Luis Gomez <ecelgp@gmail.com>
2019-03-01 13:23:06 -0800

    Remove not used python dependencies
    IOTDM project is not active anymore
    
    Change-Id: Ie78f885985916c27763646320df57eea3f2f9646
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- jjb/integration/integration-install-robotframework.sh
+++ jjb/integration/integration-install-robotframework.sh
@@ -45,11 +45,6 @@ pip install --upgrade pyangbind
 # Module for iso8601 datetime format
 pip install --upgrade isodate
 
-# Modules for tornado and jsonpointer used by client libraries of IoTDM project
-# Note: Could be removed when client running on tools VM is used instead
-#       of client libraries only.
-pip install --upgrade tornado jsonpointer
-
 # Module for TemplatedRequests.robot library
 pip install --upgrade jmespath
 
