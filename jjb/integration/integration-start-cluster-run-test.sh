aaadf1065baebc3c5fad630af87f78ba9bcba95c
Thanh Ha <zxiiro@gmail.com>
2019-08-12 12:13:43 -0400

    Fix TESTOPTIONS should not be quoted
    Change I43555ba960bf37f7cdd182be1574ae0249eb12d1 and
    I43555ba960bf37f7cdd182be1574ae0249eb12d1 introduced this regression
    trying to resolve shellcheck
    
    Change-Id: Ife7313a2a01391bcb436c0e9906b8d5230c3ea90
    Signed-off-by: Thanh Ha <zxiiro@gmail.com>

--- jjb/integration/integration-start-cluster-run-test.sh
+++ jjb/integration/integration-start-cluster-run-test.sh
@@ -111,6 +111,7 @@ done
 get_test_suites SUITES
 
 echo "Starting Robot test suites ${SUITES} ..."
+# ${TESTOPTIONS}, ${SUITES} are space-separated parameters and should not be quoted.
 # shellcheck disable=SC2086
 robot -N "${TESTPLAN}" \
       --removekeywords wuks -c critical -e exclude -e "skip_if_${DISTROSTREAM}" \
@@ -137,7 +138,7 @@ robot -N "${TESTPLAN}" \
       -v TOOLS_SYSTEM_USER:"${USER}" \
       -v USER_HOME:"${HOME}" \
       -v WORKSPACE:/tmp \
-      "${TESTOPTIONS}" ${SUITES} || true
+      ${TESTOPTIONS} ${SUITES} || true
 
 
 
