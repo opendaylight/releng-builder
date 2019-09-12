841896af5ce8e44b60a8a6085ef6bde6c87a6dc3
Tom Flynn <tom.flynn@gmail.com>
2019-09-12 13:50:23 -0700

    Upgrade rapidjson dep for opflex 1.0.2 to 1.1.0
    lftools issues seen in jenkins jobs have now been resolved
    
    Signed-off-by: Tom Flynn <tom.flynn@gmail.com>
    Change-Id: Ic8d190784f0bd397fce43a9fcefb5ef601cbe421

--- jjb/opflex/scripts/rapidjson-build.sh
+++ jjb/opflex/scripts/rapidjson-build.sh
@@ -24,7 +24,7 @@ mkdir -p "$DESTDIR/$ROOT/include"
 cp -R include/rapidjson "$DESTDIR/$ROOT/include"
 sed -e "s|@INCLUDE_INSTALL_DIR@|$ROOT/include|" \
     -e "s|@PROJECT_NAME@|RapidJSON|" \
-    -e "s|@LIB_VERSION_STRING@|1.0.2|" RapidJSON.pc.in > \
+    -e "s|@LIB_VERSION_STRING@|1.1.0|" RapidJSON.pc.in > \
     "$DESTDIR/$ROOT/lib/pkgconfig/RapidJSON.pc"
 
 pushd $DESTDIR
