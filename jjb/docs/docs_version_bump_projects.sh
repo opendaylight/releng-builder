5d71edc9d00eb71d45fda15f5ccdd19541d92ad6
Anushka Bhandari <anushka16134@iiitd.ac.in>
2019-12-02 20:09:35 +0530

    Job to update docs configuration in docs and other odl projects
    These JJB Scripts completely automates the changes in config files
    of docs and other odl-projects.
    
    Change-Id: Ia056f3f11b83b527a650979989000f4feda10679
    Signed-off-by: Anushka Bhandari <anushka16134@iiitd.ac.in>

---
+++ jjb/docs/docs_version_bump_projects.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+update_file_usage () {
+    echo "Usage: $0 <release_name> <publish>"
+    echo ""
+    echo "    release_name:  The release_name e.g Magnesium ."
+    echo "    publish:  Set to true to publish"
+    echo ""
+}
+while getopts :h: opts; do
+  case "$opts" in
+    h)
+        update_file_usage
+        exit 0
+        ;;
+    [?])
+        update_file_usage
+        exit 1
+        ;;
+  esac
+done
+if [ "$GERRIT_PROJECT" == "releng/autorelease" ]; then
+    # User input
+    echo $0
+    echo "BBBBBBBB"
+
+    release_name=$RELEASE_NAME
+    # Captilize Version Name
+    release_name="$(tr '[:lower:]' '[:upper:]' <<< "${release_name:0:1}")${release_name:1}"
+    branch_name=$STREAM
+    publish=$PUBLISH
+
+    ################
+    # Start script #
+    ###############
+    git submodule update
+
+    #'|| true' for repo like serviceutils where docs/conf.yaml doesn't exist
+    command='sed -i ''"s/.*version.*/version: '"$release_name"'/"'' docs/conf.yaml || true'
+    git submodule foreach "git checkout $branch_name"
+    git submodule foreach "$command"
+    if [ "$publish" == "true" ]
+      then
+        git submodule foreach "git add . || true"
+        git submodule foreach "git commit -s -m 'Update docs header to $release_name in $branch_name
+
+    Should be $release_name on $branch_name.' || true"
+        git submodule foreach "git review || true"
+    fi
+fi
