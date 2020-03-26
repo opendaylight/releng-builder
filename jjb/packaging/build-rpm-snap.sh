74d14c7b7904922061e1dafe7482438521aae6f7
Anil Belur <abelur@linuxfoundation.org>
2020-03-26 12:32:51 +1000

    Remove all Neon jobs
    Neon is EOL-ed with Magnesium being released.
    
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>
    Change-Id: I7f32e9da2a2a4b602c9e3c5d0219625365dce1fa
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

--- jjb/packaging/build-rpm-snap.sh
+++ jjb/packaging/build-rpm-snap.sh
@@ -15,9 +15,7 @@ $PYTHON -m pip install --upgrade pip
 $PYTHON -m pip install -r "$WORKSPACE/packaging/packages/requirements.txt"
 
 # Convert stream to numeric ODL major version
-if [ "$STREAM" == "neon" ]; then
-  VERSION_MAJOR=10
-elif [ "$STREAM" == "sodium" ]; then
+if [ "$STREAM" == "sodium" ]; then
   VERSION_MAJOR=11
 elif [ "$STREAM" == "magnesium" ]; then
   VERSION_MAJOR=12
