c1b477d68d25149687e6d3f6c76a00101dc11532
Venkatrangan Govindarajan <vgovindarajan@luminanetworks.com>
2020-06-19 21:43:26 +0530

    Fix the robotframework install fo CSIT jobs
    JIRA: INTTEST-103
    Signed-off-by: Venkatrangan Govindarajan <vgovindarajan@luminanetworks.com>
    Change-Id: Ie6ac9c0a987673a67a6c6adfd01bbef1ca3ab97f

--- jjb/integration/integration-install-robotframework.sh
+++ jjb/integration/integration-install-robotframework.sh
@@ -15,7 +15,7 @@ echo ROBOT_VENV="${ROBOT_VENV}" >> "${WORKSPACE}/env.properties"
 # The --system-site-packages parameter allows us to pick up system level
 # installed packages. This allows us to bake matplotlib which takes very long
 # to install into the image.
-virtualenv --system-site-packages "${ROBOT_VENV}"
+virtualenv -p python3 --system-site-packages "${ROBOT_VENV}"
 # shellcheck disable=SC1090
 source "${ROBOT_VENV}/bin/activate"
 
@@ -30,8 +30,7 @@ pip install --upgrade robotframework-httplibrary \
     requests==2.15.1 \
     robotframework-requests \
     robotframework-sshlibrary==3.1.1 \
-    robotframework-selenium2library \
-    robotframework-pycurllibrary
+    robotframework-selenium2library
 
 # Module jsonpath is needed by current AAA idmlite suite.
 pip install --upgrade jsonpath-rw
@@ -53,10 +52,10 @@ pip install --upgrade jsonpatch
 
 #Module for elasticsearch python client
 #Module for elasticsearch python client
-python3 -m pip install --user urllib3==1.22
-python3 -m pip install --user requests==2.9.1
-python3 -m pip install --user elasticsearch==6.2.0
-python3 -m pip install --user PyYAML==3.11
+#pip install urllib3==1.22
+#pip install requests==2.9.1
+#pip install elasticsearch==6.2.0
+#pip install PyYAML==3.11
 
 # odltools for extra debugging
 pip install odltools
