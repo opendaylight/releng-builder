32e2d836e744077e3878000e136445d403584982
Thanh Ha <zxiiro@gmail.com>
2020-08-05 15:57:58 -0400

    Update merge view job filter
    Merge jobs are now in the form maven-merge since Magnesium so need
    to adjust the filter parameters for it to pick up the appropriate
    jobs.
    
    Change-Id: Iccdceb3d1f44f6c51829bb7268a211901a0a4c39
    Signed-off-by: Thanh Ha <zxiiro@gmail.com>

--- jjb/autorelease/update-autorelease-projects-views.sh
+++ jjb/autorelease/update-autorelease-projects-views.sh
@@ -55,7 +55,7 @@ EOF
 
 for module in "${modules[@]}"; do
     echo "Include project:$module to autorelease view"
-    echo "      - '$module-merge-${STREAM}'" >> "$VIEWS_AR_YAML_FILE"
+    echo "      - '$module-maven-merge-${STREAM}'" >> "$VIEWS_AR_YAML_FILE"
 done
 echo "    <<: *releng_autorelease_view_common_${STREAM}" >> "$VIEWS_AR_YAML_FILE"
 
