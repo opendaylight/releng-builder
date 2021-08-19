c64c869b4e3241c18541a5684a88dd337b58e396
Tomas Cere <tomas.cere@pantheon.tech>
2021-08-19 13:11:07 +0200

    Add mri-stage job
    This job stages a project and then triggers an mri
    distribution test job which runs csit on the staged
    artifacts from the staging repo.
    
    Add netconf stage/distribution job definitions
    Repoint netconf streams to master
    
    Change-Id: I473ef076e22df17bb5e0c0f7a9e7adcac0bd2822
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

---
+++ jjb/releng-maven-mri-stage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "---> releng-maven-mri-stage.sh"
+# This script publishes artifacts to a staging repo in Nexus and exports the karaf bundle URL.
+#
+# $WORKSPACE/m2repo   :  Exists and used to deploy the staging repository.
+# $NEXUS_URL          :  Jenkins global variable should be defined.
+# $STAGING_PROFILE_ID :  Provided by a job parameter.
+
+# Ensure we fail the job if any steps fail.
+set -xeu -o pipefail
+
+TMP_FILE="$(mktemp)"
+lftools deploy nexus-stage "$NEXUS_URL" "$STAGING_PROFILE_ID" "$WORKSPACE/m2repo" | tee "$TMP_FILE"
+staging_repo=$(sed -n -e 's/Staging repository \(.*\) created\./\1/p' "$TMP_FILE")
+
+# Store repo info to a file in archives
+mkdir -p "$WORKSPACE/archives"
+echo "$staging_repo ${NEXUS_URL}/content/repositories/$staging_repo" | tee -a "$WORKSPACE/archives/staging-repo.txt"
+
+staged_version=$(find . -name *karaf*.pom -exec xpath -q -e '/project/version/text()' {} +)
+BUNDLE_URL="${NEXUS_URL}/content/repositories/$staging_repo/org/opendaylight/${PROJECT}/${KARAF_ARTIFACT}/${staged_version}/${KARAF_ARTIFACT}-${staged_version}.zip"
+
+# Cleanup
+rm -f "$TMP_FILE"
+
+echo "Bundle url is ${BUNDLE_URL}"
+
+# Re-inject the new BUNDLE_URL for downstream jobs to pull from Nexus
+cat > "${WORKSPACE}/maven-staged-bundle.env" <<EOF
+BUNDLE_URL=${BUNDLE_URL}
+KARAF_VERSION=${KARAF_VERSION}
+EOF
