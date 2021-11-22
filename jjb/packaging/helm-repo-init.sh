1cecc4e3811a63700cd75fc5d2c724e326919466
Anil Belur <abelur@linuxfoundation.org>
2021-11-22 20:56:04 +1000

    CI: Add verify/merge jobs for ODL Helm charts
    Issue-ID: LF JIRA RELENG-3644
    Change-Id: I4390a1639dd18031f49dea55044ecda1a1f67464
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ jjb/packaging/helm-repo-init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Ensure we fail the job if any steps fail
+set -e -o pipefail
+
+mkdir -p ".chartstorage"
+
+chartmuseum --port=6464 --storage="local" --storage-local-rootdir=".chartstorage" &> /dev/null &
+source helm.prop
+$HELM_BIN plugin install --version v0.9.0 https://github.com/chartmuseum/helm-push.git || true
+$HELM_BIN repo add local http://localhost:6464
+$HELM_BIN repo add opendaylight http://localhost:6464
