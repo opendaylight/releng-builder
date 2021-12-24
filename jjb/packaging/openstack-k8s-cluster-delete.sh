6e69867b94ab7ffbb9b4593469b1ba24fe65faf4
Anil Belur <abelur@linuxfoundation.org>
2021-12-24 14:47:46 +1000

    Feat: Add Job template to deploy ODL on K8S Magnum
    The template allows users to create a K8S cluster on Openstack Magnum
    and deploy ODL SDNC on the cluster. Once deployed the Jenkins node
    verifies the deployment by listing the restconf modules.
    
    The JJB templates uses pre-existing K8s cluster templates provided
    by the cloud provider. (v1.16.0)
    
    The job templates are created in generic manner such that users can
    pass a script to test the SDNC setup from the project definition.
    
    The cluster is configured on a private network using internal IP's
    and the Jenkins executor node IP is added to the network to allow
    kubectl commands to be run directly the Jenkins instance.
    
    Issue-ID: https://jira.linuxfoundation.org/browse/RELENG-3644
    Change-Id: Ie17340da5677bb26b0cb0f673b90018b7033fa60
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ jjb/packaging/openstack-k8s-cluster-delete.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2021 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# shellcheck disable=SC2153,SC2034
+echo "---> Delete K8S cluster"
+
+set -eux -o pipefail
+# shellcheck disable=SC1090
+source ~/lf-env.sh
+
+os_cloud="${OS_CLOUD:-vex}"
+cluster_name="${CLUSTER_NAME}"
+
+cluster_delete_status="$(openstack --os-cloud "$OS_CLOUD" coe cluster delete "${CLUSTER_NAME}")"
+if [[ -z "$cluster_delete_status" ]]; then
+    echo "ERROR: Failed to delete coe cluster ${cluster_name}"
+    exit 1
+elif [[ "${cluster_delete_status}" =~ .*accepted.* ]]; then
+    echo "Cluster ${CLUSTER_NAME} delete in progress ..."
+    echo "${cluster_delete_status}"
+fi
