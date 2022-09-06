bda6b083a378cdf81a41242def91bb2b3b5485d6
Robert Varga <robert.varga@pantheon.tech>
2022-09-06 13:03:42 +0200

    Fixup: Use lf-activate-venv to install openstack
    When we are looking for spawned VMs, we also need to search for
    openstack in venv.
    
    Change-Id: I001dfda537fc143303d863b55420c8f717916e97
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- jjb/integration/integration-get-slave-addresses.sh
+++ jjb/integration/integration-get-slave-addresses.sh
@@ -12,6 +12,25 @@ lf-activate-venv --python python3 \
 
 set -x
 
+# shellcheck disable=SC1090
+. ~/lf-env.sh
+
+
+# Check if openstack venv was previously created
+if [ -f "/tmp/.os_lf_venv" ]; then
+    os_lf_venv=$(cat "/tmp/.os_lf_venv")
+fi
+
+if [ -d "${os_lf_venv}" ] && [ -f "${os_lf_venv}/bin/openstack" ]; then
+    echo "Re-use existing venv: ${os_lf_venv}"
+    PATH=$os_lf_venv/bin:$PATH
+else
+    lf-activate-venv --python python3 \
+        python-heatclient \
+        python-openstackclient \
+        yq
+fi
+
 ODL_SYSTEM=()
 TOOLS_SYSTEM=()
 OPENSTACK_SYSTEM=()
