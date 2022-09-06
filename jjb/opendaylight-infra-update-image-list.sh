e1332f383760ed7c2a8e7f4ea6d452b73b100db0
Anil Belur <abelur@linuxfoundation.org>
2022-09-06 17:23:12 +1000

    Revert "Fix: bashisms - step 2"
    This reverts commit 71ab2645c80c9c6c8c7f9eec9ca6e350dc1ee272.
    
    The original commits fails the openstack jobs with the following error:
    
    [builder-update-image-list] $ /bin/sh -l /tmp/jenkins9084563766671196003.sh
    /home/jenkins/lf-env.sh: line 44: `lf-echo-stderr': not a valid identifier
    
    Change-Id: I4d4d3ba08fe36f2b2e385bdfdd5918fa24b09e36
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/opendaylight-infra-update-image-list.sh
+++ jjb/opendaylight-infra-update-image-list.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash -l
 # SPDX-License-Identifier: EPL-1.0
 ##############################################################################
 # Copyright (c) 2017 The Linux Foundation and others.
@@ -32,13 +32,12 @@ Following are the list of published images available to Jenkins jobs.
 EOF
 # Blank line before EOF is on purpose to ensure there is spacing.
 
-IFS='
-'
+IFS=$'\n'
 # We purposely want globbing here to build images list
 # shellcheck disable=SC2207
-IMAGES="$(openstack image list --long -f value -c Name -c Protected \
-    | grep 'ZZCI.*True' | sed 's/ True//')"
-for i in $IMAGES; do
+IMAGES=($(openstack image list --long -f value -c Name -c Protected \
+    | grep 'ZZCI.*True' | sed 's/ True//'))
+for i in "${IMAGES[@]}"; do
     echo "Adding image $i"
     echo "* $i" >> "$WORKSPACE/docs/cloud-images.rst"
 done
