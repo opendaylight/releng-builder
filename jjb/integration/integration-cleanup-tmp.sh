095ed808cd5bf4f1e90b7d46432e627ac56c117e
Luis Gomez <ecelgp@gmail.com>
2022-09-07 18:37:57 -0700

    Clean netvirt csit jobs
    Change-Id: Ib3f491775f958b96bb685cce93cc9d489adabaf7
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- jjb/integration/integration-cleanup-tmp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Cleaning up Robot installation..."
-
-# ${ROBOT_VENV} comes from the integration-install-robotframework.sh
-# script.
-# TODO: Is this still needed when we have integration-cleanup-workspace?
-rm -rf "${ROBOT_VENV}"
