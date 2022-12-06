b5655d2aff919f6ae14957127e43aefce659e73b
Mario Doman <mario.doman@pantheon.tech>
2022-12-06 18:13:47 +0100

    Feat: add a script to script/bump_mri_versions
    This script loops trough pom.xml files in given dir and update patch
    versions on parent/dependencies artifacts.
    
    Change-Id: Ib709260b71a2f62179397e17df5a71251e0f4042
    Signed-off-by: Mario Doman <mario.doman@pantheon.tech>

---
+++ scripts/bump_mri_versions/main.py
@@ -0,0 +1,87 @@
+# Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+
+import requests, re, python_lib
+from pathlib import Path
+from bs4 import BeautifulSoup
+from lxml import etree
+
+
+
+def get_version_for_artifact(groupId, artifactId):
+    versions_list = []
+    url = f'https://repo1.maven.org/maven2/org/opendaylight/{groupId}/{artifactId}/'
+    response = requests.get(url).content
+    soup = BeautifulSoup(response, 'html.parser')
+    try:
+        html_lines = str(soup.find_all('pre')[0]).splitlines()
+    except IndexError:
+        return "NOT FOUND"
+    for line in html_lines:
+        # Use a regular expression to find version
+        pattern = re.compile(r'\d+\.\d+\.\d+')
+        title = pattern.search(line)
+        try:
+            versions_list.append(title.group())
+        except AttributeError:
+            pass
+    return python_lib.find_highest_revision(versions_list)
+
+
+# get all xml files
+for path in Path(python_lib.bumping_dir).rglob('*.xml'):
+    if "pom.xml" or "feature.xml" in str(path):
+
+        tree = etree.parse(path)
+        root = tree.getroot()
+        # update major and minor artifacts versions
+        if "pom.xml" in str(path):
+            prefix = "{" + root.nsmap[None] + "}"
+            all_elements = tree.findall(f'.//{prefix}parent') + tree.findall(f'.//{prefix}dependency')
+            for element in all_elements:
+                groupId = (element.find(f'{prefix}groupId'))
+                artifactId = (element.find(f'{prefix}artifactId'))
+                version = (element.find(f'{prefix}version'))
+                try:
+                    if "org.opendaylight" in groupId.text and version != None:
+                        if not version.text == "${project.version}" and not "SNAPSHOT" in version.text and not "@project.version@" in version.text:
+                            # check major version and minor version
+                            new_version = get_version_for_artifact(groupId.text.split(".")[2], artifactId.text)
+                            if python_lib.check_minor_version(version, new_version):
+                                print(python_lib.log_artifact(path, groupId, artifactId, version.text, new_version))
+                                version.text = new_version
+                                tree.write(path, encoding="UTF-8", pretty_print=True, doctype='<?xml version="1.0" encoding="UTF-8"?>')
+                except AttributeError:
+                    pass
+
+
+        # update feature versions
+        if "feature.xml" in str(path):
+            prefix = "{" + root.nsmap[None] + "}"
+            all_featuress = tree.findall(f'.//{prefix}feature')
+
+             # Solving "name" and "xmlns" order in root
+            for elem in root.getiterator():
+                if not (isinstance(elem, etree._Comment) or isinstance(elem, etree._ProcessingInstruction)):
+                    elem.tag = etree.QName(elem).localname
+            namespace = root.nsmap[None]
+            etree.cleanup_namespaces(root)
+            root.attrib["xmlns"] = namespace
+
+            # feature versions add +1
+            for feature in all_featuress:
+                if feature.attrib["version"] and feature.attrib["version"] != "${project.version}":
+                    current_version = feature.attrib["version"]
+                    # workaround for float feature versions
+                    nums = current_version[1:-1].split(',')
+                    if "." in nums[0]:
+                        nums[0] = str(((float(nums[0]) + 0.01)))
+                    else:
+                        nums[0], nums[1] = str(int(nums[0]) + 1), str(int(nums[1])+1)
+                    result = '[' + ','.join(nums) + ')'
+                    feature.attrib["version"] = result
+                    print(python_lib.log_artifact(path=path, version=current_version, new_version=result))
+                    tree.write(path, encoding="UTF-8", pretty_print=True, doctype='<?xml version="1.0" encoding="UTF-8"?>')
---
+++ scripts/bump_mri_versions/python_lib.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+
+# modify this dir for pick up project from there
+bumping_dir = f"repos"
+
+def find_highest_revision(revisions):
+    # convert list of strings to list of tuples
+    converted_items = [tuple(map(int, item.split('.'))) for item in revisions]
+    biggest_item = max(converted_items, key=lambda x: x)
+    biggest_version = '.'.join(str(x) for x in biggest_item)
+    return biggest_version
+
+def log_artifact(path, groupId=None, artifactId=None, version=None, new_version=None):
+    log = ""
+    log += "XML FILE: " + str(path) + "\n"
+    # if none, printing feature update
+    if groupId == None:
+        log_line = ("path:", path , "VERSION:", version, "NEW VERSION:", new_version)
+    # else printing artifact update
+    else:
+        log_line = ("groupId:", groupId.text , "ARTIFACT ID:", artifactId.text, "VERSION:", version, "NEW VERSION:", new_version)
+    log += str(log_line) + "\n"
+    log += str(100 * "*" + "\n")
+    return(log)
+
+def check_minor_version(version, new_version):
+    if int(new_version.split(".")[0]) != int(version.text.split(".")[0]) or int(new_version.split(".")[1]) != int(version.text.split(".")[1]) or int(new_version.split(".")[2]) != int(version.text.split(".")[2]):
+        return True
