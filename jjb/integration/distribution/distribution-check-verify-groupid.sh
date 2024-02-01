62e2daeffac8b1bcfb1b1a87c5f622e56eed0922
Anil Belur <abelur@linuxfoundation.org>
2024-02-01 10:03:02 +1000

    Feat!: Update JJB version to 6x
    Jenkins Job Builder 6.0.0 is released and breaks backward
    compatibility with 5.1.0.
    
    The breaking changes with previous 5.1.0 release:
    
    - Jobs are now expanded the same way as job templates.
    - Macros without parameters are now expanded the same way as macros
      with parameters.
    - Tags !include-raw: and !include-raw-escape: should now be used the
      same way in jobs and macros without parameters as they are used in
      job templates and macros with parameters.
    
    Ref:
    https://storyboard.openstack.org/#!/story/2010963
    https://storyboard.openstack.org/#!/story/2010588
    
    Issue: LF-JIRA RELENG-5119
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>
    Change-Id: I9c16d62bf40c1396a630b479dca12a62a6e48c56
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ jjb/integration/distribution/distribution-check-verify-groupid.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2024 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script performs version bumping activities for an ODL release.
+echo "---> distribution-check-verify-groupid.sh"
+
+echo "These are allowed projects: $ALLOW_PROJECTS"
+echo "These are distribution pulled projects:"
+EXIT_CODE="0"
+for PROJECT in `ls /tmp/r/org/opendaylight`; do
+echo "checking $PROJECT"
+if [[ "$ALLOW_PROJECTS" != *"$PROJECT"* ]]; then
+echo "ERROR: Not allowed project $PROJECT pulled"
+EXIT_CODE="1"
+fi
+done
+exit $EXIT_CODE
+echo "verify project groupId"
+mkdir -p /tmp/t/org/opendaylight/{gerrit-project}
+mv /tmp/n/org/opendaylight/{gerrit-project}/* /tmp/t/org/opendaylight/{gerrit-project}/
+test -z "`find /tmp/n/ -type f`" || ( echo "ERROR: Mismatched groupId detected (see above)." && false )
+rm -rf /tmp/n
+mv /tmp/t /tmp/n
