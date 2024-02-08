3e14028e60c533cf11f35907dd5024aadf3998aa
Anil Belur <abelur@linuxfoundation.org>
2024-02-08 11:19:14 +1000

    Revert "Revert "Feat!: Update JJB version to 6x""
    This reverts commit aa0bf03cea9c4990670a146bcd34137212c65a4c.
    
    Run merge with JJB 6x.
    
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>
    Change-Id: I0647a9c5342a8473abe441fd4108a31113f6cae9

---
+++ jjb/integration/distribution/distribution-check-verify-groupid.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2024 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script performs version bumping activities for an ODL release.
+echo "---> distribution-check-verify-groupid.sh"
+
+echo "These are allowed projects: $ALLOW_PROJECTS"
+echo "These are distribution pulled projects:"
+EXIT_CODE="0"
+for PROJECT in /tmp/r/org/opendaylight/*; do
+    if [[ ! -d "$PROJECT" ]]; then
+        continue
+    fi
+    echo "Checking $PROJECT"
+    if [[ "$ALLOW_PROJECTS" != *"$PROJECT"* ]]; then
+        echo "ERROR: Not allowed project $PROJECT pulled"
+        EXIT_CODE="1"
+    fi
+done
+exit $EXIT_CODE
